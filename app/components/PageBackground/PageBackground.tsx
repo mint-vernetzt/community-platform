af9047a424e7b50b1167142572fac35aa5223956
Sirko Stenz <sirko.stenz@digitalnoise.de>
2022-04-05 09:56:57 -0500

    fixed bg image width

--- app/components/PageBackground/PageBackground.tsx
+++ app/components/PageBackground/PageBackground.tsx
@@ -4,7 +4,7 @@ export interface PageBackgroundProps {}
 
 function PageBackground(props: PageBackgroundProps) {
   return (
-    <div className="fixed inset-y-0 left-0 h-screen w-[calc(50%_-_151px)] bg-center bg-cover z-0"
+    <div className="fixed inset-y-0 left-0 h-screen w-[calc(50%_-_16px)] lg:w-[calc(50%_-_104px)] xl:w-[calc(50%_-_126px)] 2xl:w-[calc(50%_-_148px)] bg-center bg-cover z-0"
       style={{
         backgroundImage: `url(${imageFile})`
       }}  
