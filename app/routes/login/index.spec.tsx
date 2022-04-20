c4baa3b005032124ef9d057923de4fc7022c87bf
Paul Maier <pauljustus279@gmail.com>
2022-04-20 14:28:50 +0200

    test: tests failing if database not configured (#19)
    * One block comment instead of many line comments
    
    Closes #15

--- app/routes/login/index.spec.tsx
+++ app/routes/login/index.spec.tsx
@@ -14,34 +14,34 @@ test("call loader", async () => {
   expect(data).toStrictEqual({});
 });
 
-// TODO: run e2e test
-// test("handle empty body", async () => {
-//   const responseEmpty = await action({
-//     request: new Request(path, { method: "POST" }),
-//     params: {},
-//     context: {},
-//   });
-//   expect(responseEmpty.status).toBe(400);
-// });
+/* TODO: run e2e test
+test("handle empty body", async () => {
+  const responseEmpty = await action({
+    request: new Request(path, { method: "POST" }),
+    params: {},
+    context: {},
+  });
+  expect(responseEmpty.status).toBe(400);
+});
 
-// test("handle empty email and/or password", async () => {
-//   const responseEmpty = await action({
-//     request: new Request(path, { method: "POST", body: new FormData() }),
-//     params: {},
-//     context: {},
-//   });
-//   expect(responseEmpty.status).toBe(400);
+test("handle empty email and/or password", async () => {
+  const responseEmpty = await action({
+    request: new Request(path, { method: "POST", body: new FormData() }),
+    params: {},
+    context: {},
+  });
+  expect(responseEmpty.status).toBe(400);
 
-//   const formData = new FormData();
-//   formData.append("email", "");
-//   formData.append("password", "");
-//   const responseEmptyString = await action({
-//     request: new Request(path, {
-//       method: "POST",
-//       body: formData,
-//     }),
-//     params: {},
-//     context: {},
-//   });
-//   expect(responseEmptyString.status).toBe(400);
-// });
+  const formData = new FormData();
+  formData.append("email", "");
+  formData.append("password", "");
+  const responseEmptyString = await action({
+    request: new Request(path, {
+      method: "POST",
+      body: formData,
+    }),
+    params: {},
+    context: {},
+  });
+  expect(responseEmptyString.status).toBe(400);
+});*/
