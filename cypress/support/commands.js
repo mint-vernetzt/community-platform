ac4e528cf40b56b0b13c08d30fb053d39f681d04
Peter Holló <hello@songsforthe.dev>
2022-04-25 23:59:34 +0200

    set type delay to 0

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,1 +1,9 @@
 import "@testing-library/cypress/add-commands";
+
+Cypress.Commands.overwrite(
+  "type",
+  (originalFn, subject, text, options = {}) => {
+    options.delay = 0;
+    return originalFn(subject, text, options);
+  }
+);
