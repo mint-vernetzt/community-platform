29b224fa70c50a5b92b99c476b5fc53d5669915f
Peter Holló <hello@songsforthe.dev>
2022-04-25 19:12:53 +0200

    remove user after insert in test

--- remix.config.js
+++ remix.config.js
@@ -2,7 +2,7 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  ignoredRouteFiles: [".*", "**/*.spec.*", "**/*.integration.*"],
+  ignoredRouteFiles: [".*", "**/*.spec.*", "**/*.func.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
