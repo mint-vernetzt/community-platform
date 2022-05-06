bb1847c7b652a7f699237ed1dbdc795e4a58b45d
Sirko Stenz <sirko.stenz@digitalnoise.de>
2022-05-06 05:16:52 +0200

    added change image button

--- app/components/ProfilePage/ProfilePage.tsx
+++ app/components/ProfilePage/ProfilePage.tsx
@@ -53,7 +53,29 @@ function ProfilePage(props: ProfilePageProps) {
                 <div className="h-36 w-36 bg-primary text-white text-6xl flex items-center justify-center rounded-md">
                   AS
                 </div>
-                <h3 className="mt-6 text-5xl mb-1">Anna Schöter</h3>
+                <div>
+                  <button className="btn btn-link flex flex-row items-center mt-2">
+                    <span className="mr-3">
+                      <svg
+                        width="17"
+                        height="16"
+                        viewBox="0 0 17 16"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          clip-rule="evenodd"
+                          d="M14.9 3.116a.423.423 0 0 0-.123-.299l-1.093-1.093a.422.422 0 0 0-.598 0l-.882.882 1.691 1.69.882-.882a.423.423 0 0 0 .123-.298Zm-3.293.087 1.69 1.69v.001l-5.759 5.76a.422.422 0 0 1-.166.101l-2.04.68a.211.211 0 0 1-.267-.267l.68-2.04a.423.423 0 0 1 .102-.166l5.76-5.76ZM2.47 14.029a1.266 1.266 0 0 1-.37-.895V3.851a1.266 1.266 0 0 1 1.265-1.266h5.486a.422.422 0 0 1 0 .844H3.366a.422.422 0 0 0-.422.422v9.283a.422.422 0 0 0 .422.422h9.284a.422.422 0 0 0 .421-.422V8.07a.422.422 0 0 1 .845 0v5.064a1.266 1.266 0 0 1-1.267 1.266H3.367c-.336 0-.658-.133-.895-.37Z"
+                          fill="#454C5C"
+                        />
+                      </svg>
+                    </span>
+                    <span>Bild ändern</span>
+                  </button>
+                </div>
+
+                <h3 className="mt-4 text-5xl mb-1">Anna Schöter</h3>
                 <p className="font-bold text-sm">
                   Community-Management MINTvernetzt
                 </p>
