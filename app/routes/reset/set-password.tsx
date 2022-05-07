549e4fc96a40fc30d30b12cb6f3577b5b27414bf
Peter Holló <hello@songsforthe.dev>
2022-05-07 14:23:39 +0200

    fix: reset password pages mobile broken (#61)
    * fix reset password mobile view
    * fix set password mobile view

--- app/routes/reset/set-password.tsx
+++ app/routes/reset/set-password.tsx
@@ -77,33 +77,28 @@ export default function SetPassword() {
     <Form method="post">
       <input name="accessToken" type="hidden" value={loaderData?.accessToken} />
       <PageBackground imagePath="/images/default_kitchen.jpg" />
-      <div className="md:container md:mx-auto relative z-10">
+      <div className="md:container md:mx-auto px-4 relative z-10">
         <div className="flex flex-row -mx-4 justify-end">
-          <div className="basis-6/12 px-4 pt-4 pb-24 flex flex-row items-center">
+          <div className="basis-full md:basis-6/12 px-4 pt-4 pb-24 flex flex-row items-center">
             <div className="">
               <HeaderLogo />
             </div>
             <div className="ml-auto"></div>
           </div>
         </div>
-        <div className="flex flex-row -mx-4">
-          <div className="basis-6/12 px-4"> </div>
-          <div className="basis-6/12 px-4">
+        <div className="flex flex-col md:flex-row -mx-4">
+          <div className="basis-full md:basis-6/12"> </div>
+          <div className="basis-full md:basis-6/12 xl:basis-5/12 px-4">
             <h1 className="mb-8">Neues Passwort vergeben</h1>
-          </div>
-        </div>
-        <div className="flex flex-row -mx-4">
-          <div className="basis-6/12 px-4"> </div>
-          <div className="basis-5/12 px-4">
             <div className="mb-4">
-              <InputPassword id="password" label="Passwort" isRequired />
+              <InputPassword id="password" label="Passwort" required />
             </div>
 
             <div className="mb-8">
               <InputPassword
                 id="passwordControl"
                 label="Passwort wiederholen"
-                isRequired
+                required
               />
             </div>
 
@@ -114,6 +109,11 @@ export default function SetPassword() {
             </div>
           </div>
         </div>
+        {/* <div className="flex flex-row -mx-4">
+          <div className="basis-6/12 px-4"> </div>
+          <div className="basis-5/12 px-4">
+          </div>
+        </div> */}
       </div>
     </Form>
   );
