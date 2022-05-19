bf80463e53ec15643b39a804ad42c4cd0f9d063d
Manuel Portela <manuel.portela@digitalnoise.de>
2022-05-19 17:25:57 +0200

    added website placeholder

--- app/routes/profile/$username/edit/index.tsx
+++ app/routes/profile/$username/edit/index.tsx
@@ -547,6 +547,7 @@ export default function Index() {
                         id="website"
                         label="Website URL"
                         defaultValue={profile.website}
+                        placeholder="https://www.domainname.tld/"
                         isPublic={profile.publicFields?.includes("website")}
                         errorMessage={errors?.website?.message}
                       />
