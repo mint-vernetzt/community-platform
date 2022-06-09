a959f13eb5ff064c31fe9196d77897201c9727df
Peter Holló <hello@songsforthe.dev>
2022-06-09 17:29:11 +0200

    fix: name of getUser changed

--- app/lib/auth/handleAuth.ts
+++ app/lib/auth/handleAuth.ts
@@ -1,11 +1,11 @@
 import { badRequest, forbidden } from "remix-utils";
-import { getUser } from "~/auth.server";
+import { getUserByRequest } from "~/auth.server";
 
 export async function handleAuthorization(request: Request, username: string) {
   if (typeof username !== "string" || username === "") {
     throw badRequest({ message: "username must be provided" });
   }
-  const currentUser = await getUser(request);
+  const currentUser = await getUserByRequest(request);
 
   if (currentUser?.user_metadata.username !== username) {
     throw forbidden({ message: "not allowed" });
