b95323c020307d09c56e089640d23f8b3c37ebd1
nac62116 <56918308+nac62116@users.noreply.github.com>
2022-06-14 10:55:31 +0200

    Inserted RemixForm, mutation and formAction

--- app/routes/reset/set-password.tsx
+++ app/routes/reset/set-password.tsx
@@ -11,7 +11,7 @@ import { updatePassword } from "../../auth.server";
 import InputPassword from "../../components/FormElements/InputPassword/InputPassword";
 import HeaderLogo from "../../components/HeaderLogo/HeaderLogo";
 import PageBackground from "../../components/PageBackground/PageBackground";
-import { Form as RemixForm, performMutation } from "remix-forms";
+import { Form as RemixForm, formAction, performMutation } from "remix-forms";
 import { z } from "zod";
 import { makeDomainFunction } from "remix-domains";
 
@@ -39,52 +39,60 @@ export const loader: LoaderFunction = async (args) => {
   return json<LoaderData>({ accessToken });
 };
 
-export const action: ActionFunction = async (args) => {
-  const { request } = args;
+const mutation = makeDomainFunction(schema)(async (values) => {
+  return values;
+});
 
-  let formData: FormData;
-  try {
-    formData = await request.formData();
-  } catch (error) {
-    throw badRequest({ message: "Invalid Form Data." });
-  }
+export const action: ActionFunction = async ({ request }) =>
+  formAction({
+    request,
+    schema,
+    mutation,
+    successPath: "/login",
+  });
 
-  const accessToken = formData.get("accessToken");
-  if (accessToken === null || accessToken === "") {
-    throw badRequest({ message: "Access token required." });
-  }
+// let formData: FormData;
+// try {
+//   formData = await request.formData();
+// } catch (error) {
+//   throw badRequest({ message: "Invalid Form Data." });
+// }
 
-  const password = formData.get("password");
-  const passwordControl = formData.get("passwordControl");
+// const accessToken = formData.get("accessToken");
+// if (accessToken === null || accessToken === "") {
+//   throw badRequest({ message: "Access token required." });
+// }
 
-  if (
-    password !== null &&
-    password !== "" &&
-    passwordControl !== null &&
-    passwordControl !== ""
-  ) {
-    if (password !== passwordControl) {
-      throw badRequest({ message: "Passwords not identical." });
-    }
-    const { error } = await updatePassword(
-      accessToken as string,
-      password as string
-    );
+// const password = formData.get("password");
+// const passwordControl = formData.get("passwordControl");
 
-    // ignore user with email not exist
-    if (error) {
-      console.error(error.message);
-      throw serverError({ message: error.message });
-    }
-  }
-  return redirect("/login");
-};
+// if (
+//   password !== null &&
+//   password !== "" &&
+//   passwordControl !== null &&
+//   passwordControl !== ""
+// ) {
+//   if (password !== passwordControl) {
+//     throw badRequest({ message: "Passwords not identical." });
+//   }
+//   const { error } = await updatePassword(
+//     accessToken as string,
+//     password as string
+//   );
+
+//   // ignore user with email not exist
+//   if (error) {
+//     console.error(error.message);
+//     throw serverError({ message: error.message });
+//   }
+// }
+// return redirect("/login");
 
 export default function SetPassword() {
   const loaderData = useLoaderData<LoaderData>();
 
   return (
-    <Form method="post">
+    <>
       <input name="accessToken" type="hidden" value={loaderData?.accessToken} />
       <PageBackground imagePath="/images/default_kitchen.jpg" />
       <div className="md:container md:mx-auto px-4 relative z-10">
@@ -96,35 +104,59 @@ export default function SetPassword() {
             <div className="ml-auto"></div>
           </div>
         </div>
-        <div className="flex flex-col md:flex-row -mx-4">
-          <div className="basis-full md:basis-6/12"> </div>
-          <div className="basis-full md:basis-6/12 xl:basis-5/12 px-4">
-            <h1 className="mb-8">Neues Passwort vergeben</h1>
-            <div className="mb-4">
-              <InputPassword id="password" label="Passwort" required />
-            </div>
+        <RemixForm method="post" schema={schema}>
+          {({ Field, Button, Errors, register }) => (
+            <div className="flex flex-col md:flex-row -mx-4">
+              <div className="basis-full md:basis-6/12"> </div>
+              <div className="basis-full md:basis-6/12 xl:basis-5/12 px-4">
+                <h1 className="mb-8">Neues Passwort vergeben</h1>
+                <div className="mb-4">
+                  <Field name="password" label="Neues Passwort">
+                    {({ Errors }) => (
+                      <>
+                        <InputPassword
+                          id="password"
+                          label="Neues Passwort"
+                          required
+                          {...register("password")}
+                        />
+                        <Errors />
+                      </>
+                    )}
+                  </Field>
+                </div>
 
-            <div className="mb-8">
-              <InputPassword
-                id="passwordControl"
-                label="Passwort wiederholen"
-                required
-              />
-            </div>
+                <div className="mb-8">
+                  <Field name="confirmPassword" label="Wiederholen">
+                    {({ Errors }) => (
+                      <>
+                        <InputPassword
+                          id="confirmPassword"
+                          label="Passwort wiederholen"
+                          required
+                          {...register("confirmPassword")}
+                        />
+                        <Errors />
+                      </>
+                    )}
+                  </Field>
+                </div>
 
-            <div className="mb-8">
-              <button type="submit" className="btn btn-primary">
-                Passwort speichern
-              </button>
+                <div className="mb-8">
+                  <button type="submit" className="btn btn-primary">
+                    Passwort speichern
+                  </button>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          )}
+        </RemixForm>
         {/* <div className="flex flex-row -mx-4">
           <div className="basis-6/12 px-4"> </div>
           <div className="basis-5/12 px-4">
           </div>
         </div> */}
       </div>
-    </Form>
+    </>
   );
 }
