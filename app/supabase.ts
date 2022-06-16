ea348b7e23380708a5fbe7e470937f254906b1dc
Peter Holló <hello@songsforthe.dev>
2022-06-16 15:56:23 +0200

    create bucket (if not exist)

--- app/supabase.ts
+++ app/supabase.ts
@@ -33,4 +33,48 @@ export const supabaseAdmin = createClient(
   process.env.SERVICE_ROLE_KEY as string
 );
 
+// initialize buckets
+
+async function initializeBuckets(buckets: string[]) {
+  // for (let i = 0, len = buckets.length; i < len; i++) {
+  //   const {error} = await supabaseAdmin.storage.deleteBucket(buckets[i]);
+  //   console.log(error);
+  // }
+
+  const { data, error } = await supabaseAdmin.storage.listBuckets();
+
+  if (error) {
+    throw error;
+  }
+
+  if (data === null) {
+    throw new Error("Something went wrong initializing buckets");
+  }
+
+  const bucketNames = data.map((bucket) => bucket.name);
+
+  const bucketsNotInitialized = buckets.filter(
+    (bucketName) => !bucketNames.includes(bucketName)
+  );
+  const bucketsStillInitialized = bucketsNotInitialized.length === 0;
+  if (bucketsStillInitialized) {
+    return;
+  }
+
+  for (let i = 0, len = bucketsNotInitialized.length; i < len; i++) {
+    const { error } = await supabaseAdmin.storage.createBucket(
+      bucketsNotInitialized[i],
+      { public: true }
+    );
+    if (error) {
+      throw error;
+    }
+  }
+}
+
+const buckets = ["images"];
+initializeBuckets(buckets).finally(() =>
+  console.log(`Bucket(s) "${buckets.join(", ")}" initialized.`)
+);
+
 export { Session };
