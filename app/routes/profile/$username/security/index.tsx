00edc3f901c34553b8acb1518fd3f0b8f4a305f1
nac62116 <56918308+nac62116@users.noreply.github.com>
2022-06-27 09:11:48 +0200

    bug: fixed profile menu and edit profile menu (#123)

--- app/routes/profile/$username/security/index.tsx
+++ app/routes/profile/$username/security/index.tsx
@@ -27,6 +27,8 @@ import { z } from "zod";
 import { makeDomainFunction } from "remix-domains";
 import { Form as RemixForm, performMutation } from "remix-forms";
 import InputPassword from "~/components/FormElements/InputPassword/InputPassword";
+import ProfileMenu from "../ProfileMenu";
+import Header from "../Header";
 
 const emailSchema = z.object({
   email: z
@@ -152,86 +154,13 @@ export default function Index() {
 
   return (
     <>
-      <header className="shadow-md mb-8">
-        <div className="container mx-auto px-4 relative z-11">
-          <div className="basis-full md:basis-6/12 px-4 pt-3 pb-3 flex flex-row items-center">
-            <div>
-              <Link to="/explore">
-                <HeaderLogo />
-              </Link>
-            </div>
-
-            <div className="ml-auto">
-              <div className="dropdown dropdown-end">
-                <label tabIndex={0} className="btn btn-primary w-10 h-10">
-                  {initials}
-                </label>
-                <ul
-                  tabIndex={0}
-                  className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
-                >
-                  <li>
-                    <Link to={`/profile/${username}`}>Profil anzeigen</Link>
-                  </li>
-                  <li>
-                    <Link to={`/profile/${username}/edit`}>
-                      Profil bearbeiten
-                    </Link>
-                  </li>
-                  <li>
-                    <Form action="/logout?index" method="post">
-                      <button type="submit" className="w-full text-left">
-                        Logout
-                      </button>
-                    </Form>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </header>
+      <Header username={username ?? ""} initials={initials} />
       <fieldset disabled={transition.state === "submitting"}>
         <div className="container mx-auto px-4 relative z-10 pb-44">
           <div className="flex flex-col lg:flex-row -mx-4">
-            {/* refactor menu */}
             <div className="md:flex md:flex-row px-4 pt-10 lg:pt-0">
               <div className="basis-4/12 px-4">
-                <div className="px-4 py-8 lg:p-8 pb-15 rounded-lg bg-neutral-200 shadow-lg relative mb-8">
-                  <h3 className="font-bold mb-7">Profil bearbeiten</h3>
-                  {/*TODO: add missing pages*/}
-                  <ul>
-                    <li>
-                      <a
-                        href={`/profile/${username}/edit`}
-                        className="block text-3xl  text-neutral-500 hover:text-primary py-3"
-                      >
-                        Persönliche Daten
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href={`/profile/${username}/security`}
-                        className="block text-3xl text-primary py-3"
-                      >
-                        Login und Sicherheit
-                      </a>
-                    </li>
-                  </ul>
-
-                  <hr className="border-neutral-400 my-4 lg:my-8" />
-
-                  <div className="">
-                    {/* <a
-                          href="/#"
-                          className="block text-3xl text-neutral-500 hover:text-primary py-3"
-                        > */}
-                    <span className="block text-3xl text-neutral-500  py-3">
-                      Profil löschen
-                    </span>
-                    {/* </a> */}
-                  </div>
-                </div>
+                <ProfileMenu username={username as string} />
               </div>
 
               <div className="basis-6/12 px-4">
