cf9930af6bfb01ab2559f7fe4a9bd001cf02661d
nac62116 <56918308+nac62116@users.noreply.github.com>
2022-06-30 18:00:47 +0200

    Added Index and Action function wip: Fix self relation MemberOfNetwork

--- app/organization.server.ts
+++ app/organization.server.ts
@@ -1,31 +1,236 @@
-import {
-  AreasOnOrganizations,
-  MemberOfNetwork,
-  MemberOfOrganization,
-  Organization,
-  OrganizationTypesOnOrganizations,
-} from ".prisma/client";
+import { Organization } from ".prisma/client";
 import { prismaClient } from "./prisma";
 
 export type OrganizationWithRelations = Organization & {
-  types: OrganizationTypesOnOrganizations[];
-  teamMembers: MemberOfOrganization[];
-  memberOf: MemberOfNetwork[];
-  networkMembers: MemberOfNetwork[];
-  areas: AreasOnOrganizations[];
+  types: {
+    organizationType: {
+      title: string;
+    };
+  }[];
+  teamMembers: {
+    profileId: string;
+    isPrivileged: boolean;
+    profile: {
+      username: string;
+      avatar: string | null;
+      firstName: string;
+      lastName: string;
+      academicTitle: string | null;
+      position: string | null;
+    };
+  }[];
+  memberOf: {
+    umbrellaOrganization: {
+      slug: string;
+      logo: string | null;
+      name: string;
+      types: {
+        organizationType: {
+          title: string;
+        };
+      }[];
+    };
+  }[];
+  networkMembers: {
+    organization: {
+      slug: string;
+      logo: string | null;
+      name: string;
+      types: {
+        organizationType: {
+          title: string;
+        };
+      }[];
+    };
+  }[];
+  areas: {
+    area: {
+      name: string;
+    };
+  }[];
 };
 
-export async function getOrganisationBySlug(slug: string) {
+export async function getOrganizationBySlug(slug: string) {
   const organization = await prismaClient.organization.findUnique({
     where: { slug },
     include: {
-      types: true,
-      teamMembers: true,
-      memberOf: true,
-      networkMembers: true,
-      areas: true,
+      types: {
+        select: {
+          organizationType: {
+            select: {
+              title: true,
+            },
+          },
+        },
+      },
+      teamMembers: {
+        select: {
+          profileId: true,
+          isPrivileged: true,
+          profile: {
+            select: {
+              username: true,
+              avatar: true,
+              firstName: true,
+              lastName: true,
+              academicTitle: true,
+              position: true,
+            },
+          },
+        },
+      },
+      memberOf: {
+        select: {
+          umbrellaOrganization: {
+            select: {
+              slug: true,
+              logo: true,
+              name: true,
+              types: {
+                select: {
+                  organizationType: {
+                    select: {
+                      title: true,
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+      networkMembers: {
+        select: {
+          organization: {
+            select: {
+              slug: true,
+              logo: true,
+              name: true,
+              types: {
+                select: {
+                  organizationType: {
+                    select: {
+                      title: true,
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+      areas: {
+        select: {
+          area: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      },
     },
   });
 
   return organization as OrganizationWithRelations | null;
 }
+
+export async function getOrganizationMembersBySlug(slug: string) {
+  const organization = await prismaClient.organization.findUnique({
+    where: { slug },
+    select: {
+      teamMembers: true,
+    },
+  });
+
+  return organization;
+}
+
+// export async function getOrganizationById(id: string) {
+//   const organization = await prismaClient.organization.findUnique({
+//     where: { id },
+//     include: {
+//       types: {
+//         select: {
+//           organizationType: {
+//             select: {
+//               title: true,
+//             },
+//           },
+//         },
+//       },
+//       teamMembers: {
+//         select: {
+//           profileId: true,
+//           isPrivileged: true,
+//           profile: {
+//             select: {
+//               username: true,
+//               avatar: true,
+//               firstName: true,
+//               lastName: true,
+//               academicTitle: true,
+//               position: true,
+//             },
+//           },
+//         },
+//       },
+//       memberOf: {
+//         select: {
+//           umbrellaOrganization: {
+//             select: {
+//               slug: true,
+//               logo: true,
+//               name: true,
+//               types: {
+//                 select: {
+//                   organizationType: {
+//                     select: {
+//                       title: true,
+//                     },
+//                   },
+//                 },
+//               },
+//             },
+//           },
+//         },
+//         where: {
+//           organizationId: id,
+//         },
+//       },
+//       networkMembers: {
+//         select: {
+//           organization: {
+//             select: {
+//               slug: true,
+//               logo: true,
+//               name: true,
+//               types: {
+//                 select: {
+//                   organizationType: {
+//                     select: {
+//                       title: true,
+//                     },
+//                   },
+//                 },
+//               },
+//             },
+//           },
+//         },
+//         where: {
+//           umbrellaOrganizationId: id,
+//         },
+//       },
+//       areas: {
+//         select: {
+//           area: {
+//             select: {
+//               name: true,
+//             },
+//           },
+//         },
+//       },
+//     },
+//   });
+
+//   return organization as OrganizationWithRelations | null;
+// }
