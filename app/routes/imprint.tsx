dff40adc7de0362f9b3eb957e47c301af2adcea0
nac62116 <56918308+nac62116@users.noreply.github.com>
2022-07-29 12:17:05 +0200

    Created imprint route
    - Also beautified goodbye.tsx a little

---
+++ app/routes/imprint.tsx
@@ -0,0 +1,48 @@
+export default function Imprint() {
+  return (
+    <section className="container mt-8 md:mt-10 lg:mt-20">
+      <h1>Impressum</h1>
+      <p className="mb-2">MINTvernetzt ist ein Verbundprojekt des / der:</p>
+      <ul className="mb-4">
+        <li>- Körber-Stiftung</li>
+        <li>- matrix gGmbH</li>
+        <li>- Stifterverbands für die deutsche Wissenschaft e.V.</li>
+        <li>- Nationalen MINT Forum e.V.</li>
+        <li>- Universität Regensburg</li>
+      </ul>
+      <p className="mb-2">
+        Als Dienstanbieterin im Sinne des § 5 TMG, Kontaktstelle, Betreiberin
+        und Vertragspartnerin nach diesen Bestimmungen ist die
+      </p>
+      <p>
+        matrix Gesellschaft für Beratung in Wirtschaft, Politik und Verwaltung
+        mbH &amp; Co. KG
+      </p>
+      <p>Schloss Elbroich</p>
+      <p>Am Falder 4</p>
+      <p className="mb-4">40589 Düsseldorf</p>
+      <h6 className="mb-2">Vertretungsberechtigt:</h6>
+      <p>Gregor Frankenstein-von der Beeck</p>
+      <p>Guido Lohnherr</p>
+      <p>Tel.: +49(0)211-75707-910</p>
+      <p>Fax: +49(0)211-987300</p>
+      <p>
+        E-Mail:{" "}
+        <a
+          className="text-primary hover:underline"
+          href="mailto:info@matrix-gmbh.de"
+          target="_blank"
+        >
+          info@matrix-gmbh.de
+        </a>
+      </p>
+      <p>Umsatzsteueridentifikationsnummer: (USt. Ident-Nr) DE 222703085</p>
+      <p>Amtsgericht Düsseldorf – HRA 15120</p>
+      <p className="mb-2">berufen.</p>
+      <p>
+        Inhaltlich verantwortlich nach § 18 MStVist Arne Klauke, erreichbar über
+        die Dienstanbieter.
+      </p>
+    </section>
+  );
+}
