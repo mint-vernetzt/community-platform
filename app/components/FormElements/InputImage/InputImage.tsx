3bb961a90fd4a8a2b8a0a7a14f05fa98e17a6ef9
nac62116 <56918308+nac62116@users.noreply.github.com>
2022-08-03 12:47:43 +0200

    Improved InputImage component on profile

--- app/components/FormElements/InputImage/InputImage.tsx
+++ app/components/FormElements/InputImage/InputImage.tsx
@@ -7,6 +7,7 @@ export type InputImageProps = {
   maxWidth?: number;
   maxHeight?: number;
   classes?: string;
+  containerRef: React.RefObject<HTMLDivElement>;
 } & Pick<React.HTMLProps<HTMLInputElement>, "id" | "name" | "accept">;
 
 function InputImage(props: InputImageProps) {
@@ -18,10 +19,13 @@ function InputImage(props: InputImageProps) {
     maxHeight,
     classes,
     accept = "image/*",
+    containerRef,
     ...otherProps
   } = props;
 
   const [error, setError] = React.useState<Error | null>(null);
+  const [selectedImage, setSelectedImage] =
+    React.useState<HTMLImageElement | null>(null);
 
   const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
     const fileList = event.target.files;
@@ -34,30 +38,41 @@ function InputImage(props: InputImageProps) {
     const file = fileList[0];
 
     if (maxSize !== undefined && file.size > maxSize) {
-      setError(new Error(`Image to large (max. ${maxSize} Byte)`));
+      setError(
+        new Error(`Bild zu groß (max. ${Math.round(maxSize / 100000) / 10} MB)`)
+      );
       return;
     }
 
     const image = new Image();
 
     image.onload = () => {
-      if (minWidth !== undefined && image.width < minWidth) {
-        setError(new Error(`Image width to small (min. ${minWidth} px)`));
+      if (
+        minWidth !== undefined &&
+        minHeight !== undefined &&
+        (image.width < minWidth || image.height < minHeight)
+      ) {
+        setError(
+          new Error(`Bild zu klein (min. ${minWidth} x ${minHeight} px)`)
+        );
         return;
       }
-      if (maxWidth !== undefined && image.width > maxWidth) {
-        setError(new Error(`Image width to large (max. ${maxWidth} px)`));
+      if (
+        maxWidth !== undefined &&
+        maxHeight !== undefined &&
+        (image.width > maxWidth || image.height > maxHeight)
+      ) {
+        setError(
+          new Error(`Bild zu groß (max. ${maxWidth} x ${maxHeight} px)`)
+        );
         return;
       }
-      if (minHeight !== undefined && image.height < minHeight) {
-        setError(new Error(`Image height to small (min. ${minHeight} px)`));
-        return;
+      if (containerRef !== null && containerRef.current !== null) {
+        containerRef.current.innerHTML = "";
+        image.classList.add("object-cover", "w-full", "h-full");
+        containerRef.current.appendChild(image);
       }
-      if (maxHeight !== undefined && image.height > maxHeight) {
-        setError(new Error(`Image height to large (max. ${maxHeight} px)`));
-        return;
-      }
-
+      setSelectedImage(image);
       setError(null);
     };
 
@@ -66,6 +81,21 @@ function InputImage(props: InputImageProps) {
 
   return (
     <>
+      <label
+        htmlFor={props.id}
+        className="flex content-center items-center nowrap py-2 cursor-pointer text-primary"
+      >
+        <svg
+          width="17"
+          height="16"
+          viewBox="0 0 17 16"
+          xmlns="http://www.w3.org/2000/svg"
+          className="fill-neutral-600"
+        >
+          <path d="M14.9 3.116a.423.423 0 0 0-.123-.299l-1.093-1.093a.422.422 0 0 0-.598 0l-.882.882 1.691 1.69.882-.882a.423.423 0 0 0 .123-.298Zm-3.293.087 1.69 1.69v.001l-5.759 5.76a.422.422 0 0 1-.166.101l-2.04.68a.211.211 0 0 1-.267-.267l.68-2.04a.423.423 0 0 1 .102-.166l5.76-5.76ZM2.47 14.029a1.266 1.266 0 0 1-.37-.895V3.851a1.266 1.266 0 0 1 1.265-1.266h5.486a.422.422 0 0 1 0 .844H3.366a.422.422 0 0 0-.422.422v9.283a.422.422 0 0 0 .422.422h9.284a.422.422 0 0 0 .421-.422V8.07a.422.422 0 0 1 .845 0v5.064a1.266 1.266 0 0 1-1.267 1.266H3.367c-.336 0-.658-.133-.895-.37Z" />
+        </svg>
+        <span className="ml-2 mr-4">Bild auswählen</span>
+      </label>
       <input
         type="file"
         onChange={handleChange}
@@ -73,6 +103,10 @@ function InputImage(props: InputImageProps) {
         {...otherProps}
         className={classes}
       />
+
+      {selectedImage !== null && (
+        <button className="btn btn-primary btn-small">Bild hochladen</button>
+      )}
       {error !== null && <p>{error.message}</p>}
     </>
   );
--- app/routes/profile/$username/index.tsx
+++ app/routes/profile/$username/index.tsx
@@ -6,6 +6,7 @@ import {
   Profile,
 } from "@prisma/client";
 import { GravityType } from "imgproxy/dist/types";
+import React from "react";
 import {
   ActionFunction,
   Form,
@@ -110,8 +111,7 @@ export const loader: LoaderFunction = async (
     const publicURL = getPublicURL(profile.background);
     if (publicURL !== null) {
       images.background = getImageURL(publicURL, {
-        resize: { type: "fill", width: 1488, height: 480 },
-        gravity: GravityType.north_east,
+        resize: { type: "fit", width: 1488, height: 480 },
       });
     }
   }
@@ -226,7 +226,7 @@ export const action: ActionFunction = async (args) => {
   const avatarPublicURL = formData.get("avatar");
   if (avatarPublicURL !== null && typeof avatarPublicURL === "string") {
     images.avatar = getImageURL(avatarPublicURL, {
-      resize: { type: "fit", width: 144, height: 144 },
+      resize: { type: "fill", width: 144, height: 144 },
     });
   }
   const backgroundPublicURL = formData.get("background");
@@ -276,6 +276,9 @@ export default function Index() {
 
   const actionData = useActionData();
 
+  const backgroundContainer = React.useRef(null);
+  const avatarContainer = React.useRef(null);
+
   let avatar;
   if (actionData !== undefined && actionData.images.avatar !== undefined) {
     avatar = actionData.images.avatar;
@@ -290,39 +293,30 @@ export default function Index() {
     background = loaderData.images.background;
   }
 
+  console.log(background);
+
   return (
     <>
       <section className="hidden md:block container mt-8 md:mt-10 lg:mt-20">
         <div className="hero hero-news flex items-end rounded-3xl relative overflow-hidden bg-yellow-500 h-60 lg:h-120">
-          {background !== undefined && (
-            <img
-              src={background}
-              alt={fullName}
-              className="object-cover w-full h-full"
-            />
-          )}
+          <div ref={backgroundContainer} className="w-full h-full">
+            {background !== undefined && (
+              <img
+                src={background}
+                alt={fullName}
+                className="object-cover w-full h-full"
+              />
+            )}
+          </div>
           {loaderData.mode === "owner" && (
             <div className="absolute bottom-6 right-6">
+              {/** TODO: Transition on upload */}
               <Form
                 method="post"
                 encType="multipart/form-data"
                 className="flex items-center"
+                reloadDocument
               >
-                <label
-                  htmlFor="background"
-                  className="flex content-center items-center nowrap py-2 cursor-pointer text-primary"
-                >
-                  <svg
-                    width="17"
-                    height="16"
-                    viewBox="0 0 17 16"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="fill-neutral-600"
-                  >
-                    <path d="M14.9 3.116a.423.423 0 0 0-.123-.299l-1.093-1.093a.422.422 0 0 0-.598 0l-.882.882 1.691 1.69.882-.882a.423.423 0 0 0 .123-.298Zm-3.293.087 1.69 1.69v.001l-5.759 5.76a.422.422 0 0 1-.166.101l-2.04.68a.211.211 0 0 1-.267-.267l.68-2.04a.423.423 0 0 1 .102-.166l5.76-5.76ZM2.47 14.029a1.266 1.266 0 0 1-.37-.895V3.851a1.266 1.266 0 0 1 1.265-1.266h5.486a.422.422 0 0 1 0 .844H3.366a.422.422 0 0 0-.422.422v9.283a.422.422 0 0 0 .422.422h9.284a.422.422 0 0 0 .421-.422V8.07a.422.422 0 0 1 .845 0v5.064a1.266 1.266 0 0 1-1.267 1.266H3.367c-.336 0-.658-.133-.895-.37Z" />
-                  </svg>
-                  <span className="ml-2 mr-4">Hintergrund ändern</span>
-                </label>
                 <InputImage
                   id="background"
                   name="background"
@@ -332,8 +326,8 @@ export default function Index() {
                   maxWidth={1920} // 1920 px
                   maxHeight={1080} // 1080 px
                   classes="opacity-0 w-0 h-0"
+                  containerRef={backgroundContainer}
                 />
-                <button className="btn btn-primary btn-small">Upload</button>
               </Form>
             </div>
           )}
@@ -344,7 +338,10 @@ export default function Index() {
           <div className="md:flex-1/2 lg:flex-5/12 px-4 pt-10 lg:pt-0">
             <div className="px-4 py-8 lg:p-8 pb-15 md:pb-5 rounded-3xl border border-neutral-400 bg-neutral-200 shadow-lg relative lg:ml-14 lg:-mt-64">
               <div className="flex items-center flex-col">
-                <div className="h-36 w-36 bg-primary text-white text-6xl flex items-center justify-center rounded-md overflow-hidden">
+                <div
+                  ref={avatarContainer}
+                  className="h-36 w-36 bg-primary text-white text-6xl flex items-center justify-center rounded-md overflow-hidden"
+                >
                   {avatar !== undefined ? (
                     <img src={avatar} alt={fullName} />
                   ) : (
@@ -356,22 +353,8 @@ export default function Index() {
                     method="post"
                     encType="multipart/form-data"
                     className="flex items-center mt-4"
+                    reloadDocument
                   >
-                    <label
-                      htmlFor="avatar"
-                      className="flex content-center items-center nowrap py-2 cursor-pointer text-primary"
-                    >
-                      <svg
-                        width="17"
-                        height="16"
-                        viewBox="0 0 17 16"
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="fill-neutral-600"
-                      >
-                        <path d="M14.9 3.116a.423.423 0 0 0-.123-.299l-1.093-1.093a.422.422 0 0 0-.598 0l-.882.882 1.691 1.69.882-.882a.423.423 0 0 0 .123-.298Zm-3.293.087 1.69 1.69v.001l-5.759 5.76a.422.422 0 0 1-.166.101l-2.04.68a.211.211 0 0 1-.267-.267l.68-2.04a.423.423 0 0 1 .102-.166l5.76-5.76ZM2.47 14.029a1.266 1.266 0 0 1-.37-.895V3.851a1.266 1.266 0 0 1 1.265-1.266h5.486a.422.422 0 0 1 0 .844H3.366a.422.422 0 0 0-.422.422v9.283a.422.422 0 0 0 .422.422h9.284a.422.422 0 0 0 .421-.422V8.07a.422.422 0 0 1 .845 0v5.064a1.266 1.266 0 0 1-1.267 1.266H3.367c-.336 0-.658-.133-.895-.37Z" />
-                      </svg>
-                      <span className="ml-2 mr-4">Bild ändern</span>
-                    </label>
                     <InputImage
                       id="avatar"
                       name="avatar"
@@ -381,10 +364,8 @@ export default function Index() {
                       maxWidth={500} // 500 px
                       maxHeight={500} // 500 px
                       classes="opacity-0 w-0 h-0"
+                      containerRef={avatarContainer}
                     />
-                    <button className="btn btn-primary btn-small">
-                      Upload
-                    </button>
                   </Form>
                 )}
 
