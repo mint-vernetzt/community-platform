80e9af58a7a9a2ac3d921b1d162de3f7e7a98736
Peter Holló <hello@songsforthe.dev>
2022-08-04 18:44:39 +0200

    todo check jest setup

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,10 +1,10 @@
 import "@testing-library/jest-dom/extend-expect";
-
 import { installGlobals } from "@remix-run/node";
+
 installGlobals();
 
 //global.setImmediate = jest.useRealTimers;
-global.setImmediate = (callback) => callback();
+global.setImmediate = (callback) => callback(); // TODO: seems to be unnecessary
 
 jest.mock("~/prisma", () => {
   return { prismaClient: {} };
