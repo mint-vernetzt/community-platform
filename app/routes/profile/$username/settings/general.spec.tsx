c87c029dcead205c79c87e2a61802f1000b11453
Peter Holló <hello@songsforthe.dev>
2022-08-04 18:44:55 +0200

    add some unit tests

--- app/routes/profile/$username/settings/general.spec.tsx
+++ app/routes/profile/$username/settings/general.spec.tsx
@@ -1,27 +1,27 @@
-import { loader } from "./general";
-import { getWholeProfileFromId, handleAuthorization } from "./utils.server";
+import { createRequestWithFormData } from "~/lib/utils/tests";
+import { action, loader } from "./general";
+import { getWholeProfileFromId, updateProfileById } from "./utils.server";
 
 /** @type {jest.Expect} */
 // @ts-ignore
 const expect = global.expect;
 
+const id = "1";
+const username = "sookie";
+
 jest.mock("./utils.server", () => {
-  return { getWholeProfileFromId: jest.fn(), handleAuthorization: jest.fn() };
+  return {
+    getWholeProfileFromId: jest.fn(),
+    handleAuthorization: jest.fn().mockResolvedValue({ id }),
+    updateProfileById: jest.fn(),
+  };
 });
 
 jest.mock("~/profile.server", () => {
   return { getAllOffers: jest.fn(), getAreas: jest.fn() };
 });
 
-const username = "sookie";
-const id = "1";
-
 describe("loader", () => {
-  beforeEach(() => {
-    (handleAuthorization as jest.Mock).mockImplementation(() => {
-      return { id };
-    });
-  });
   test("no profile found in db", async () => {
     (getWholeProfileFromId as jest.Mock).mockImplementationOnce(() => {
       return null;
@@ -76,5 +76,182 @@ describe("loader", () => {
     expect(response.profile.areas).toEqual(["area1", "area2"]);
     expect(response.profile.offers).toEqual(["offer1"]);
     expect(response.profile.seekings).toEqual(["offer1", "offer2", "offer3"]);
+  });
+});
+
+jest.mock("~/utils.server", () => {
+  return {
+    validateCSRFToken: jest.fn(),
+  };
+});
+
+describe("action", () => {
+  const formDefaults = {
+    academicTitle: "",
+    position: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    bio: "",
+    website: "",
+    facebook: "",
+    linkedin: "",
+    twitter: "",
+    youtube: "",
+    instagram: "",
+    xing: "",
+    areas: [],
+    skills: [],
+    offers: [],
+    interests: [],
+    seekings: [],
+    publicFields: [],
+  };
+
+  const parsedDataDefaults = Object.entries(formDefaults).reduce(
+    (acc: { [key: string]: string | null | string[] }, cur) => {
+      const [key, value] = cur;
+      if (value === "") {
+        acc[key] = null;
+      } else {
+        acc[key] = value;
+      }
+      return acc;
+    },
+    {}
+  );
+
+  test("all fields required", async () => {
+    const request = createRequestWithFormData({ firstName: "First Name" });
+
+    expect.assertions(2);
+
+    try {
+      await action({
+        request,
+        context: {},
+        params: { username },
+      });
+    } catch (error) {
+      const response = error as Response;
+      expect(response.status).toBe(400);
+
+      const json = await response.json();
+      expect(json.message).toBe("Validation failed");
+    }
+  });
+
+  describe("validate email", () => {
+    test('email is "undefined"', async () => {
+      const { email: _email, ...otherDefaults } = formDefaults;
+      const request = createRequestWithFormData({
+        ...otherDefaults,
+      });
+
+      const response = await action({
+        request,
+        context: {},
+        params: { username },
+      });
+      expect(response.errors.email).not.toBeUndefined();
+      expect(response.errors.email.message).toEqual(
+        expect.stringContaining("email must be a `string` type")
+      );
+    });
+    test("email is empty", async () => {
+      const request = createRequestWithFormData({
+        ...formDefaults,
+      });
+
+      const response = await action({
+        request,
+        context: {},
+        params: { username },
+      });
+      expect(response.errors.email).not.toBeUndefined();
+      expect(response.errors.email.message).toEqual(
+        expect.stringContaining("email is a required field")
+      );
+    });
+
+    test("email is invalid", async () => {
+      const request = createRequestWithFormData({
+        ...formDefaults,
+        email: "invalid email",
+      });
+
+      const response = await action({
+        request,
+        context: {},
+        params: { username },
+      });
+      expect(response.errors.email).not.toBeUndefined();
+      expect(response.errors.email.message).toEqual(
+        expect.stringContaining("email must be a valid email")
+      );
+    });
+
+    test("email is valid", async () => {
+      const email = "hello@songsforthe.dev";
+
+      const request = createRequestWithFormData({
+        ...formDefaults,
+        email,
+      });
+
+      const response = await action({
+        request,
+        context: {},
+        params: { username },
+      });
+      expect(response.errors.email).toBeUndefined();
+      expect(response.profile.email).toBe(email);
+    });
+
+    test("email is valid", async () => {
+      const email = "hello@songsforthe.dev";
+
+      const request = createRequestWithFormData({
+        ...formDefaults,
+        email,
+      });
+
+      const response = await action({
+        request,
+        context: {},
+        params: { username },
+      });
+      expect(response.errors.email).toBeUndefined();
+      expect(response.profile.email).toBe(email);
+    });
+  });
+
+  describe("submit", () => {
+    test("update profile", async () => {
+      const email = "hello@songsforthe.dev";
+      const firstName = "First Name";
+      const lastName = "Last Name";
+
+      const request = createRequestWithFormData({
+        ...formDefaults,
+        submit: "submit",
+        email,
+        firstName,
+        lastName,
+      });
+      const response = await action({
+        request,
+        context: {},
+        params: { username },
+      });
+      expect(response.errors).toBeNull();
+      expect(updateProfileById).toHaveBeenCalledWith(id, {
+        ...parsedDataDefaults,
+        email,
+        firstName,
+        lastName,
+      });
+    });
   });
 });
