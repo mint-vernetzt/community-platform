2353a19de93237c917ec208cde47711481629c10
Peter Holló <hello@songsforthe.dev>
2022-08-11 11:18:32 +0200

    add files

---
+++ app/routes/event/create.spec.tsx
@@ -0,0 +1,28 @@
+import { action, loader } from "./create";
+
+// @ts-ignore
+const expect = global.expect as jest.Expect;
+
+const path = "/event/create";
+
+describe("context", () => {
+  test("call loader", async () => {
+    const res = await loader({
+      request: new Request(path),
+      params: {},
+      context: {},
+    });
+
+    expect(res).toBeNull();
+  });
+
+  test("call action", async () => {
+    const res = await action({
+      request: new Request(path, { method: "POST" }),
+      params: {},
+      context: {},
+    });
+
+    expect(res).toBeNull();
+  });
+});
---
+++ app/routes/event/create.tsx
@@ -0,0 +1,13 @@
+import { ActionFunction, LoaderFunction } from "remix";
+
+export const loader: LoaderFunction = async (args) => {
+  return null;
+};
+
+export const action: ActionFunction = async (args) => {
+  return null;
+};
+
+export default function Create() {
+  return <>Create</>;
+}
