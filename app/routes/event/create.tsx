1d63efac9e3eceb7bfba7538f68bd71ec8aa897d
Peter Holló <hello@songsforthe.dev>
2022-08-11 13:28:57 +0200

    implement form draft

--- app/routes/event/create.tsx
+++ app/routes/event/create.tsx
@@ -1,4 +1,4 @@
-import { ActionFunction, LoaderFunction } from "remix";
+import { ActionFunction, Form, LoaderFunction } from "remix";
 import { forbidden } from "remix-utils";
 import { getUserByRequest } from "~/auth.server";
 import { validateFeatureAccess } from "~/lib/utils/application";
@@ -20,5 +20,30 @@ export const action: ActionFunction = async (args) => {
 };
 
 export default function Create() {
-  return <>Create</>;
+  return (
+    <Form>
+      <h1>create event</h1>
+      <div className="m-2">
+        <label htmlFor="name">Name*</label>
+        <input id="name" type="text" className="mx-2 border" required />
+      </div>
+      <div className="m-2">
+        <label htmlFor="startDate">Start Date*</label>
+        <input id="startDate" type="date" className="mx-2 border" required />
+        <label htmlFor="startTime">Start Time</label>
+        <input id="starTime" type="time" className="mx-2 border" required />
+      </div>
+      <div className="m-2">
+        <label htmlFor="endDate">End Date*</label>
+        <input id="endDate" type="date" className="mx-2 border" required />
+        <label htmlFor="endTime">End Dime</label>
+        <input id="endTime" type="time" className="mx-2 border" required />
+      </div>
+      <div className="m-2">
+        <button className="btn btn-primary" type="submit">
+          Submit
+        </button>
+      </div>
+    </Form>
+  );
 }
