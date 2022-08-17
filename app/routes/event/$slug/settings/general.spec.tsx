6b0be0dc1a4088777d82b1d72fb0a4f61c401282
Peter Holló <hello@songsforthe.dev>
2022-08-17 10:45:00 +0200

    move loader tests to context

--- app/routes/event/$slug/settings/general.spec.tsx
+++ app/routes/event/$slug/settings/general.spec.tsx
@@ -46,137 +46,136 @@ describe("/event/$slug/settings/general", () => {
         expect(json.message).toBe('"slug" missing');
       }
     });
-  });
+    test("event not found", async () => {
+      expect.assertions(2);
 
-  test("event not found", async () => {
-    expect.assertions(2);
+      (prismaClient.event.findFirst as jest.Mock).mockImplementationOnce(() => {
+        return null;
+      });
+      (getUserByRequest as jest.Mock).mockImplementationOnce(() => {
+        return { id: "some-user-id" };
+      });
 
-    (prismaClient.event.findFirst as jest.Mock).mockImplementationOnce(() => {
-      return null;
-    });
-    (getUserByRequest as jest.Mock).mockImplementationOnce(() => {
-      return { id: "some-user-id" };
-    });
+      const request = new Request("");
+      try {
+        await loader({ request, context: {}, params: { slug } });
+      } catch (error) {
+        const response = error as Response;
+        expect(response.status).toBe(404);
 
-    const request = new Request("");
-    try {
-      await loader({ request, context: {}, params: { slug } });
-    } catch (error) {
-      const response = error as Response;
-      expect(response.status).toBe(404);
-
-      const json = await response.json();
-      expect(json.message).toBe("Event not found");
-    }
-  });
-
-  test("anon user", async () => {
-    expect.assertions(2);
-
-    (getUserByRequest as jest.Mock).mockImplementationOnce(() => {
-      return null;
-    });
-    (prismaClient.event.findFirst as jest.Mock).mockImplementationOnce(() => {
-      return { slug };
+        const json = await response.json();
+        expect(json.message).toBe("Event not found");
+      }
     });
 
-    try {
-      await loader({
+    test("anon user", async () => {
+      expect.assertions(2);
+
+      (getUserByRequest as jest.Mock).mockImplementationOnce(() => {
+        return null;
+      });
+      (prismaClient.event.findFirst as jest.Mock).mockImplementationOnce(() => {
+        return { slug };
+      });
+
+      try {
+        await loader({
+          request: new Request(""),
+          context: {},
+          params: { slug },
+        });
+      } catch (error) {
+        const response = error as Response;
+        expect(response.status).toBe(401);
+
+        const json = await response.json();
+        expect(json.message).toBe("Not authorized");
+      }
+    });
+
+    test("authenticated user", async () => {
+      expect.assertions(2);
+
+      (getUserByRequest as jest.Mock).mockImplementationOnce(() => {
+        return { id: "some-user-id" };
+      });
+      (prismaClient.event.findFirst as jest.Mock).mockImplementationOnce(() => {
+        return { slug };
+      });
+      (
+        prismaClient.teamMemberOfEvent.findFirst as jest.Mock
+      ).mockImplementationOnce(() => {
+        return null;
+      });
+
+      try {
+        await loader({
+          request: new Request(""),
+          context: {},
+          params: { slug },
+        });
+      } catch (error) {
+        const response = error as Response;
+        expect(response.status).toBe(401);
+
+        const json = await response.json();
+        expect(json.message).toBe("Not authorized");
+      }
+    });
+
+    test("not privileged user", async () => {
+      expect.assertions(2);
+
+      (getUserByRequest as jest.Mock).mockImplementationOnce(() => {
+        return { id: "some-user-id" };
+      });
+      (prismaClient.event.findFirst as jest.Mock).mockImplementationOnce(() => {
+        return { slug };
+      });
+      (
+        prismaClient.teamMemberOfEvent.findFirst as jest.Mock
+      ).mockImplementationOnce(() => {
+        return null;
+      });
+
+      try {
+        await loader({
+          request: new Request(""),
+          context: {},
+          params: { slug },
+        });
+      } catch (error) {
+        const response = error as Response;
+        expect(response.status).toBe(401);
+
+        const json = await response.json();
+        expect(json.message).toBe("Not authorized");
+      }
+    });
+
+    test("privileged user", async () => {
+      (getUserByRequest as jest.Mock).mockImplementationOnce(() => {
+        return { id: "some-user-id" };
+      });
+      (prismaClient.event.findFirst as jest.Mock).mockImplementationOnce(() => {
+        return { slug };
+      });
+      (
+        prismaClient.teamMemberOfEvent.findFirst as jest.Mock
+      ).mockImplementationOnce(() => {
+        return { isPrivileged: true };
+      });
+
+      const response = await loader({
         request: new Request(""),
         context: {},
         params: { slug },
       });
-    } catch (error) {
-      const response = error as Response;
-      expect(response.status).toBe(401);
-
-      const json = await response.json();
-      expect(json.message).toBe("Not authorized");
-    }
-  });
-
-  test("authenticated user", async () => {
-    expect.assertions(2);
-
-    (getUserByRequest as jest.Mock).mockImplementationOnce(() => {
-      return { id: "some-user-id" };
-    });
-    (prismaClient.event.findFirst as jest.Mock).mockImplementationOnce(() => {
-      return { slug };
-    });
-    (
-      prismaClient.teamMemberOfEvent.findFirst as jest.Mock
-    ).mockImplementationOnce(() => {
-      return null;
+      expect(response.event.slug).toBe(slug);
     });
 
-    try {
-      await loader({
-        request: new Request(""),
-        context: {},
-        params: { slug },
-      });
-    } catch (error) {
-      const response = error as Response;
-      expect(response.status).toBe(401);
-
-      const json = await response.json();
-      expect(json.message).toBe("Not authorized");
-    }
-  });
-
-  test("not privileged user", async () => {
-    expect.assertions(2);
-
-    (getUserByRequest as jest.Mock).mockImplementationOnce(() => {
-      return { id: "some-user-id" };
+    afterAll(() => {
+      delete process.env.FEATURES;
     });
-    (prismaClient.event.findFirst as jest.Mock).mockImplementationOnce(() => {
-      return { slug };
-    });
-    (
-      prismaClient.teamMemberOfEvent.findFirst as jest.Mock
-    ).mockImplementationOnce(() => {
-      return null;
-    });
-
-    try {
-      await loader({
-        request: new Request(""),
-        context: {},
-        params: { slug },
-      });
-    } catch (error) {
-      const response = error as Response;
-      expect(response.status).toBe(401);
-
-      const json = await response.json();
-      expect(json.message).toBe("Not authorized");
-    }
-  });
-
-  test("privileged user", async () => {
-    (getUserByRequest as jest.Mock).mockImplementationOnce(() => {
-      return { id: "some-user-id" };
-    });
-    (prismaClient.event.findFirst as jest.Mock).mockImplementationOnce(() => {
-      return { slug };
-    });
-    (
-      prismaClient.teamMemberOfEvent.findFirst as jest.Mock
-    ).mockImplementationOnce(() => {
-      return { isPrivileged: true };
-    });
-
-    const response = await loader({
-      request: new Request(""),
-      context: {},
-      params: { slug },
-    });
-    expect(response.event.slug).toBe(slug);
-  });
-
-  afterAll(() => {
-    delete process.env.FEATURES;
   });
 });
