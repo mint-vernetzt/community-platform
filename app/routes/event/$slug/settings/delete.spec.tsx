cf818c2bec44c637708813fa1e192eaa68331fe4
Peter Holló <hello@songsforthe.dev>
2022-08-22 22:19:35 +0200

    implement mutation (next: deletion)

--- app/routes/event/$slug/settings/delete.spec.tsx
+++ app/routes/event/$slug/settings/delete.spec.tsx
@@ -1,7 +1,8 @@
 import { User } from "@supabase/supabase-js";
 import * as authServerModule from "~/auth.server";
+import { createRequestWithFormData } from "~/lib/utils/tests";
 import { prismaClient } from "~/prisma";
-import { loader } from "./delete";
+import { action, loader } from "./delete";
 
 // @ts-ignore
 const expect = global.expect as jest.Expect;
@@ -29,7 +30,7 @@ jest.mock("~/prisma", () => {
 
 const slug = "slug-test";
 
-describe("/event/$slug/settings/general", () => {
+describe("/event/$slug/settings/delete", () => {
   describe("loader", () => {
     beforeAll(() => {
       process.env.FEATURES = "events";
@@ -152,6 +153,7 @@ describe("/event/$slug/settings/general", () => {
 
       (prismaClient.event.findFirst as jest.Mock).mockImplementationOnce(() => {
         return {
+          id: "some-event-id",
           slug,
         };
       });
@@ -166,7 +168,223 @@ describe("/event/$slug/settings/general", () => {
         context: {},
         params: { slug },
       });
-      expect(response).toBeNull();
+      expect(response.userId).toBe("some-user-id");
+      expect(response.eventId).toBe("some-event-id");
+    });
+
+    afterAll(() => {
+      delete process.env.FEATURES;
+    });
+  });
+
+  describe("action", () => {
+    beforeAll(() => {
+      process.env.FEATURES = "events";
+    });
+
+    test("no params", async () => {
+      const request = createRequestWithFormData({});
+
+      expect.assertions(2);
+
+      try {
+        await action({ request, context: {}, params: {} });
+      } catch (error) {
+        const response = error as Response;
+        expect(response.status).toBe(400);
+
+        const json = await response.json();
+        expect(json.message).toBe('"slug" missing');
+      }
+    });
+
+    test("event not found", async () => {
+      const request = createRequestWithFormData({ userId: "some-user-id" });
+
+      expect.assertions(2);
+
+      (prismaClient.event.findFirst as jest.Mock).mockResolvedValue(null);
+
+      getUserByRequest.mockResolvedValue({ id: "some-user-id" } as User);
+
+      try {
+        await action({ request, context: {}, params: { slug } });
+      } catch (error) {
+        const response = error as Response;
+        expect(response.status).toBe(404);
+
+        const json = await response.json();
+        expect(json.message).toBe("Event not found");
+      }
+    });
+
+    test("anon user", async () => {
+      const request = createRequestWithFormData({});
+
+      expect.assertions(2);
+
+      getUserByRequest.mockResolvedValue(null);
+
+      try {
+        await action({
+          request,
+          context: {},
+          params: { slug },
+        });
+      } catch (error) {
+        const response = error as Response;
+        expect(response.status).toBe(401);
+
+        const json = await response.json();
+        expect(json.message).toBe("No session or session user found");
+      }
+    });
+
+    test("authenticated user", async () => {
+      const request = createRequestWithFormData({ userId: "some-user-id" });
+
+      expect.assertions(2);
+
+      getUserByRequest.mockResolvedValue({ id: "some-user-id" } as User);
+
+      (prismaClient.event.findFirst as jest.Mock).mockImplementationOnce(() => {
+        return { slug };
+      });
+      (
+        prismaClient.teamMemberOfEvent.findFirst as jest.Mock
+      ).mockImplementationOnce(() => {
+        return null;
+      });
+
+      try {
+        await action({
+          request,
+          context: {},
+          params: { slug },
+        });
+      } catch (error) {
+        const response = error as Response;
+        expect(response.status).toBe(401);
+
+        const json = await response.json();
+        expect(json.message).toBe("Not privileged");
+      }
+    });
+
+    test("not privileged user", async () => {
+      const request = createRequestWithFormData({ userId: "some-user-id" });
+
+      expect.assertions(2);
+
+      getUserByRequest.mockResolvedValue({ id: "some-user-id" } as User);
+
+      (prismaClient.event.findFirst as jest.Mock).mockImplementationOnce(() => {
+        return { slug };
+      });
+      (
+        prismaClient.teamMemberOfEvent.findFirst as jest.Mock
+      ).mockImplementationOnce(() => {
+        return null;
+      });
+
+      try {
+        await action({
+          request,
+          context: {},
+          params: { slug },
+        });
+      } catch (error) {
+        const response = error as Response;
+        expect(response.status).toBe(401);
+
+        const json = await response.json();
+        expect(json.message).toBe("Not privileged");
+      }
+    });
+
+    test("different user id", async () => {
+      const request = createRequestWithFormData({ userId: "some-user-id" });
+
+      expect.assertions(2);
+
+      getUserByRequest.mockResolvedValue({ id: "another-user-id" } as User);
+
+      try {
+        await action({
+          request,
+          context: {},
+          params: { slug },
+        });
+      } catch (error) {
+        const response = error as Response;
+        expect(response.status).toBe(401);
+
+        const json = await response.json();
+        expect(json.message).toBe("Identity check failed");
+      }
+    });
+
+    test("different event id", async () => {
+      expect.assertions(2);
+
+      const request = createRequestWithFormData({
+        userId: "some-user-id",
+        eventId: "some-event-id",
+        eventName: "Some event name",
+      });
+
+      getUserByRequest.mockResolvedValue({ id: "some-user-id" } as User);
+      (prismaClient.event.findFirst as jest.Mock).mockImplementationOnce(() => {
+        return { id: "another-event-id", name: "Some other event name", slug };
+      });
+      (
+        prismaClient.teamMemberOfEvent.findFirst as jest.Mock
+      ).mockImplementationOnce(() => {
+        return { isPrivileged: true };
+      });
+
+      try {
+        await action({
+          request,
+          context: {},
+          params: { slug },
+        });
+      } catch (error) {
+        const response = error as Response;
+        expect(response.status).toBe(400);
+
+        const json = await response.json();
+        expect(json.message).toBe("Id nicht korrekt");
+      }
+    });
+
+    test("different event name", async () => {
+      const request = createRequestWithFormData({
+        userId: "some-user-id",
+        eventId: "some-event-id",
+        eventName: "Some event name",
+      });
+
+      getUserByRequest.mockResolvedValue({ id: "some-user-id" } as User);
+      (prismaClient.event.findFirst as jest.Mock).mockImplementationOnce(() => {
+        return { id: "some-event-id", name: "Some other event name", slug };
+      });
+      (
+        prismaClient.teamMemberOfEvent.findFirst as jest.Mock
+      ).mockImplementationOnce(() => {
+        return { isPrivileged: true };
+      });
+
+      const response = await action({
+        request,
+        context: {},
+        params: { slug },
+      });
+
+      expect(response.errors.eventName).toBeDefined();
+      expect(response.errors.eventName[0]).toBe(
+        "Der Name der Veranstaltung ist nicht korrekt"
+      );
     });
 
     afterAll(() => {
--- app/routes/event/$slug/settings/delete.tsx
+++ app/routes/event/$slug/settings/delete.tsx
@@ -1,9 +1,21 @@
-import { LoaderFunction } from "remix";
+import { ActionFunction, LoaderFunction } from "remix";
+import { InputError, makeDomainFunction } from "remix-domains";
+import { performMutation } from "remix-forms";
+import { badRequest } from "remix-utils";
+import { z } from "zod";
 import { getUserByRequestOrThrow } from "~/auth.server";
 import { checkFeatureAbilitiesOrThrow } from "~/lib/utils/application";
 import { getParamValueOrThrow } from "~/lib/utils/routes";
 import { getEventBySlugOrThrow } from "../utils.server";
-import { checkOwnershipOrThrow } from "./utils.server";
+import { checkIdentityOrThrow, checkOwnershipOrThrow } from "./utils.server";
+
+const schema = z.object({
+  userId: z.string().optional(),
+  eventId: z.string().optional(),
+  eventName: z.string().optional(),
+});
+
+const environmentSchema = z.object({ id: z.string(), name: z.string() });
 
 export const loader: LoaderFunction = async (args) => {
   const { request, params } = args;
@@ -17,7 +29,61 @@ export const loader: LoaderFunction = async (args) => {
 
   await checkOwnershipOrThrow(event, currentUser);
 
-  return null;
+  return { userId: currentUser.id, eventId: event.id };
+};
+
+const mutation = makeDomainFunction(
+  schema,
+  environmentSchema
+)(async (values, environment) => {
+  if (values.eventId !== environment.id) {
+    throw new Error("Id nicht korrekt");
+  }
+  if (values.eventName !== environment.name) {
+    throw new InputError(
+      "Der Name der Veranstaltung ist nicht korrekt",
+      "eventName"
+    );
+  }
+  try {
+    //
+  } catch (error) {
+    throw "Die Veranstaltung konnte nicht gelöscht werden.";
+  }
+});
+
+export const action: ActionFunction = async (args) => {
+  const { request, params } = args;
+
+  await checkFeatureAbilitiesOrThrow(request, "events");
+
+  const slug = getParamValueOrThrow(params, "slug");
+
+  const currentUser = await getUserByRequestOrThrow(request);
+
+  await checkIdentityOrThrow(request, currentUser);
+
+  const event = await getEventBySlugOrThrow(slug);
+
+  await checkOwnershipOrThrow(event, currentUser);
+
+  const result = await performMutation({
+    request,
+    schema,
+    mutation,
+    environment: { id: event.id, name: event.name },
+  });
+
+  if (result.success === false) {
+    if (
+      result.errors._global !== undefined &&
+      result.errors._global.includes("Id nicht korrekt")
+    ) {
+      throw badRequest({ message: "Id nicht korrekt" });
+    }
+  }
+
+  return result;
 };
 
 function Delete() {}
