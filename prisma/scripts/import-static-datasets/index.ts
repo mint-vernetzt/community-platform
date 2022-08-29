85f08d3f7bc92ccb4bea3dbcae7c881914faaf27
nac62116 <56918308+nac62116@users.noreply.github.com>
2022-08-29 18:27:23 +0200

    wip: use existing code to create dataset from cms

--- prisma/scripts/import-static-datasets/index.ts
+++ prisma/scripts/import-static-datasets/index.ts
@@ -1,71 +1,19 @@
-import { prismaClient } from "../../../app/prisma";
 import organizationTypes from "./data/organizationTypes.json";
 import offers from "./data/offers.json";
 import focuses from "./data/focuses.json";
-import {
-  dataToBeUpdated,
-  entriesOnlyExistingOnDatabase,
-  filterMissingData,
-  GenericEntry,
-} from "./src/utils";
+import { createDataset, GenericEntry, TableName } from "../utils";
 
-type TableName = "offer" | "organizationType" | "focus";
-
-async function createDataset(datasets: GenericEntry[], tableName: TableName) {
-  console.log(`create entries for ${tableName}`);
-
-  // @ts-ignore
-  const existingEntries = await prismaClient[tableName].findMany();
-  const missingData = filterMissingData(datasets, existingEntries);
-
-  if (missingData.length > 0) {
-    await prismaClient[tableName].createMany({ data: missingData });
-    console.log("added: ", missingData);
-  }
-
-  const entriesToUpdate = dataToBeUpdated(datasets, existingEntries);
-  if (entriesToUpdate.length > 0) {
-    entriesToUpdate.forEach(async ({ id, ...rest }) => {
-      // @ts-ignore
-      await prismaClient[tableName].update({
-        where: {
-          id,
-        },
-        data: {
-          ...rest,
-        },
-      });
-    });
-
-    console.log("updated: ", entriesToUpdate);
-  }
-
-  const unknownEntries = entriesOnlyExistingOnDatabase(
-    datasets,
-    existingEntries
-  );
-  if (unknownEntries.length > 0) {
-    console.log(`warning, unknown "${tableName}s" in db: `, unknownEntries);
-  }
-
-  if (missingData.length === 0 && entriesToUpdate.length === 0) {
-    console.log(`table ${tableName} is up to date`);
-  }
-}
-
-const datasets = [
+const staticDatasets: Array<{ tableName: TableName; data: GenericEntry[] }> = [
   { tableName: "offer", data: offers },
   { tableName: "organizationType", data: organizationTypes },
   { tableName: "focus", data: focuses },
 ];
 
 Promise.all(
-  datasets.map(
+  staticDatasets.map(
     (dataset) =>
       new Promise((resolve) => {
-        createDataset(dataset.data, dataset.tableName as TableName).then(
-          resolve
-        );
+        createDataset(dataset.data, dataset.tableName).then(resolve);
       })
   )
 )
--- prisma/scripts/utils.ts
+++ prisma/scripts/utils.ts
@@ -1,6 +1,9 @@
-import type { Offer, OrganizationType, Focus } from "@prisma/client";
+import type { Offer, OrganizationType, Focus, Tag } from "@prisma/client";
+import { prismaClient } from "~/prisma";
 
-export type GenericEntry = Offer | OrganizationType | Focus;
+export type GenericEntry = Offer | OrganizationType | Focus | Tag;
+
+export type TableName = "offer" | "organizationType" | "focus" | "tag";
 
 type Lookup = {
   [keyof: string]: GenericEntry;
@@ -60,4 +63,50 @@ export function entriesOnlyExistingOnDatabase(
   return existingEntries.filter(
     (existing) => wantedLookup[existing.id] === undefined
   );
+}
+
+export async function createDataset(
+  datasets: GenericEntry[],
+  tableName: TableName
+) {
+  console.log(`create entries for ${tableName}`);
+
+  // @ts-ignore
+  const existingEntries = await prismaClient[tableName].findMany();
+  const missingData = filterMissingData(datasets, existingEntries);
+
+  if (missingData.length > 0) {
+    // WIP
+    await prismaClient[tableName].createMany({ data: missingData });
+    console.log("added: ", missingData);
+  }
+
+  const entriesToUpdate = dataToBeUpdated(datasets, existingEntries);
+  if (entriesToUpdate.length > 0) {
+    entriesToUpdate.forEach(async ({ id, ...rest }) => {
+      // @ts-ignore
+      await prismaClient[tableName].update({
+        where: {
+          id,
+        },
+        data: {
+          ...rest,
+        },
+      });
+    });
+
+    console.log("updated: ", entriesToUpdate);
+  }
+
+  const unknownEntries = entriesOnlyExistingOnDatabase(
+    datasets,
+    existingEntries
+  );
+  if (unknownEntries.length > 0) {
+    console.log(`warning, unknown "${tableName}s" in db: `, unknownEntries);
+  }
+
+  if (missingData.length === 0 && entriesToUpdate.length === 0) {
+    console.log(`table ${tableName} is up to date`);
+  }
 }
