16ae0adc39f0bea63d184da9eb7a37f04dd8a10c
nac62116 <56918308+nac62116@users.noreply.github.com>
2022-08-31 10:48:05 +0200

    Made URL option required without default value

--- prisma/scripts/generate-graphql-datasets/index.ts
+++ prisma/scripts/generate-graphql-datasets/index.ts
@@ -9,11 +9,7 @@ program
     "CLI tool to generate .json datasets from a given graphQL url and query options. The datasets are stored inside /import-datasets/data/."
   )
   .version("1.0.0")
-  .option(
-    "-u, --url <char>",
-    "The graphQL url",
-    "https://cms.mint-vernetzt.de/wp/graphql"
-  )
+  .requiredOption("-u, --url <char>", "The graphQL url")
   .requiredOption(
     "-n, --name <char>",
     "The name of the object to query for. This also specifies the name of the .json dataset"
