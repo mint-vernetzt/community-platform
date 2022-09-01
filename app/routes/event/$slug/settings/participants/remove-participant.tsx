ce08ab76941279c64a2643887db8606bf769d00b
Peter Holló <hello@songsforthe.dev>
2022-09-01 13:36:05 +0200

    implement removing participant

---
+++ app/routes/event/$slug/settings/participants/remove-participant.tsx
@@ -0,0 +1,38 @@
+import { ActionFunction } from "remix";
+import { makeDomainFunction } from "remix-domains";
+import { performMutation } from "remix-forms";
+import { z } from "zod";
+import { getUserByRequestOrThrow } from "~/auth.server";
+import { checkFeatureAbilitiesOrThrow } from "~/lib/utils/application";
+import { checkSameEventOrThrow, getEventByIdOrThrow } from "../../utils.server";
+import { checkIdentityOrThrow, checkOwnershipOrThrow } from "../utils.server";
+import { disconnectParticipantFromEvent } from "./utils.server";
+
+const schema = z.object({
+  userId: z.string(),
+  eventId: z.string(),
+  profileId: z.string(),
+});
+
+export const removeParticipantSchema = schema;
+
+const mutation = makeDomainFunction(schema)(async (values) => {
+  return values;
+});
+
+export const action: ActionFunction = async (args) => {
+  const { request } = args;
+  await checkFeatureAbilitiesOrThrow(request, "events");
+  const currentUser = await getUserByRequestOrThrow(request);
+  await checkIdentityOrThrow(request, currentUser);
+
+  const result = await performMutation({ request, schema, mutation });
+
+  if (result.success === true) {
+    const event = await getEventByIdOrThrow(result.data.eventId);
+    await checkOwnershipOrThrow(event, currentUser);
+    await checkSameEventOrThrow(request, event.id);
+    await disconnectParticipantFromEvent(event.id, result.data.profileId);
+  }
+  return result;
+};
