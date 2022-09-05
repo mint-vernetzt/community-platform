f298390955aba3c9202ad980fc20c690c148bc50
nac62116 <56918308+nac62116@users.noreply.github.com>
2022-09-05 17:31:33 +0200

    fix: domainname.tld/a-b should be valid (#373)

--- app/lib/utils/yup.ts
+++ app/lib/utils/yup.ts
@@ -22,7 +22,7 @@ const phoneValidation = {
 
 const websiteValidation = {
   match:
-    /(https?:\/\/)?(www\.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$|^$/,
+    /(https?:\/\/)?(www\.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9-#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$|^$/,
   error: "Deine Eingabe entspricht nicht dem Format einer Website URL.",
 };
 
