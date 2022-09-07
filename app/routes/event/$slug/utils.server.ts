e1e15b0656e5e65b996cbe60f43eea954a66d6eb
nac62116 <56918308+nac62116@users.noreply.github.com>
2022-09-07 16:06:47 +0200

    wip: combining RECURSIVE sql query with JOIN sql query

--- app/routes/event/$slug/utils.server.ts
+++ app/routes/event/$slug/utils.server.ts
@@ -202,59 +202,128 @@ export async function checkSameEventOrThrow(request: Request, eventId: string) {
   }
 }
 
-async function getEventWithChildEventsById(id: string) {
-  const result = prismaClient.event.findFirst({
-    where: { id },
-    include: {
-      childEvents: {
-        include: {
-          participants: true,
-          speakers: true,
-          responsibleOrganizations: true,
-        },
-      },
-    },
-  });
-  return result;
-}
+export async function getFullDepthParticipants(id: string) {
+  // TODO: Raw query to get participants, speakers and responsibleOrganizations of this and all child events
+  try {
+    // Get event and all child events of arbitrary depth with raw query
+    // const result = await prismaClient.$queryRaw`
+    //   WITH RECURSIVE get_full_depth AS (
+    //       SELECT id, parent_event_id, name
+    //       FROM "events"
+    //       WHERE id = ${id}
+    //     UNION
+    //       SELECT "events".id, "events".parent_event_id, "events".name
+    //       FROM "events"
+    //         JOIN get_full_depth
+    //         ON "events".parent_event_id = get_full_depth.id
+    //   )
+    //   SELECT *
+    //   FROM get_full_depth;`;
 
-export async function getFullDepthRelations(
-  event: Pick<
-    Awaited<ReturnType<typeof getEventBySlugOrThrow>>,
-    "speakers" | "participants" | "responsibleOrganizations" | "id"
-  >,
-  fullDepthRelationKeys: Array<
-    keyof Pick<
-      typeof event,
-      "speakers" | "participants" | "responsibleOrganizations"
-    >
-  >
-) {
-  let fullDepthRelations = {};
-  fullDepthRelationKeys.map((key) => {
-    // TODO: Fix type issue
-    // @ts-ignore
-    fullDepthRelations[key] = event[key];
+    // Get participants of event with raw query IN statement
+    // const result = await prismaClient.$queryRaw`
+    //   SELECT username, first_name, last_name, avatar, academic_title, position
+    //   FROM "profiles"
+    //   WHERE id IN (
+    //     SELECT profile_id
+    //     FROM "participants_of_events"
+    //     WHERE event_id = ${id}
+    //   );`;
+
+    // Get participants of event with raw query JOIN statement
+    // const result = await prismaClient.$queryRaw`
+    //   SELECT event_id, parent_event_id, username, first_name, last_name, avatar, academic_title, position
+    //   FROM "profiles"
+    //     JOIN "participants_of_events"
+    //     ON "profiles".id = "participants_of_events".profile_id
+    //     JOIN "events"
+    //     ON ${id} = "events".id
+    //   WHERE "participants_of_events".event_id = ${id};`;
+
+    // TODO: Combining RECURSIVE with JOIN query
+    const result = await prismaClient.$queryRaw`
+      WITH RECURSIVE get_full_depth AS (
+          SELECT event_id, parent_event_id, username, first_name, last_name, avatar, academic_title, position
+          FROM "profiles"
+            JOIN "participants_of_events"
+            ON "profiles".id = "participants_of_events".profile_id
+            JOIN "events"
+            ON ${id} = "events".id
+          WHERE "participants_of_events".event_id = ${id}
+        UNION
+        SELECT "events".id, "events".parent_event_id
+          FROM "events"
+            JOIN get_full_depth 
+            ON "events".parent_event_id = get_full_depth.id
+      )
+      SELECT *
+      FROM get_full_depth;`;
+
+    console.log(result);
+
+    return result as Pick<
+      Event,
+      "id" | "parentEventId" | "name" | "slug" | "published"
+    >[];
+  } catch (e) {
+    console.error(e);
     return null;
-  });
-
-  // TODO: recursive function
-
-  const eventWithChilds = await getEventWithChildEventsById(event.id);
-
-  while (eventWithChilds !== null && eventWithChilds.childEvents.length !== 0) {
-    for (let childEvent of eventWithChilds.childEvents) {
-      fullDepthRelationKeys.map((key) => {
-        // TODO: Fix type issue
-        // @ts-ignore
-        fullDepthRelations[key] = [
-          // TODO: Fix type issue
-          // @ts-ignore
-          ...fullDepthRelations[key],
-          ...childEvent[key],
-        ];
-        return null;
-      });
-    }
   }
 }
+
+// async function getEventWithChildEventsById(id: string) {
+//   const result = prismaClient.event.findFirst({
+//     where: { id },
+//     include: {
+//       childEvents: {
+//         include: {
+//           participants: true,
+//           speakers: true,
+//           responsibleOrganizations: true,
+//         },
+//       },
+//     },
+//   });
+//   return result;
+// }
+
+// export async function getFullDepthRelations(
+//   event: Pick<
+//     Awaited<ReturnType<typeof getEventBySlugOrThrow>>,
+//     "speakers" | "participants" | "responsibleOrganizations" | "id"
+//   >,
+//   fullDepthRelationKeys: Array<
+//     keyof Pick<
+//       typeof event,
+//       "speakers" | "participants" | "responsibleOrganizations"
+//     >
+//   >
+// ) {
+//   let fullDepthRelations = {};
+//   fullDepthRelationKeys.map((key) => {
+//     // TODO: Fix type issue
+//     // @ts-ignore
+//     fullDepthRelations[key] = event[key];
+//     return null;
+//   });
+
+//   // TODO: recursive function
+
+//   const eventWithChilds = await getEventWithChildEventsById(event.id);
+
+//   while (eventWithChilds !== null && eventWithChilds.childEvents.length !== 0) {
+//     for (let childEvent of eventWithChilds.childEvents) {
+//       fullDepthRelationKeys.map((key) => {
+//         // TODO: Fix type issue
+//         // @ts-ignore
+//         fullDepthRelations[key] = [
+//           // TODO: Fix type issue
+//           // @ts-ignore
+//           ...fullDepthRelations[key],
+//           ...childEvent[key],
+//         ];
+//         return null;
+//       });
+//     }
+//   }
+// }
