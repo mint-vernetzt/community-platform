d43f0b42308b770ec56060b3a310f3a2a9aef6e6
Sirko Stenz <sirko.stenz@digitalnoise.de>
2022-09-14 23:11:04 -0500

    styling image cropper

--- app/components/ImageCropper/ImageCropper.tsx
+++ app/components/ImageCropper/ImageCropper.tsx
@@ -234,29 +234,87 @@ function ImageCropper(props: ImageCropperProps) {
 
   return (
     <div className="flex flex-col items-center">
-      <h2 className="text-center font-bolder">{headline}</h2>
-      <div>
-        {!imgSrc && props.children}
+      <h2 className="text-center font-bold">{headline}</h2>
+      <div className="flex items-center">
+        <div className="relative">
+          {image && !completedCrop && (
+            <Form
+              action={DELETE_URL}
+              method="post"
+              reloadDocument
+              schema={schema}
+              hiddenFields={[
+                "subject",
+                "slug",
+                "uploadKey",
+                "csrf",
+                "redirect",
+              ]}
+              values={{
+                subject: props.subject,
+                slug: props.slug,
+                uploadKey: props.uploadKey,
+                csrf: props.csrfToken,
+                redirect: props.redirect,
+              }}
+            >
+              {({ Field }) => (
+                <>
+                  <Field name="subject" />
+                  <Field name="slug" />
+                  <Field name="csrf" />
+                  <Field name="uploadKey" />
+                  <Field name="redirect" />
+                  <button
+                    className="bg-transparent w-8 h-8 p-0 border border-transparent absolute -top-4 -right-4 border-0"
+                    type="submit"
+                    disabled={isSaving}
+                    onClick={(e) => {
+                      if (!confirm("Bild wirklich entfernen?")) {
+                        e.preventDefault();
+                      }
+                    }}
+                  >
+                    <svg
+                      width="32"
+                      height="32"
+                      viewBox="0 0 32 32"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <circle cx="16" cy="16" r="16" fill="#EE7775" />
+                      <path
+                        d="M9.29196 9.29208C9.38485 9.19895 9.4952 9.12507 9.61669 9.07465C9.73818 9.02424 9.86842 8.99829 9.99996 8.99829C10.1315 8.99829 10.2617 9.02424 10.3832 9.07465C10.5047 9.12507 10.6151 9.19895 10.708 9.29208L16 14.5861L21.292 9.29208C21.3849 9.1991 21.4953 9.12535 21.6168 9.07503C21.7383 9.02471 21.8685 8.99881 22 8.99881C22.1314 8.99881 22.2616 9.02471 22.3831 9.07503C22.5046 9.12535 22.615 9.1991 22.708 9.29208C22.8009 9.38505 22.8747 9.49543 22.925 9.61691C22.9753 9.73839 23.0012 9.86859 23.0012 10.0001C23.0012 10.1316 22.9753 10.2618 22.925 10.3832C22.8747 10.5047 22.8009 10.6151 22.708 10.7081L17.414 16.0001L22.708 21.2921C22.8009 21.3851 22.8747 21.4954 22.925 21.6169C22.9753 21.7384 23.0012 21.8686 23.0012 22.0001C23.0012 22.1316 22.9753 22.2618 22.925 22.3832C22.8747 22.5047 22.8009 22.6151 22.708 22.7081C22.615 22.8011 22.5046 22.8748 22.3831 22.9251C22.2616 22.9754 22.1314 23.0013 22 23.0013C21.8685 23.0013 21.7383 22.9754 21.6168 22.9251C21.4953 22.8748 21.3849 22.8011 21.292 22.7081L16 17.4141L10.708 22.7081C10.615 22.8011 10.5046 22.8748 10.3831 22.9251C10.2616 22.9754 10.1314 23.0013 9.99996 23.0013C9.86847 23.0013 9.73827 22.9754 9.61679 22.9251C9.49531 22.8748 9.38493 22.8011 9.29196 22.7081C9.19898 22.6151 9.12523 22.5047 9.07491 22.3832C9.02459 22.2618 8.99869 22.1316 8.99869 22.0001C8.99869 21.8686 9.02459 21.7384 9.07491 21.6169C9.12523 21.4954 9.19898 21.3851 9.29196 21.2921L14.586 16.0001L9.29196 10.7081C9.19883 10.6152 9.12494 10.5048 9.07453 10.3833C9.02412 10.2619 8.99817 10.1316 8.99817 10.0001C8.99817 9.86854 9.02412 9.7383 9.07453 9.61681C9.12494 9.49532 9.19883 9.38497 9.29196 9.29208Z"
+                        fill="#FCFCFD"
+                      />
+                    </svg>
+                  </button>
+                </>
+              )}
+            </Form>
+          )}
+          {!imgSrc && props.children}
 
-        {Boolean(imgSrc) && (
-          <ReactCrop
-            crop={crop}
-            onChange={(_, percentCrop) => setCrop(percentCrop)}
-            onComplete={(c) => setCompletedCrop(c)}
-            aspect={aspect ?? undefined}
-            minWidth={minCropWidth}
-            minHeight={minCropHeight}
-            style={{ maxHeight: "288px" }}
-          >
-            <img
-              ref={imgRef}
-              alt="Crop Preview"
-              src={imgSrc}
-              style={{ transform: `scale(${scale})` }}
-              onLoad={onImageLoad}
-            />
-          </ReactCrop>
-        )}
+          {Boolean(imgSrc) && (
+            <ReactCrop
+              crop={crop}
+              onChange={(_, percentCrop) => setCrop(percentCrop)}
+              onComplete={(c) => setCompletedCrop(c)}
+              aspect={aspect ?? undefined}
+              minWidth={minCropWidth}
+              minHeight={minCropHeight}
+              style={{ maxHeight: "288px" }}
+            >
+              <img
+                ref={imgRef}
+                alt="Crop Preview"
+                src={imgSrc}
+                style={{ transform: `scale(${scale})` }}
+                onLoad={onImageLoad}
+              />
+            </ReactCrop>
+          )}
+        </div>
       </div>
       <div>
         {Boolean(completedCrop) && (
@@ -271,22 +329,34 @@ function ImageCropper(props: ImageCropperProps) {
           />
         )}
       </div>
-      <div className="Crop-Controls flex flex-col items-center">
+      <div className="Crop-Controls flex flex-col items-center w-full">
         <InputFile
           id={`${id}-file`}
           onSelectFile={onSelectFile}
           hasImage={image !== undefined}
         />
         {imgSrc && (
-          <div className="flex mb-5">
-            <button
-              id="scaleUp"
-              className="btn btn-outline btn-primary mr-1"
-              onClick={handleZoomClick}
-            >
-              +
-            </button>
-            <div className="w-[250px] p-5">
+          <div className="flex items-center w-full">
+            <div className="flex-auto w-1/2 text-center md:w-[calc(25%+1rem)] flex justify-center md:justify-end">
+              <button
+                id="scaleUp"
+                className="bg-white border border-primary h-8 w-8 flex items-center justify-center rounded-md hover:bg-primary text-primary hover:text-white"
+                onClick={handleZoomClick}
+              >
+                <svg
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M6 0C6.41421 0 6.75 0.335786 6.75 0.75V5.25H11.25C11.6642 5.25 12 5.58579 12 6C12 6.41421 11.6642 6.75 11.25 6.75H6.75V11.25C6.75 11.6642 6.41421 12 6 12C5.58579 12 5.25 11.6642 5.25 11.25V6.75H0.75C0.335786 6.75 0 6.41421 0 6C0 5.58579 0.335786 5.25 0.75 5.25H5.25V0.75C5.25 0.335786 5.58579 0 6 0Z"
+                    fill="currentColor"
+                  />
+                </svg>
+              </button>
+            </div>
+            <div className="w-[250px] py-2 px-4 hidden md:block md:w-[calc(50%-2rem)] ">
               <Slider
                 min={0.1}
                 max={DEFAULT_SCALE * 2}
@@ -295,59 +365,32 @@ function ImageCropper(props: ImageCropperProps) {
                 onChange={(v) => setScale(v as number)}
               />
             </div>
-            <button
-              id="scaleDown"
-              className="btn btn-outline btn-primary ml-1"
-              onClick={handleZoomClick}
-            >
-              -
-            </button>
+            <div className="flex-auto w-1/2 text-center md:w-[calc(25%+1rem)] flex justify-center md:justify-start">
+              <button
+                id="scaleDown"
+                className="bg-white border border-primary h-8 w-8 flex items-center justify-center rounded-md hover:bg-primary text-primary hover:text-white"
+                onClick={handleZoomClick}
+              >
+                <svg
+                  width="11"
+                  height="3"
+                  viewBox="0 0 11 3"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M0.398682 1.41893C0.398682 0.898206 0.68838 0.476074 1.04574 0.476074H10.1046C10.4619 0.476074 10.7516 0.898206 10.7516 1.41893C10.7516 1.93966 10.4619 2.36179 10.1046 2.36179H1.04574C0.68838 2.36179 0.398682 1.93966 0.398682 1.41893Z"
+                    fill="currentColor"
+                  />
+                </svg>
+              </button>
+            </div>
           </div>
         )}
-
-        {image && !completedCrop && (
-          <Form
-            action={DELETE_URL}
-            method="post"
-            reloadDocument
-            schema={schema}
-            hiddenFields={["subject", "slug", "uploadKey", "csrf", "redirect"]}
-            values={{
-              subject: props.subject,
-              slug: props.slug,
-              uploadKey: props.uploadKey,
-              csrf: props.csrfToken,
-              redirect: props.redirect,
-            }}
-          >
-            {({ Field }) => (
-              <>
-                <Field name="subject" />
-                <Field name="slug" />
-                <Field name="csrf" />
-                <Field name="uploadKey" />
-                <Field name="redirect" />
-                <button
-                  className="btn btn-link"
-                  type="submit"
-                  disabled={isSaving}
-                  onClick={(e) => {
-                    if (!confirm("Bild wirklich entfernen?")) {
-                      e.preventDefault();
-                    }
-                  }}
-                >
-                  Bild entfernen
-                </button>
-              </>
-            )}
-          </Form>
-        )}
       </div>
-      <div className="flex justify-between w-full items-stretch ">
+      <div className="grid grid-cols-2 gap-x-8 w-full mt-6">
         <label
           htmlFor={id}
-          className="btn btn-link p-5"
+          className="btn btn-small btn-link p-5"
           onClick={() => {
             reset();
 
@@ -359,7 +402,7 @@ function ImageCropper(props: ImageCropperProps) {
 
         <button
           onClick={handleSave}
-          className="btn btn-primary"
+          className="btn btn-small btn-primary"
           disabled={isSaving || !imgSrc}
         >
           Speichern{isSaving && "..."}
