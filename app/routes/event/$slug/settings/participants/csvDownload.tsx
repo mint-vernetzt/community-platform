79ae7b93ba5e3f6cc685005a6f45ecfbc3078024
nac62116 <56918308+nac62116@users.noreply.github.com>
2022-09-21 09:38:53 +0200

    wip: create loader structure in resource route

--- app/routes/event/$slug/settings/participants/csvDownload.tsx
+++ app/routes/event/$slug/settings/participants/csvDownload.tsx
@@ -1,5 +1,9 @@
 import { LoaderFunction } from "remix";
 import { getParamValueOrThrow } from "~/lib/utils/routes";
+import {
+  getEventBySlugOrThrow,
+  getFullDepthParticipants,
+} from "../../utils.server";
 
 export function createCsvString() {}
 
@@ -9,12 +13,24 @@ export const loader: LoaderFunction = async (args): Promise<LoaderData> => {
   const { request, params } = args;
 
   const slug = getParamValueOrThrow(params, "slug");
+  const event = await getEventBySlugOrThrow(slug);
+
+  const url = new URL(request.url);
+  const depth = url.searchParams.get("depth");
+  const type = url.searchParams.get("type");
+
+  if (type === "participants") {
+    if (depth === "full") {
+    } else {
+    }
+  }
+  const fullDepthParticipants = await getFullDepthParticipants(event.id);
 
   return new Response("TODO", {
     status: 200,
     headers: {
-      "Content-Type": "text/calendar",
-      "Content-Disposition": `filename="${"TODO"}.ics"`,
+      "Content-Type": "text/csv",
+      "Content-Disposition": `filename="${"TODO"}.csv"`,
     },
   });
 };
