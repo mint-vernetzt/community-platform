e9cbdbf6239618333a022acaf625734b31ef7efc
nac62116 <56918308+nac62116@users.noreply.github.com>
2022-10-05 13:39:54 +0200

    Own route for edit document with useFetcher

--- app/routes/event/$slug/settings/documents.tsx
+++ app/routes/event/$slug/settings/documents.tsx
@@ -1,12 +1,18 @@
 import { Document } from "@prisma/client";
 import { DataFunctionArgs } from "@remix-run/server-runtime";
 import { useState } from "react";
-import { ActionFunction, Link, LoaderFunction, useLoaderData } from "remix";
+import {
+  ActionFunction,
+  Link,
+  LoaderFunction,
+  useFetcher,
+  useLoaderData,
+} from "remix";
 import { makeDomainFunction } from "remix-domains";
 import { Form as RemixForm, performMutation } from "remix-forms";
 import { badRequest, serverError } from "remix-utils";
 import { z } from "zod";
-import { getUserByRequestOrThrow } from "~/auth.server";
+import { getUserByRequestOrThrow, resetPassword } from "~/auth.server";
 import InputText from "~/components/FormElements/InputText/InputText";
 import TextAreaWithCounter from "~/components/FormElements/TextAreaWithCounter/TextAreaWithCounter";
 import Modal from "~/components/Modal/Modal";
@@ -15,6 +21,10 @@ import { checkFeatureAbilitiesOrThrow } from "~/lib/utils/application";
 import { getParamValueOrThrow } from "~/lib/utils/routes";
 import { upload } from "~/storage.server";
 import { getEventBySlugOrThrow } from "../utils.server";
+import {
+  ActionData as EditDocumentActionData,
+  editDocumentSchema,
+} from "./documents/edit-document";
 import {
   checkIdentityOrThrow,
   checkOwnershipOrThrow,
@@ -205,8 +215,10 @@ export const action: ActionFunction = async (args) => {
 
 function Documents() {
   const loaderData = useLoaderData<LoaderData>();
-  const [fileSelected, setFileSelected] = useState(false);
 
+  const editDocumentFetcher = useFetcher<EditDocumentActionData>();
+
+  const [fileSelected, setFileSelected] = useState(false);
   const onSelectFile = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files.length > 0) {
       setFileSelected(true);
@@ -216,14 +228,6 @@ function Documents() {
   return (
     <>
       <h1 className="mb-8">Dokumente verwalten</h1>
-
-      {/* Show documents from loader data */}
-      {/* For each document */}
-      {/* - RemixForm to delete document with submit value delete */}
-      {/* - Modal to edit document. Inside RemixForm with edit inputs and submit value edit */}
-      {/* - Link to resource route for download with url parameter filename=${filename} */}
-      {/* For all documents */}
-      {/* - Link to resource route for download WITHOUT url parameter filename (meaning download all as zip) */}
       {loaderData.event.documents.length > 0 && (
         <div className="mb-8">
           <h3>Aktuelle Dokumente</h3>
@@ -250,10 +254,16 @@ function Documents() {
                     Editieren
                   </label>
                   <Modal id="modal-background-upload">
-                    <RemixForm method="post" schema={schema} reloadDocument>
+                    <RemixForm
+                      method="post"
+                      fetcher={editDocumentFetcher}
+                      action={`/event/${loaderData.event.slug}/settings/documents/edit-document`}
+                      schema={editDocumentSchema}
+                      // TODO: How to close modal after submit?
+                      //reloadDocument
+                    >
                       {({ Field, Errors, register }) => (
                         <>
-                          <Field name="submit" hidden value="edit" />
                           <Field
                             name="userId"
                             hidden
