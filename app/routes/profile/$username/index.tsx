df26f5c96887c05d24e0a078d1e8f2c5a1a561f3
Sirko Stenz <sirko.stenz@digitalnoise.de>
2022-10-10 14:17:40 -0500

    UI changes image upload

--- app/routes/profile/$username/index.tsx
+++ app/routes/profile/$username/index.tsx
@@ -137,7 +137,7 @@ export default function Index() {
   const avatar = loaderData.images.avatar;
   const Avatar = React.useCallback(
     () => (
-      <div className="h-36 w-36 bg-primary text-white text-6xl flex items-center justify-center rounded-md overflow-hidden">
+      <div className="h-36 w-36 bg-primary text-white text-6xl flex items-center justify-center rounded-md overflow-hidden rounded-full">
         {avatar !== undefined ? <img src={avatar} alt={fullName} /> : initials}
       </div>
     ),
@@ -214,7 +214,7 @@ export default function Index() {
                   <>
                     <label
                       htmlFor="modal-avatar"
-                      className="flex content-center items-center nowrap py-2 cursor-pointer text-primary"
+                      className="flex content-center items-center nowrap pt-4 pb-2 cursor-pointer text-primary"
                     >
                       <svg
                         width="17"
@@ -225,7 +225,7 @@ export default function Index() {
                       >
                         <path d="M14.9 3.116a.423.423 0 0 0-.123-.299l-1.093-1.093a.422.422 0 0 0-.598 0l-.882.882 1.691 1.69.882-.882a.423.423 0 0 0 .123-.298Zm-3.293.087 1.69 1.69v.001l-5.759 5.76a.422.422 0 0 1-.166.101l-2.04.68a.211.211 0 0 1-.267-.267l.68-2.04a.423.423 0 0 1 .102-.166l5.76-5.76ZM2.47 14.029a1.266 1.266 0 0 1-.37-.895V3.851a1.266 1.266 0 0 1 1.265-1.266h5.486a.422.422 0 0 1 0 .844H3.366a.422.422 0 0 0-.422.422v9.283a.422.422 0 0 0 .422.422h9.284a.422.422 0 0 0 .421-.422V8.07a.422.422 0 0 1 .845 0v5.064a1.266 1.266 0 0 1-1.267 1.266H3.367c-.336 0-.658-.133-.895-.37Z" />
                       </svg>
-                      <span className="ml-2 mr-4">Bild ändern</span>
+                      <span className="ml-2">Bild ändern</span>
                     </label>
 
                     <Modal id="modal-avatar">
