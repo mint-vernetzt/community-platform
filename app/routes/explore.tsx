452790ab01e46d145465695f33c4fff0bb8637e4
Peter Holló <hello@songsforthe.dev>
2022-10-20 23:41:17 +0200

    link events if feature active

--- app/routes/explore.tsx
+++ app/routes/explore.tsx
@@ -1,10 +1,20 @@
-import { Link, NavLink, Outlet } from "remix";
+import { Link, LoaderFunction, Outlet, useLoaderData } from "remix";
+import { getFeatureAbilities } from "~/lib/utils/application";
+
+type LoaderData = {
+  abilities: ReturnType<Awaited<typeof getFeatureAbilities>>;
+};
+
+export const loader: LoaderFunction = async (args) => {
+  const { request } = args;
+  const abilities = await getFeatureAbilities(request, "events");
+
+  return { abilities };
+};
 
 function Explore() {
-  const getClassName = (active: boolean) =>
-    `block text-3xl ${
-      active ? "text-primary" : "text-neutral-500"
-    }  hover:text-primary py-3`;
+  const loaderData = useLoaderData<LoaderData>();
+
   return (
     <div className="container relative pb-44">
       <div className="flex flex-col lg:flex-row -mx-4 pt-10 lg:pt-0">
@@ -15,6 +25,11 @@ function Explore() {
           <li>
             <Link to="./organizations">Organizations</Link>
           </li>
+          {loaderData.abilities.events.hasAccess && (
+            <li>
+              <Link to="./events">Events</Link>
+            </li>
+          )}
         </ul>
       </div>
       <div className="basis-6/12 px-4">
