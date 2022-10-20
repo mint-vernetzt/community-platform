c4043d3cedb25e7203c9fe433900e6c4192dbcb1
Peter Holló <hello@songsforthe.dev>
2022-10-20 10:47:32 +0200

    add functionality to back button

--- app/routes/event/$slug/index.tsx
+++ app/routes/event/$slug/index.tsx
@@ -2,7 +2,7 @@ import { GravityType } from "imgproxy/dist/types";
 import { Link, LoaderFunction, useLoaderData } from "remix";
 import { badRequest, forbidden, notFound } from "remix-utils";
 import { getUserByRequest } from "~/auth.server";
-import { H3 } from "~/components/Heading/Heading";
+import { useNavigate } from "react-router-dom";
 import { getImageURL } from "~/images.server";
 import { getInitials } from "~/lib/profile/getInitials";
 import { getOrganizationInitials } from "~/lib/organization/getOrganizationInitials";
@@ -276,6 +276,8 @@ function formatDateTime(date: Date) {
 function Index() {
   const loaderData = useLoaderData<LoaderData>();
 
+  const navigate = useNavigate();
+
   const reachedParticipationDeadline =
     new Date() > new Date(loaderData.event.participationUntil);
 
@@ -307,7 +309,7 @@ function Index() {
                   viewBox="0 0 16 16"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
                   />
                 </svg>
@@ -317,7 +319,8 @@ function Index() {
           {loaderData.event.name}
         </div>
         <div className="font-semi text-neutral-600 flex items-center">
-          <a href="#" className="flex items-center">
+          {/* TODO: get back route from loader */}
+          <button onClick={() => navigate(-1)} className="flex items-center">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
@@ -327,12 +330,12 @@ function Index() {
               viewBox="0 0 16 16"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
               />
             </svg>
             <span className="ml-2">zurück</span>
-          </a>
+          </button>
         </div>
       </section>
       <section className="hidden md:block container mt-6">
