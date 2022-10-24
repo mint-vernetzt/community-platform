440211a3be3dc770577a73653f95969bb4556704
nac62116 <56918308+nac62116@users.noreply.github.com>
2022-10-24 15:50:32 +0200

    Fixed typo

--- app/routes/organization/create.tsx
+++ app/routes/organization/create.tsx
@@ -128,7 +128,7 @@ export default function Create() {
                 d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
               />
             </svg>
-            <span className="ml-2">zurück</span>
+            <span className="ml-2">Zurück</span>
           </button>
         </div>
       </section>
