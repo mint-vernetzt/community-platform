7be0841d08a2a30df32271a610b9b36bf66a3b57
Manuel Portela <manuel.portela@digitalnoise.de>
2022-11-09 18:02:03 +0100

    fix: 477 bug limit document upload size to 5 mb (#606)
    * removed makeDomainFunction
    * splitted upload in parse + persist in order to get formData in between those two calls
    * needs more cleanup - this is kind of a wip to hotfix current upload issue

--- app/routes/event/$slug/settings/documents/upload-document.tsx
+++ app/routes/event/$slug/settings/documents/upload-document.tsx
@@ -1,14 +1,13 @@
 import { Document } from "@prisma/client";
 import { ActionFunction } from "remix";
-import { makeDomainFunction } from "remix-domains";
-import { PerformMutation, performMutation } from "remix-forms";
+import { PerformMutation } from "remix-forms";
 import { Schema, z } from "zod";
 import { getUserByRequestOrThrow } from "~/auth.server";
 import { checkFeatureAbilitiesOrThrow } from "~/lib/utils/application";
 import { getParamValueOrThrow } from "~/lib/utils/routes";
-import { upload } from "~/storage.server";
+import { doPersistUpload, parseMultipart } from "~/storage.server";
 import { getEventBySlugOrThrow } from "../../utils.server";
-import { checkIdentityOrThrow, checkOwnershipOrThrow } from "../utils.server";
+import { checkOwnershipOrThrow } from "../utils.server";
 import { createDocumentOnEvent } from "./utils.server";
 
 const schema = z.object({
@@ -18,56 +17,9 @@ const schema = z.object({
   document: z.unknown(),
 });
 
-const environmentSchema = z.object({
-  eventId: z.string(),
-  request: z.instanceof(Request),
-});
-
 export const uploadDocumentSchema = schema;
 
-const mutation = makeDomainFunction(
-  schema,
-  environmentSchema
-)(async (values, environment) => {
-  if (values.eventId !== environment.eventId) {
-    throw "Event id nicht korrekt";
-  }
-
-  const formData = await upload(environment.request, "documents");
-  const uploadHandlerResponseJSON = formData.get("document");
-  if (uploadHandlerResponseJSON === null) {
-    throw "Das hochladen des Dokumentes ist fehlgeschlagen.";
-  }
-  const uploadHandlerResponse: {
-    buffer: Buffer;
-    path: string;
-    filename: string;
-    extension: string;
-    mimeType: string;
-    sizeInBytes: number;
-  } = JSON.parse(uploadHandlerResponseJSON as string);
-
-  const document: Pick<
-    Document,
-    "filename" | "path" | "extension" | "sizeInMB" | "mimeType"
-  > = {
-    filename: uploadHandlerResponse.filename,
-    path: uploadHandlerResponse.path,
-    extension: uploadHandlerResponse.extension,
-    sizeInMB:
-      Math.round((uploadHandlerResponse.sizeInBytes / 1024 / 1024) * 100) / 100,
-    mimeType: uploadHandlerResponse.mimeType,
-  };
-
-  try {
-    await createDocumentOnEvent(values.eventId, document);
-  } catch (error) {
-    throw "Dokument konnte nicht in der Datenbank gespeichert werden.";
-  }
-
-  return values;
-});
-
+// TODO: wrap with performMutation + makeDomainFunction
 export type ActionData = PerformMutation<
   z.infer<Schema>,
   z.infer<typeof schema>
@@ -82,18 +34,37 @@ export const action: ActionFunction = async (args) => {
 
   const currentUser = await getUserByRequestOrThrow(request);
 
-  await checkIdentityOrThrow(request, currentUser, true);
-
   const event = await getEventBySlugOrThrow(slug);
 
   await checkOwnershipOrThrow(event, currentUser);
 
-  const result = await performMutation({
-    request,
-    schema,
-    mutation,
-    environment: { eventId: event.id, request: request },
-  });
+  const parsedData = await parseMultipart(request, "documents");
 
-  return result;
+  const { uploadHandlerResponse, formData } = parsedData;
+  const eventId = formData.get("eventId") as string;
+  if (eventId !== event.id) {
+    throw "Event id nicht korrekt";
+  }
+
+  await doPersistUpload("documents", uploadHandlerResponse);
+
+  const document: Pick<
+    Document,
+    "filename" | "path" | "extension" | "sizeInMB" | "mimeType"
+  > = {
+    filename: uploadHandlerResponse.filename,
+    path: uploadHandlerResponse.path,
+    extension: uploadHandlerResponse.extension,
+    sizeInMB:
+      Math.round((uploadHandlerResponse.sizeInBytes / 1024 / 1024) * 100) / 100,
+    mimeType: uploadHandlerResponse.mimeType,
+  };
+
+  try {
+    await createDocumentOnEvent(event.id, document);
+  } catch (error) {
+    throw "Dokument konnte nicht in der Datenbank gespeichert werden.";
+  }
+
+  return null;
 };
--- app/storage.server.ts
+++ app/storage.server.ts
@@ -87,7 +87,7 @@ function validatePersistence(
   }
 }
 
-export const upload = async (request: Request, bucketName: string) => {
+export const parseMultipart = async (request: Request, bucketName: string) => {
   try {
     const formData = await unstable_parseMultipartFormData(
       request,
@@ -101,7 +101,7 @@ export const upload = async (request: Request, bucketName: string) => {
     if (uploadHandlerResponseJSON === null) {
       throw serverError({ message: "Something went wrong on upload." });
     }
-    const uploadHandlerResponse: {
+    let uploadHandlerResponse: {
       buffer: {
         type: "Buffer";
         data: number[];
@@ -118,19 +118,39 @@ export const upload = async (request: Request, bucketName: string) => {
       throw serverError({ message: "Cannot upload empty file." });
     }
 
-    const { data, error } = await persistUpload(
-      uploadHandlerResponse.path,
-      buffer,
-      bucketName,
-      uploadHandlerResponse.mimeType
-    );
-    validatePersistence(error, data, uploadHandlerResponse.path, bucketName);
-
-    return formData;
-  } catch (exception) {
-    throw serverError({ message: "Something went wrong on upload." });
+    return {
+      uploadHandlerResponse: {
+        ...uploadHandlerResponse,
+        buffer,
+      },
+      formData,
+    };
+  } catch (e) {
+    throw "Error on upload document.";
   }
 };
+
+export async function doPersistUpload(
+  bucketName: string,
+  uploadHandlerResponse: {
+    buffer: Buffer;
+    path: string;
+    filename: string;
+    extension: string;
+    mimeType: string;
+    sizeInBytes: number;
+  }
+) {
+  const { data, error } = await persistUpload(
+    uploadHandlerResponse.path,
+    uploadHandlerResponse.buffer,
+    bucketName,
+    uploadHandlerResponse.mimeType
+  );
+  validatePersistence(error, data, uploadHandlerResponse.path, bucketName);
+
+  return true;
+}
 
 export function getPublicURL(relativePath: string, bucket = "images") {
   const { publicURL, error } = supabaseAdmin.storage // TODO: don't use admin (supabaseClient.setAuth)
