2f09a03ace711015fb74f8ed49682b6c6f612647
Peter Holló <hello@songsforthe.dev>
2022-11-28 12:01:58 +0100

    wip

---
+++ scripts/check-diffs.ts
@@ -0,0 +1,20 @@
+import { simpleGit, SimpleGit } from "simple-git";
+
+const git: SimpleGit = simpleGit();
+
+async function main() {
+  const summary = await git.diffSummary(["@{-1}}"]);
+  console.log({ summary });
+
+  const packagesChanged = summary.files.some(
+    (item) => item.file === "package.json" || item.file === "package-lock.json"
+  );
+
+  console.log({ packagesChanged });
+}
+
+main()
+  .catch(console.error)
+  .finally(() => {
+    console.log("done.");
+  });
