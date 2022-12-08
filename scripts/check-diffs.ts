dd491e20271ab4bfa38c8befbf4c835db844ccf2
Peter Holló <hello@songsforthe.dev>
2022-12-08 14:24:18 +0100

    feat: show relevant changes on branch switch (#728)
    * check for all relevant changes
    * use inquire for prompts
    * run specific commands
    * spawn process to pipe output to main threat
    * add hook

---
+++ scripts/check-diffs.ts
@@ -0,0 +1,105 @@
+import * as inquirer from "inquirer";
+import * as childProcess from "node:child_process";
+import type { SimpleGit } from "simple-git";
+import { simpleGit } from "simple-git";
+
+const git: SimpleGit = simpleGit();
+
+console.log("\nChecking diffs... 🔎\n");
+
+async function main() {
+  const diffSummary = await git.diffSummary(["HEAD@{1}"]);
+
+  let packagesChanged = false;
+  let migrationsChanged = false;
+  let datasetsChanged = false;
+  let enhancementsChanged = false;
+
+  diffSummary.files.forEach((item) => {
+    if (item.file === "package.json" || item.file === "package-lock.json") {
+      packagesChanged = true;
+    }
+    if (item.file.startsWith("prisma/migrations/")) {
+      migrationsChanged = true;
+    }
+    if (item.file.startsWith("prisma/scripts/import-datasets/data/")) {
+      datasetsChanged = true;
+    }
+    if (item.file === "supabase.enhancements.sql") {
+      enhancementsChanged = true;
+    }
+  });
+
+  const prompts = [];
+  if (packagesChanged) {
+    prompts.push({
+      type: "confirm",
+      name: "packagesChanged",
+      message: "Changes in packages found. Do you want to install packages?",
+      default: true,
+    });
+  }
+  if (migrationsChanged) {
+    prompts.push({
+      type: "confirm",
+      name: "migrationsChanged",
+      message: "Changes on migrations found. Do you want to run migration?",
+      default: true,
+    });
+  }
+  if (datasetsChanged) {
+    prompts.push({
+      type: "confirm",
+      name: "datasetsChanged",
+      message: "Changes on datasets found. Do you want to (re-)import data?",
+      default: true,
+    });
+  }
+
+  if (prompts.length === 0) {
+    console.log("\nNothing found. Happy Coding! 🚀\n");
+    return;
+  }
+
+  const answers = await inquirer.prompt(prompts);
+  if (answers.packagesChanged === true) {
+    await executeCommand("npm", ["i"]);
+  }
+  if (answers.migrationsChanged) {
+    await executeCommand("make", ["prisma-migrate"]);
+  }
+  if (answers.datasetsChanged) {
+    await executeCommand("make", ["import-datasets"]);
+  }
+  if (enhancementsChanged) {
+    await inquirer.prompt({
+      name: "enhancementsChanged",
+      message:
+        "Changes on supabase enhancements found. Please execute statements on your local supabase instance.",
+    });
+  }
+
+  console.log("\nDone.\n");
+}
+
+function executeCommand(command: string, options: string[]) {
+  return new Promise((resolve) => {
+    const runningCommand = childProcess.spawn(command, options, {
+      stdio: "inherit",
+    });
+    runningCommand.on("data", (data) => {
+      console.log(data.toString());
+    });
+
+    runningCommand.on("close", (code) => {
+      console.log(
+        `\nRunning "${command} ${options.join(
+          " "
+        )}" exited with code ${code}.\n`
+      );
+      resolve(code);
+    });
+  });
+}
+
+main().catch(console.error);
