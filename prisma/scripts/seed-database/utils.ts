cb2cfde61216315b005a1a4f8cc38f1676d18afc
nac62116 <56918308+nac62116@users.noreply.github.com>
2022-12-13 10:10:14 +0100

    wip: create utils for seed script

--- prisma/scripts/seed-database/utils.ts
+++ prisma/scripts/seed-database/utils.ts
@@ -1,4 +1,16 @@
-export function seedEntity() {
-  // TODO
-  // TODO
+import { Prisma } from "@prisma/client";
+import { prismaClient } from "~/prisma";
+
+type EntityTypes = Prisma.ProfileDelegate<
+  Prisma.RejectOnNotFound | Prisma.RejectPerOperation | undefined
+>;
+
+// TODO: More specific types
+export function seedEntity(type: EntityTypes, entity: object) {
+  const result = await prismaClient.profile.create({
+    data: entity,
+  });
 }
+
+// TODO: More specific types
+export function getStandardEntity(type: string) {}
