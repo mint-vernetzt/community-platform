7c478bf0e255cd726168f3ad1f076058c044fe1a
nac62116 <56918308+nac62116@users.noreply.github.com>
2022-12-13 19:49:19 +0100

    Improve generic type of seedEntity() method

--- prisma/scripts/seed-database/utils.ts
+++ prisma/scripts/seed-database/utils.ts
@@ -1,16 +1,37 @@
-import { Prisma } from "@prisma/client";
+import type { Prisma, PrismaClient } from "@prisma/client";
 import { prismaClient } from "~/prisma";
 
-type EntityTypes = Prisma.ProfileDelegate<
-  Prisma.RejectOnNotFound | Prisma.RejectPerOperation | undefined
->;
+type EntityData<T> = T extends "profile"
+  ? Prisma.ProfileCreateArgs["data"]
+  : T extends "organization"
+  ? Prisma.OrganizationCreateArgs["data"]
+  : T extends "project"
+  ? Prisma.ProjectCreateArgs["data"]
+  : T extends "event"
+  ? Prisma.EventCreateArgs["data"]
+  : T extends "award"
+  ? Prisma.AwardCreateArgs["data"]
+  : T extends "document"
+  ? Prisma.DocumentCreateArgs["data"]
+  : never;
 
-// TODO: More specific types
-export function seedEntity(type: EntityTypes, entity: object) {
-  const result = await prismaClient.profile.create({
-    data: entity,
-  });
+export async function seedEntity<
+  T extends keyof Pick<
+    PrismaClient,
+    "profile" | "organization" | "project" | "event" | "award" | "document"
+  >
+>(type: T, entity: EntityData<T>) {
+  // TODO: fix union type issue (almost got it but thats too hard...)
+  const result = await prismaClient[type].create(entity);
 }
 
 // TODO: More specific types
 export function getStandardEntity(type: string) {}
+
+seedEntity<"profile">("profile", {
+  email: "someuser",
+  username: "",
+  firstName: "",
+  lastName: "",
+  termsAccepted: true,
+});
