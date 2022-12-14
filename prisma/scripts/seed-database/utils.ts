8ea531f2120479b6d7bcfca235df63e6d32dfbab
nac62116 <56918308+nac62116@users.noreply.github.com>
2022-12-14 09:44:17 +0100

    Generic types for seedEntity and getEntityData

--- prisma/scripts/seed-database/utils.ts
+++ prisma/scripts/seed-database/utils.ts
@@ -1,32 +1,130 @@
 import type { Prisma, PrismaClient } from "@prisma/client";
 import { prismaClient } from "~/prisma";
 
-type EntityData<T> = T extends "profile"
-  ? Prisma.ProfileCreateArgs["data"]
+type EntityData = {
+  profile: Prisma.ProfileCreateArgs["data"];
+  organization: Prisma.OrganizationCreateArgs["data"];
+  project: Prisma.ProjectCreateArgs["data"];
+  event: Prisma.EventCreateArgs["data"];
+  award: Prisma.AwardCreateArgs["data"];
+  document: Prisma.DocumentCreateArgs["data"];
+};
+
+type EntityTypeOnData<T> = T extends "profile"
+  ? EntityData["profile"]
   : T extends "organization"
-  ? Prisma.OrganizationCreateArgs["data"]
+  ? EntityData["organization"]
   : T extends "project"
-  ? Prisma.ProjectCreateArgs["data"]
+  ? EntityData["project"]
   : T extends "event"
-  ? Prisma.EventCreateArgs["data"]
+  ? EntityData["event"]
   : T extends "award"
-  ? Prisma.AwardCreateArgs["data"]
+  ? EntityData["award"]
   : T extends "document"
-  ? Prisma.DocumentCreateArgs["data"]
+  ? EntityData["document"]
   : never;
+
+type EntityTypeOnStructure<T> = T extends "profile"
+  ? EntityStructure[
+      | "developer"
+      | "standard"
+      | "private"
+      | "public"
+      | "smallest"
+      | "emptyStrings"
+      | "singleFieldMissing"
+      | "onlyOneField"
+      | "unicode"
+      | "randomFieldSizes"
+      | "largest"]
+  : T extends "organization"
+  ? EntityStructure[
+      | "developer"
+      | "standard"
+      | "private"
+      | "public"
+      | "smallest"
+      | "emptyStrings"
+      | "singleFieldMissing"
+      | "onlyOneField"
+      | "unicode"
+      | "randomFieldSizes"
+      | "largest"]
+  : T extends "project"
+  ? EntityStructure[
+      | "developer"
+      | "standard"
+      | "smallest"
+      | "emptyStrings"
+      | "singleFieldMissing"
+      | "onlyOneField"
+      | "unicode"
+      | "randomFieldSizes"
+      | "largest"]
+  : T extends "event"
+  ? EntityStructure[
+      | "developer"
+      | "standard"
+      | "depth2"
+      | "depth3"
+      | "participantLimit"
+      | "canceled"
+      | "unpublished"
+      | "smallest"
+      | "emptyStrings"
+      | "singleFieldMissing"
+      | "onlyOneField"
+      | "unicode"
+      | "randomFieldSizes"
+      | "largest"]
+  : T extends "award"
+  ? EntityStructure["standard" | "smallest" | "largest"]
+  : T extends "document"
+  ? EntityStructure["standard" | "smallest" | "largest"]
+  : never;
+
+type EntityStructure = {
+  developer: "developer";
+  standard: "standard";
+  private: "private";
+  public: "public";
+  smallest: "smallest";
+  emptyStrings: "emptyStrings";
+  singleFieldMissing: "singleFieldMissing";
+  onlyOneField: "onlyOneField";
+  unicode: "unicode";
+  randomFieldSizes: "randomFieldSizes";
+  largest: "largest";
+  depth2: "depth2";
+  depth3: "depth3";
+  participantLimit: "participantLimit";
+  canceled: "canceled";
+  unpublished: "unpublished";
+};
 
 export async function seedEntity<
   T extends keyof Pick<
     PrismaClient,
     "profile" | "organization" | "project" | "event" | "award" | "document"
   >
->(type: T, entity: EntityData<T>) {
-  // TODO: fix union type issue (almost got it but thats too hard...)
-  const result = await prismaClient[type].create(entity);
+>(entityType: T, entity: EntityTypeOnData<T>) {
+  // TODO: fix union type issue (almost got the generic working, but thats too hard...)
+  // What i wanted was: When i pass "profile" as type T then the passed entity must be of type Prisma.ProfileCreateArgs["data"]
+  // @ts-ignore
+  const result = await prismaClient[entityType].create(entity);
 }
 
-// TODO: More specific types
-export function getStandardEntityData(type: string) {}
+export function getEntityData<
+  T extends keyof Pick<
+    PrismaClient,
+    "profile" | "organization" | "project" | "event" | "award" | "document"
+  >
+>(entityType: T, entityStructure: EntityTypeOnStructure<T>) {
+  const entityData = {
+    username: "",
+  };
+  return entityData as EntityTypeOnData<T>;
+}
 
 seedEntity<"profile">("profile", {
   email: "someuser",
@@ -35,3 +133,5 @@ seedEntity<"profile">("profile", {
   lastName: "",
   termsAccepted: true,
 });
+
+const event = getEntityData<"event">("event", "standard");
