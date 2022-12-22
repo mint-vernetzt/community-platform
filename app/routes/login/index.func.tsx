865a8e6692ac5f1c442844e8b4a03357dcf1377b
Rafael B Oliveira <92989835+olicoding@users.noreply.github.com>
2022-12-22 14:21:09 +0100

    Update index.func.tsx
    The suggested improvements on this test includes a describe() block wrapping the tests, also includes a try-catch block to catch any errors that might occur during the test, and uses test() instead of it() as you wished on issue #34.

--- app/routes/login/index.func.tsx
+++ app/routes/login/index.func.tsx
@@ -12,54 +12,56 @@ const authClient = createServerClient(
 
 let uid: string | undefined;
 
-before(async () => {
-  const email = "hello@songsforthe.dev";
-  const password = "password";
-  const firstName = "Peter";
-  const lastName = "Holló";
-  const termsAccepted = "on";
-  const username = "peterhollo";
+describe("Login flow", () => {
+  before(async () => {
+    const email = "hello@songsforthe.dev";
+    const password = "password";
+    const firstName = "Peter";
+    const lastName = "Holló";
+    const termsAccepted = "on";
+    const username = "peterhollo";
 
-  const {
-    data: { users },
-  } = await authClient.auth.admin.listUsers();
-  let user = users.filter((user) => user.email === email)[0];
-
-  if (!user) {
     const {
-      data: { user: newUser },
-      error,
-    } = await authClient.auth.admin.createUser({
-      email,
-      password,
-      email_confirm: true,
-      user_metadata: { firstName, lastName, username, termsAccepted },
-    });
+      data: { users },
+    } = await authClient.auth.admin.listUsers();
+    let user = users.filter((user) => user.email === email)[0];
 
-    if (newUser === null) {
-      console.error(error);
-      throw new Error("Couldn't create user.");
+    if (!user) {
+      const {
+        data: { user: newUser },
+        error,
+      } = await authClient.auth.admin.createUser({
+        email,
+        password,
+        email_confirm: true,
+        user_metadata: { firstName, lastName, username, termsAccepted },
+      });
+
+      if (error || !newUser) {
+        console.error(error);
+        throw new Error("Couldn't create user.");
+      }
+
+      user = newUser;
     }
 
-    user = newUser;
-  }
+    uid = user.id;
+  });
 
-  uid = user.id;
-});
+  test("redirect after login", () => {
+    cy.visit("http://localhost:3000/login");
+    cy.findByLabelText("E-Mail").type("hello@songsforthe.dev");
+    cy.findByLabelText("Passwort").type("password");
+    cy.findByText("Login").click();
+    cy.url().should("eq", "http://localhost:3000/");
+  });
 
-it("redirect after login", () => {
-  cy.visit("http://localhost:3000/login");
-  cy.findByLabelText("E-Mail").type("hello@songsforthe.dev");
-  cy.findByLabelText("Passwort").type("password");
-  cy.findByText("Login").click();
-  cy.url().should("eq", "http://localhost:3000/");
-});
-
-after(async () => {
-  if (uid !== undefined) {
-    await authClient.from("profiles").delete().match({ id: uid });
-    await authClient.auth.admin.deleteUser(uid);
-  }
+  after(async () => {
+    if (uid !== undefined) {
+      await authClient.from("profiles").delete().match({ id: uid });
+      await authClient.auth.admin.deleteUser(uid);
+    }
+  });
 });
 
 export {};
