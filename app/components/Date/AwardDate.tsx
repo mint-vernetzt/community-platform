f974a4efec9892f6b20c96ef98eeadf5e505134c
Peter Holló <hello@songsforthe.dev>
2023-01-02 15:25:57 +0100

    wip: use intl for date creation

--- app/components/Date/AwardDate.tsx
+++ app/components/Date/AwardDate.tsx
@@ -1,22 +1,26 @@
-import React from "react";
+// import React from "react";
 
 function AwardDate(props: { date: string }) {
-  const [initialRenderDone, setInitialRenderDone] = React.useState(false);
+  // const [initialRenderDone, setInitialRenderDone] = React.useState(false);
 
-  React.useEffect(() => {
-    if (initialRenderDone === false) {
-      setInitialRenderDone(true);
-    }
-  }, [initialRenderDone]);
+  // React.useEffect(() => {
+  //   if (initialRenderDone === false) {
+  //     setInitialRenderDone(true);
+  //   }
+  // }, [initialRenderDone]);
 
-  if (initialRenderDone) {
-    const date = new Date(props.date);
-    return (
-      <p className="text-xxs text-center leading-none">{date.getFullYear()}</p>
-    );
-  } else {
-    return <p className="text-xxs text-center leading-none">{props.date}</p>;
-  }
+  // if (initialRenderDone) {
+  const date = new Date(
+    Intl.DateTimeFormat("de-DE", { timeZone: "Europe/Berlin" }).format(
+      new Date(props.date)
+    )
+  );
+  return (
+    <p className="text-xxs text-center leading-none">{date.getFullYear()}</p>
+  );
+  // } else {
+  //   return <p className="text-xxs text-center leading-none">{props.date}</p>;
+  // }
 }
 
 export default AwardDate;
