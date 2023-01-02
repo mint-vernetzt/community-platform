f499ef59166cf1af5a0d08a52dfd4dedcb91eb4b
Peter Holló <hello@songsforthe.dev>
2023-01-02 15:43:49 +0100

    wip: test all other components

--- app/components/Date/CreatedAt.tsx
+++ app/components/Date/CreatedAt.tsx
@@ -1,30 +1,34 @@
+import { utcToZonedTime } from "date-fns-tz";
 import React from "react";
 
 function CreatedAt(props: { createdAt: string }) {
-  const [initialRenderDone, setInitialRenderDone] = React.useState(false);
+  //   const [initialRenderDone, setInitialRenderDone] = React.useState(false);
 
-  React.useEffect(() => {
-    if (initialRenderDone === false) {
-      setInitialRenderDone(true);
-    }
-  }, [initialRenderDone]);
+  //   React.useEffect(() => {
+  //     if (initialRenderDone === false) {
+  //       setInitialRenderDone(true);
+  //     }
+  //   }, [initialRenderDone]);
 
-  if (initialRenderDone) {
-    const date = new Date(props.createdAt).toLocaleDateString("de-De", {
-      day: "numeric",
-      month: "long",
-      year: "numeric",
-    });
-    return (
-      <p className="text-xs mb-4 text-center">Profil besteht seit dem {date}</p>
-    );
-  } else {
-    return (
-      <p className="text-xs mb-4 text-center">
-        Profil besteht seit dem {props.createdAt}
-      </p>
-    );
-  }
+  //   if (initialRenderDone) {
+  const date = utcToZonedTime(props.createdAt, "Europe/Berlin");
+  const formattedDate = date.toLocaleDateString("de-De", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+  return (
+    <p className="text-xs mb-4 text-center">
+      Profil besteht seit dem {formattedDate}
+    </p>
+  );
+  //   } else {
+  //     return (
+  //       <p className="text-xs mb-4 text-center">
+  //         Profil besteht seit dem {props.createdAt}
+  //       </p>
+  //     );
+  //   }
 }
 
 export default CreatedAt;
--- app/components/Date/FormattedDuration.tsx
+++ app/components/Date/FormattedDuration.tsx
@@ -1,27 +1,28 @@
+import { utcToZonedTime } from "date-fns-tz";
 import React from "react";
 import { getDuration } from "~/lib/utils/time";
 
 function FormattedDuration(props: { startDate: string; endDate: string }) {
-  const [initialRenderDone, setInitialRenderDone] = React.useState(false);
+  //   const [initialRenderDone, setInitialRenderDone] = React.useState(false);
 
-  React.useEffect(() => {
-    if (initialRenderDone === false) {
-      setInitialRenderDone(true);
-    }
-  }, [initialRenderDone]);
+  //   React.useEffect(() => {
+  //     if (initialRenderDone === false) {
+  //       setInitialRenderDone(true);
+  //     }
+  //   }, [initialRenderDone]);
 
-  if (initialRenderDone) {
-    const startTime = new Date(props.startDate);
-    const endTime = new Date(props.endDate);
-    const duration = getDuration(startTime, endTime);
-    return <>{duration}</>;
-  } else {
-    return (
-      <>
-        {props.startDate} - {props.endDate}
-      </>
-    );
-  }
+  //   if (initialRenderDone) {
+  const startTime = utcToZonedTime(props.startDate, "Europe/Berlin");
+  const endTime = utcToZonedTime(props.endDate, "Europe/Berlin");
+  const duration = getDuration(startTime, endTime);
+  return <>{duration}</>;
+  //   } else {
+  //     return (
+  //       <>
+  //         {props.startDate} - {props.endDate}
+  //       </>
+  //     );
+  //   }
 }
 
 export default FormattedDuration;
