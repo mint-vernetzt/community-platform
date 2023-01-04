7977661c63d7db295c6f513c0d0f18e49a8221ec
nac62116 <56918308+nac62116@users.noreply.github.com>
2023-01-04 17:06:50 +0100

    Added --force (-f) flag functionality

--- prisma/scripts/seed-database/index.ts
+++ prisma/scripts/seed-database/index.ts
@@ -1,5 +1,8 @@
 import { program } from "commander";
 import { getEntityData, seedEntity, setFakerSeed } from "./utils";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: "./.env" });
 
 program
   .name("seed-database")
@@ -35,26 +38,35 @@ async function main(
   // remove: boolean,
   edgeCases: boolean
 ) {
-  // TODO: if (--force option is not set)
-  // -> Check for local environment
-  // -> if (environment !== local)
-  // -> throw "run script with --force option to seed db on production environment"
+  if (!force) {
+    const databaseUrl = process.env.DATABASE_URL;
+    if (databaseUrl === undefined) {
+      throw new Error(
+        "No database url provided via the .env file. Database could not be seeded."
+      );
+    }
+    if (!databaseUrl.includes("localhost:")) {
+      throw new Error(
+        "You are not seeding the database on a local environment. All data will be dropped when you seed the database with this script. If you intended to run this script on a production environment please use the --force flag."
+      );
+    }
+  }
 
   // TODO: Upload fake avatars/backgrounds/logos/documents/awardIcons to bucket
 
-  setFakerSeed(123);
+  // setFakerSeed(123);
 
   // TODO: Generate entity data for each entityType and entityStructure
   // TODO: Create global constants to enable easy configuring (f.e. number of profiles, etc...) / Maybe include some of them in the script options
   // TODO: Define and implement edge cases in getEntityData()
-  const award = getEntityData<"award">("award", "Standard", 0, {
-    logo: { path: "" },
-  });
+  // const award = getEntityData<"award">("award", "Standard", 0, {
+  //   logo: { path: "" },
+  // });
 
   // TODO: Create corresponding users (pw: 12345678) on supabase auth.users table (see supabase local auth.users table and login.func.tsx for example)
 
   // Seed db via prisma call
-  seedEntity<"award">("award", award);
+  // seedEntity<"award">("award", award);
 
   // TODO: Log the profile list with emails and password: 12345678
 }
