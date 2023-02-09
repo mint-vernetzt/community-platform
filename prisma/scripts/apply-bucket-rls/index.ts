5eeface99ddfaab1ddf03e5ae0703d6ba3dde426
nac62116 <56918308+nac62116@users.noreply.github.com>
2023-02-09 21:26:06 +0100

    feat: apply supabase related sql enhancements (#815)
    * outsource bucket creation to script
    
    - remove bucket creation from supabase.enhancements.sql
    - use this script in seed-database.ts
    - run this script in the make file
    
    * Outsourced db and auth functions in seed-database
    
    - Moved them to own scripts in prisma/scripts and supabase/scripts
    - Execute them as child processes inside seed-database script
    
    * Removed bucket creation from sql enhancements
    
    - Buckets are now created via make all or make create-buckets
    
    * Outsource trigger creation to Makefile
    
    - The profile trigger is now applied via the Makefile and removed from the supabase.enhancements.sql
    - The seed script also executes the trigger creation as it depends on the trigger
    
    * Apply bucket RLS policies via script in Makefile
    
    - This was the last command in supabase.enhancements.sql and therefor it was deleted
    - All enhancement functionality is now outsourced to scripts and run via Makefile
    
    * Code cleaning
    
    - Added scripts to make file

---
+++ prisma/scripts/apply-bucket-rls/index.ts
@@ -0,0 +1,200 @@
+import type { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+import { createClient } from "@supabase/supabase-js";
+import { program } from "commander";
+import { prismaClient } from "~/prisma";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: "./.env" });
+
+program
+  .name("apply-bucket-rls")
+  .description(
+    `CLI tool to apply row level security policies on the supabase buckets.`
+  )
+  .version("1.0.0");
+
+program.parse();
+
+async function main() {
+  const supabaseUrl = process.env.SUPABASE_URL;
+  const supabaseServiceRoleKey = process.env.SERVICE_ROLE_KEY;
+  if (supabaseUrl === undefined) {
+    throw new Error(
+      "No SUPABASE_URL provided via the .env file. RLS could not be applied to buckets."
+    );
+  }
+  if (supabaseServiceRoleKey === undefined) {
+    throw new Error(
+      "No SERVICE_ROLE_KEY provided via the .env file. RLS could not be applied to buckets."
+    );
+  }
+  const authClient = createClient(supabaseUrl, supabaseServiceRoleKey);
+
+  const { data: imageBucketData } = await authClient.storage.getBucket(
+    "images"
+  );
+  if (imageBucketData === null) {
+    console.log(
+      `Bucket "images" does not exists. Cannot apply RLS. Please run "create-buckets" script located in "/supabase/scripts/create-buckets/index.ts" first.`
+    );
+  }
+  const { data: documentBucketData } = await authClient.storage.getBucket(
+    "documents"
+  );
+  if (documentBucketData !== null) {
+    console.log(
+      `Bucket "documents" does not exists. Cannot apply RLS. Please run "create-buckets" script located in "/supabase/scripts/create-buckets/index.ts" first.`
+    );
+  }
+
+  try {
+    await prismaClient.$queryRaw`
+      create policy "anyone can access images"
+        on storage.objects for select
+        using ( bucket_id = 'images' );
+    `;
+    console.log(
+      'Succesfully applied RLS policy "anyone can access images" to bucket "images".'
+    );
+  } catch (e: any) {
+    let error: PrismaClientKnownRequestError = e;
+    if (
+      error.code === "P2010" &&
+      error.meta !== undefined &&
+      error.meta.code === "42710" &&
+      error.meta.message ===
+        'db error: ERROR: policy "anyone can access images" for table "objects" already exists'
+    ) {
+      console.log(
+        'The RLS policy "anyone can access images" is already applied to bucket "images". Skipping this RLS application.'
+      );
+    }
+  }
+
+  try {
+    await prismaClient.$queryRaw`
+      create policy "authenticated user can upload images"
+        on storage.objects for insert
+        to authenticated
+        with check ( bucket_id = 'images' );
+    `;
+    console.log(
+      'Succesfully applied RLS policy "authenticated user can upload images" to bucket "images".'
+    );
+  } catch (e: any) {
+    let error: PrismaClientKnownRequestError = e;
+    if (
+      error.code === "P2010" &&
+      error.meta !== undefined &&
+      error.meta.code === "42710" &&
+      error.meta.message ===
+        'db error: ERROR: policy "authenticated user can upload images" for table "objects" already exists'
+    ) {
+      console.log(
+        'The RLS policy "authenticated user can upload images" is already applied to bucket "images". Skipping this RLS application.'
+      );
+    }
+  }
+
+  try {
+    await prismaClient.$queryRaw`
+      create policy "authenticated user can update images"
+        on storage.objects for update
+        to authenticated
+        using ( bucket_id = 'images' );
+    `;
+    console.log(
+      'Succesfully applied RLS policy "authenticated user can update images" to bucket "images".'
+    );
+  } catch (e: any) {
+    let error: PrismaClientKnownRequestError = e;
+    if (
+      error.code === "P2010" &&
+      error.meta !== undefined &&
+      error.meta.code === "42710" &&
+      error.meta.message ===
+        'db error: ERROR: policy "authenticated user can update images" for table "objects" already exists'
+    ) {
+      console.log(
+        'The RLS policy "authenticated user can update images" is already applied to bucket "images". Skipping this RLS application.'
+      );
+    }
+  }
+
+  try {
+    await prismaClient.$queryRaw`
+      create policy "authenticated user can access documents"
+        on storage.objects for select
+        to authenticated
+        using ( bucket_id = 'documents' );
+    `;
+    console.log(
+      'Succesfully applied RLS policy "authenticated user can access documents" to bucket "documents".'
+    );
+  } catch (e: any) {
+    let error: PrismaClientKnownRequestError = e;
+    if (
+      error.code === "P2010" &&
+      error.meta !== undefined &&
+      error.meta.code === "42710" &&
+      error.meta.message ===
+        'db error: ERROR: policy "authenticated user can access documents" for table "objects" already exists'
+    ) {
+      console.log(
+        'The RLS policy "authenticated user can access documents" is already applied to bucket "documents". Skipping this RLS application.'
+      );
+    }
+  }
+
+  try {
+    await prismaClient.$queryRaw`
+      create policy "authenticated user can upload documents"
+        on storage.objects for insert
+        to authenticated
+        with check ( bucket_id = 'documents' );
+    `;
+    console.log(
+      'Succesfully applied RLS policy "authenticated user can upload documents" to bucket "documents".'
+    );
+  } catch (e: any) {
+    let error: PrismaClientKnownRequestError = e;
+    if (
+      error.code === "P2010" &&
+      error.meta !== undefined &&
+      error.meta.code === "42710" &&
+      error.meta.message ===
+        'db error: ERROR: policy "authenticated user can upload documents" for table "objects" already exists'
+    ) {
+      console.log(
+        'The RLS policy "authenticated user can upload documents" is already applied to bucket "documents". Skipping this RLS application.'
+      );
+    }
+  }
+
+  try {
+    await prismaClient.$queryRaw`
+      create policy "authenticated user can update documents"
+        on storage.objects for update
+        to authenticated
+        using ( bucket_id = 'documents' );
+    `;
+    console.log(
+      'Succesfully applied RLS "authenticated user can update documents" to bucket "documents".'
+    );
+  } catch (e: any) {
+    let error: PrismaClientKnownRequestError = e;
+    if (
+      error.code === "P2010" &&
+      error.meta !== undefined &&
+      error.meta.code === "42710" &&
+      error.meta.message ===
+        'db error: ERROR: policy "authenticated user can update documents" for table "objects" already exists'
+    ) {
+      console.log(
+        'The RLS policy "authenticated user can update documents" is already applied to bucket "documents". Skipping this RLS application.'
+      );
+    }
+  }
+}
+
+main();
---
+++ prisma/scripts/truncate-tables/index.ts
@@ -0,0 +1,32 @@
+import { program } from "commander";
+import { prismaClient } from "~/prisma";
+
+program
+  .name("truncate-tables")
+  .description(
+    `CLI tool to truncate all prisma tables except the prisma migrations table.`
+  )
+  .version("1.0.0");
+
+program.parse();
+
+async function main() {
+  const tablenames = await prismaClient.$queryRaw<
+    Array<{ tablename: string }>
+  >`SELECT tablename FROM pg_tables WHERE schemaname='public'`;
+
+  const tables = tablenames
+    .map(({ tablename }) => tablename)
+    .filter((name) => name !== "_prisma_migrations")
+    .map((name) => `"public"."${name}"`)
+    .join(", ");
+
+  try {
+    await prismaClient.$executeRawUnsafe(`TRUNCATE TABLE ${tables} CASCADE;`);
+    console.log(`Successfully truncated tables: ${tables}`);
+  } catch (error) {
+    console.log({ error });
+  }
+}
+
+main();
