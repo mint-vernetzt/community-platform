068eeb19be3acfc1f11ab49e84d6757bd206e912
Sirko Stenz <sirko.stenz@digitalnoise.de>
2023-02-23 01:08:41 -0500

    styled search field in header

--- app/components/Search/Search.tsx
+++ app/components/Search/Search.tsx
@@ -25,9 +25,18 @@ function Search(props: React.HTMLProps<HTMLInputElement> & SearchProps) {
 
   return (
     <div className="form-control w-full">
-      <div className="flex items-center">
+      <div className="relative">
+        <div className="absolute left-4 top-4">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16">
+            <path
+              fill="#3C4658"
+              fill-rule="nonzero"
+              d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1ZM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0Z"
+            />
+          </svg>
+        </div>
         <input
-          className="w-full input bg-gray-300 pr-3"
+          className="w-full input search"
           placeholder={placeholder}
           minLength={minLength}
           value={value}
@@ -35,20 +44,22 @@ function Search(props: React.HTMLProps<HTMLInputElement> & SearchProps) {
           {...inputProps}
         />
         {value.length > 0 && (
-          <button className="-ml-3" type="reset" onClick={handleClear}>
-            <svg
-              viewBox="0 0 10 10"
-              width="10px"
-              height="10px"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M.808.808a.625.625 0 0 1 .885 0L5 4.116 8.308.808a.626.626 0 0 1 .885.885L5.883 5l3.31 3.308a.626.626 0 1 1-.885.885L5 5.883l-3.307 3.31a.626.626 0 1 1-.885-.885L4.116 5 .808 1.693a.625.625 0 0 1 0-.885Z"
-                fill="currentColor"
-              />
-            </svg>
-          </button>
+          <div className="absolute right-0 top-0.5">
+            <button className="p-4" type="reset" onClick={handleClear}>
+              <svg
+                viewBox="0 0 10 10"
+                width="10px"
+                height="10px"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M.808.808a.625.625 0 0 1 .885 0L5 4.116 8.308.808a.626.626 0 0 1 .885.885L5.883 5l3.31 3.308a.626.626 0 1 1-.885.885L5 5.883l-3.307 3.31a.626.626 0 1 1-.885-.885L4.116 5 .808 1.693a.625.625 0 0 1 0-.885Z"
+                  fill="currentColor"
+                />
+              </svg>
+            </button>
+          </div>
         )}
       </div>
     </div>
