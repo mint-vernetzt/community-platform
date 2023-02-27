2f1059649d578b4f3bd2f8fdc8672347dcdf3171
Peter Holló <hello@songsforthe.dev>
2023-02-27 14:35:16 +0100

    feat: release global search (#841)
    * increase space for name in nav bar
    * remove access check

--- app/root.tsx
+++ app/root.tsx
@@ -58,7 +58,7 @@ export const loader: LoaderFunction = async (args) => {
 
   const { abilities } = await validateFeatureAccess(
     authClient,
-    ["events", "projects", "search"],
+    ["events", "projects"],
     { throw: false }
   );
 
@@ -209,8 +209,7 @@ function NavBar(props: NavBarProps) {
                 </Link>
               </li>
             </ul>
-            {props.sessionUserInfo !== undefined &&
-            props.abilities.search.hasAccess === true ? (
+            {props.sessionUserInfo !== undefined ? (
               <div className="flex-initial w-full lg:w-auto order-last lg:order-2 py-3 lg:py-0 lg:px-5 ">
                 <Form method="get" action="/search">
                   <Search name="query" query={query} />
@@ -221,7 +220,7 @@ function NavBar(props: NavBarProps) {
 
           {/* TODO: link to login on anon*/}
           {props.sessionUserInfo !== undefined ? (
-            <div className="flex-initial h-10 w-1/2 xl:w-[150px] flex justify-end items-center xl:order-3">
+            <div className="flex-initial h-10 w-1/2 xl:w-[250px] flex justify-end items-center xl:order-3">
               <div className="dropdown dropdown-end">
                 <label
                   tabIndex={0}
