81606447330085a389d130d3e8762964504a3421
nac62116 <56918308+nac62116@users.noreply.github.com>
2023-03-09 11:07:57 +0100

    Wrote component stories for autocomplete

--- app/components/Autocomplete/Autocomplete.stories.tsx
+++ app/components/Autocomplete/Autocomplete.stories.tsx
@@ -9,14 +9,87 @@ export default {
 
 export const Default: ComponentStory<typeof Autocomplete> = (
   args: AutocompleteProps
-) => <Autocomplete {...args} />;
+) => (
+  <Autocomplete {...args} suggestions={[]} suggestionsLoaderPath="" value="" />
+);
 
-export const WithInput: ComponentStory<typeof Autocomplete> = (
+Default.storyName = "default";
+Default.args = {};
+
+export const WithOrganizationInput: ComponentStory<typeof Autocomplete> = (
   args: AutocompleteProps
 ) => (
   <Autocomplete
-    {...args} /** TODO: insert with input props query="a search query" */
+    {...args}
+    suggestions={[
+      {
+        name: "Some Organization",
+        logo: null,
+        id: "d38bf195-ec57-46a9-918e-8035436c0069",
+        types: [
+          {
+            organizationType: {
+              title: "Some OrganizationType",
+            },
+          },
+          {
+            organizationType: {
+              title: "Another OrganizationType",
+            },
+          },
+        ],
+      },
+      {
+        name: "Another Organization",
+        logo: null,
+        id: "45eb6867-9b9f-4384-860a-78496b86e089",
+        types: [
+          {
+            organizationType: {
+              title: "Some OrganizationType",
+            },
+          },
+          {
+            organizationType: {
+              title: "Another OrganizationType",
+            },
+          },
+        ],
+      },
+    ]}
+    suggestionsLoaderPath="/some/loader/path/to/get/suggestions"
+    value="organi"
   />
 );
-Default.storyName = "default";
-Default.args = {};
+
+WithOrganizationInput.storyName = "withOrganizationInput";
+WithOrganizationInput.args = {};
+
+export const WithProfileInput: ComponentStory<typeof Autocomplete> = (
+  args: AutocompleteProps
+) => (
+  <Autocomplete
+    {...args}
+    suggestions={[
+      {
+        firstName: "Friendly",
+        lastName: "Gardener",
+        avatar: null,
+        id: "ced1a004-2105-41b4-b020-f9891f344428",
+        position: "Chief of Gardening",
+      },
+      {
+        firstName: "Mad",
+        lastName: "Gardener",
+        avatar: null,
+        id: "735db8cb-3c7b-497b-8e0b-5e85d81ad84c",
+        position: "Executive Gardener",
+      },
+    ]}
+    suggestionsLoaderPath="/some/loader/path/to/get/suggestions"
+    value="garden"
+  />
+);
+
+WithProfileInput.storyName = "withProfileInput";
+WithProfileInput.args = {};
