516e4257a9695ca8c61986c3310aa5b5fa9192cf
Sirko Stenz <sirko.stenz@digitalnoise.de>
2023-03-14 23:56:11 -0500

    styling profil list, orga list, event participants list

--- app/routes/organization/$slug/settings/network/remove.tsx
+++ app/routes/organization/$slug/settings/network/remove.tsx
@@ -75,8 +75,8 @@ export function NetworkMemberRemoveForm(
       {({ Field, Button, Errors }) => {
         const initials = getInitialsOfName(networkMember.name);
         return (
-          <div className="w-full flex items-center flex-row border-b border-neutral-400 p-4">
-            <div className="h-16 w-16 bg-primary text-white text-3xl flex items-center justify-center rounded-full border overflow-hidden">
+          <div className="w-full flex items-center flex-row flex-nowrap border-b border-neutral-400 py-4 md:px-4">
+            <div className="h-16 w-16 bg-primary text-white text-3xl flex items-center justify-center rounded-full border overflow-hidden shrink-0">
               {networkMember.logo !== null && networkMember.logo !== "" ? (
                 <img src={networkMember.logo} alt={networkMember.name} />
               ) : (
