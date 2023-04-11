4ff2d74c1397f789a7be67231ba6b5b4f10f73ce
Peter Holló <hello@songsforthe.dev>
2023-04-11 16:35:59 +0200

    set up design workspace

---
+++ common/design/stories/Test.stories.tsx
@@ -0,0 +1,10 @@
+import type { Meta } from "@storybook/react";
+
+export const Test = () => {
+  return <h1 className="text-blue-500 text-sm">Test</h1>;
+};
+
+export default {
+  title: "Test",
+  component: Test,
+} as Meta;
---
+++ common/design/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["stories/**/*.mdx", "stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
