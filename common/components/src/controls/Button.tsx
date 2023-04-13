2f9d62a2319a27871ad4a11600bb92ab6e29531a
Peter Holló <hello@songsforthe.dev>
2023-04-13 08:43:09 +0200

    use classnames to organize conditional styles

--- common/components/src/controls/Button.tsx
+++ common/components/src/controls/Button.tsx
@@ -1,36 +1,48 @@
 import React from "react";
+import classnames from "classnames";
 
 type ButtonSize = "small" | "medium" | "large";
 
 type ButtonProps = {
   size?: ButtonSize;
+  loading?: boolean;
 };
 
 function Button(
   props: React.ButtonHTMLAttributes<HTMLButtonElement> & ButtonProps
 ) {
-  const { size = "medium" } = props;
+  const { size = "medium", ...otherProps } = props;
 
-  let buttonClass = "btn-md";
-  let fontSize = "text-sm";
-  let padding = "px-7 py-2";
-  if (size === "small") {
-    buttonClass = "btn-sm";
-    fontSize = "text-xs";
-    padding = "px-4";
-  } else if (size === "large") {
-    buttonClass = "btn-lg";
-    fontSize = "text-base";
-    padding = "px-10";
-  }
-
-  return (
-    <button
-      className={`btn ${buttonClass} ${fontSize} ${padding} font-semibold bg-primary hover:bg-primary-400 active:bg-primary-700 text-neutral-50`}
-    >
-      {props.children}
-    </button>
+  const classes = classnames(
+    "btn",
+    {
+      "btn-sm": size === "small",
+      "btn-md": size === "medium",
+      "btn-lg": size === "large",
+    },
+    {
+      "text-xs": size === "small",
+      "text-sm": size === "medium",
+      "text-base": size === "large",
+    },
+    {
+      "px-4": size === "small",
+      "px-7 py-2": size === "medium",
+      "px-10": size === "large",
+    },
+    {
+      "gap-2":
+        Array.isArray(otherProps.children) && otherProps.children.length > 1,
+    },
+    "font-semibold",
+    "bg-primary",
+    "hover:bg-primary-400",
+    "active:bg-primary-700",
+    "text-neutral-50",
+    { loading: otherProps.loading !== undefined }
   );
+
+  return <button {...otherProps} className={classes} />;
 }
 
 export default Button;
