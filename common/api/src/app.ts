b9de8ad43a0b4f1c05773843263da668951393b5
nac62116 <56918308+nac62116@users.noreply.github.com>
2023-04-17 17:39:33 +0200

    wip: error handling

--- common/api/src/app.ts
+++ common/api/src/app.ts
@@ -1,7 +1,13 @@
 import express, { json, urlencoded } from "express";
+import type {
+  Response as ExResponse,
+  Request as ExRequest,
+  NextFunction,
+} from "express";
 import { RegisterRoutes } from "../build/routes";
 import swaggerUI from "swagger-ui-express";
 import swaggerDocument from "../build/swagger.json";
+import { ValidateError } from "tsoa";
 
 export const app = express();
 
@@ -19,3 +25,26 @@ app.use(
 );
 
 RegisterRoutes(app);
+
+// Global express error handler (see https://tsoa-community.github.io/docs/error-handling.html)
+app.use(function errorHandler(
+  err: unknown,
+  req: ExRequest,
+  res: ExResponse,
+  next: NextFunction
+): ExResponse | void {
+  if (err instanceof ValidateError) {
+    console.warn(`Caught Validation Error for ${req.path}:`, err.fields);
+    return res.status(422).json({
+      message: "Validation Failed",
+      details: err?.fields,
+    });
+  }
+  if (err instanceof Error) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+
+  next();
+});
