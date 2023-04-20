f4d23fb0b2519abf9d56b6fe6295c54bc0076571
nac62116 <56918308+nac62116@users.noreply.github.com>
2023-04-20 13:21:24 +0200

    Added descriptions and tag

--- common/api/src/organizations/organizations-controller.ts
+++ common/api/src/organizations/organizations-controller.ts
@@ -1,13 +1,28 @@
-import { Controller, Get, Query, Route, Response, Security, Res } from "tsoa";
+import {
+  Controller,
+  Get,
+  Query,
+  Route,
+  Response,
+  Security,
+  Res,
+  Tags,
+} from "tsoa";
 import type { TsoaResponse } from "tsoa";
 import type { ValidateError } from "tsoa";
 import { getAllOrganizations } from "./organizations-service";
 
 type GetOrganizationsResult = ReturnType<typeof getAllOrganizations>;
 @Route("organizations")
+@Tags("Organizations")
 export class OrganizationsController extends Controller {
   /**
+   * Retrieves all organizations of the community including their public information.
+   * They do not include private (Community-only) fields.
+   * @param skip The number of items to skip before starting to collect the result set
+   * @param take The number of items to return (A take parameter larger than 50 is not allowed)
    * @param badRequestResponse A take parameter larger than 50 is not allowed
+   * @summary Retrieve all organizations.
    */
   @Response<ValidateError>(422, "Validation Failed")
   @Response<ValidateError>(401, "Authentication failed")
