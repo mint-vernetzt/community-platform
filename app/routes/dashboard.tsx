fccb193ebe0285f8769546d2c6d31d132508e9b6
Sirko Stenz <sirko.stenz@digitalnoise.de>
2023-05-09 22:24:55 -0500

    added profil card styling

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -4,6 +4,7 @@ import { useLoaderData } from "@remix-run/react";
 import ProfileCard from "common/components/src/organisms/cards/ProfileCard";
 import { useTransition } from "react";
 import { createAuthClient } from "~/auth.server";
+import Chip from "~/components/Chip/Chip";
 
 export const loader = async (args: LoaderArgs) => {
   const { request } = args;
@@ -20,7 +21,15 @@ function Dashboard() {
 
   return (
     <>
-      <ProfileCard />
+      <section className="py-16 lg:py-24 relative">
+        <div className="container relative">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 items-stretch">
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+          </div>
+        </div>
+      </section>
     </>
   );
 }
