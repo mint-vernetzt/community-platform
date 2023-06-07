4f931a25df7059028fbb88bda8dc2a2706199af9
Peter Holló <hello@songsforthe.dev>
2023-06-07 17:10:37 +0200

    implement feature access and redirects

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,16 +1,27 @@
-import type { LoaderArgs } from "@remix-run/node";
+import { LoaderArgs, redirect } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import ProfileCard from "common/components/src/organisms/cards/ProfileCard";
 import { useTransition } from "react";
-import { createAuthClient } from "~/auth.server";
-import Chip from "~/components/Chip/Chip";
+import { createAuthClient, getSessionUser } from "~/auth.server";
+import { getFeatureAbilities } from "~/lib/utils/application";
 
 export const loader = async (args: LoaderArgs) => {
   const { request } = args;
   const response = new Response();
 
-  createAuthClient(request, response);
+  const authClient = createAuthClient(request, response);
+
+  const featureAbilities = await getFeatureAbilities(authClient, "dashboard");
+  if (featureAbilities["dashboard"].hasAccess === false) {
+    return redirect("/");
+  }
+
+  const sessionUser = await getSessionUser(authClient);
+
+  if (sessionUser === null) {
+    return redirect("/login");
+  }
 
   return json({}, { headers: response.headers });
 };
@@ -24,9 +35,7 @@ function Dashboard() {
       <section className="py-16 lg:py-24 relative">
         <div className="container relative">
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 items-stretch">
-            <ProfileCard />
-            <ProfileCard />
-            <ProfileCard />
+            hello
           </div>
         </div>
       </section>
