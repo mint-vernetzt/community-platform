a9346c11dac7225379a4dea3fb440e103211c775
Peter Holló <hello@songsforthe.dev>
2023-06-08 11:39:16 +0200

    button can be used as links

--- common/components/src/molecules/Button.tsx
+++ common/components/src/molecules/Button.tsx
@@ -5,6 +5,7 @@ import classnames from "classnames";
 
 export type ButtonSize = "small" | "medium" | "large";
 export type ButtonVariant = "normal" | "outline" | "ghost";
+export type ButtonType = "button" | "a";
 export type ButtonLevel =
   | "primary"
   // | "secondary"
@@ -18,17 +19,27 @@ export type ButtonProps = {
   variant?: ButtonVariant;
   level?: ButtonLevel;
   responsive?: boolean; // lg -> md -> sm
+  // TODO: implement Button or Anchor props conditional (eg. if as="button" then href not possible)
+  as?: ButtonType;
 };
 
 function Button(
-  props: React.ButtonHTMLAttributes<HTMLButtonElement> & ButtonProps
+  props: ButtonProps &
+    (
+      | React.ButtonHTMLAttributes<HTMLButtonElement>
+      | React.AnchorHTMLAttributes<HTMLAnchorElement>
+    )
 ) {
   const {
     size = "medium",
     variant = "normal",
     level = "primary",
+    as = "button",
     ...otherProps
   } = props;
+
+  // check if disabled prop is present and true (reason: Button and Anchor props)
+  const isDisabled = "disabled" in props && props.disabled;
 
   const classes = classnames(
     "mv-btn",
@@ -39,39 +50,37 @@ function Button(
     size === "medium" && "mv-btn-md mv-text-sm mv-px-7 mv-py-2",
     size === "large" && "mv-btn-lg mv-text-base mv-px-10",
     // button primary disabled
-    props.disabled &&
+    isDisabled &&
       variant === "normal" &&
       level === "primary" &&
       "mv-bg-neutral-200 mv-text-neutral-400",
     // button negative disabled
-    props.disabled &&
+    isDisabled &&
       variant === "normal" &&
       level === "negative" &&
       "mv-bg-negative-300 mv-text-white",
     // button primary
-    !props.disabled &&
+    !isDisabled &&
       variant === "normal" &&
       level === "primary" &&
       "mv-bg-primary mv-text-neutral-50 hover:mv-bg-primary-600 focus:mv-bg-primary-600 active:mv-bg-primary-700",
     // button negative
-    !props.disabled &&
+    !isDisabled &&
       variant === "normal" &&
       level === "negative" &&
       "mv-bg-negative-600 mv-text-white hover:mv-bg-negative-700 focus:mv-bg-negative-700 active:mv-bg-negative-800",
     // button primary outline disabled
-    props.disabled &&
+    isDisabled &&
       variant === "outline" &&
       "mv-bg-neutral-50 mv-border-neutral-300 mv-text-neutral-300",
     // button primary outline
-    !props.disabled &&
+    !isDisabled &&
       variant === "outline" &&
       "mv-bg-neutral-50 mv-border-primary mv-text-primary hover:mv-bg-primary-50 focus:mv-bg-primary-50 active:mv-bg-primary-100",
     // button primary ghost disabled
-    props.disabled &&
-      variant === "ghost" &&
-      "mv-bg-neutral-50 mv-text-neutral-300",
+    isDisabled && variant === "ghost" && "mv-bg-neutral-50 mv-text-neutral-300",
     // button primary ghost
-    !props.disabled &&
+    !isDisabled &&
       variant === "ghost" &&
       "mv-text-primary hover:mv-text-primary-700 hover:mv-bg-neutral-50 focus:mv-text-primary-700 focus:mv-bg-neutral-50 active:mv-bg-neutral-100",
 
@@ -83,7 +92,12 @@ function Button(
       "mv-loading"
   );
 
-  return <button {...otherProps} className={classes} />;
+  const element = React.createElement(as, {
+    ...otherProps,
+    className: classes,
+  });
+
+  return element;
 }
 
 export default Button;
