01fa3577d3366aab23974528fae3232021d8a721
nac62116 <56918308+nac62116@users.noreply.github.com>
2023-06-12 12:33:29 +0200

    Profile visibilities must be optional first
    This is the first migration step, to ensure a working migration. The goal is to make profile visbilities mandatory. This will be done after the first migration step and after creating the visibility settings for already existing profiles.

--- prisma/migrations/20230612103002_one_to_one_relation_profile_to_profile_visibilities_optional/migration.sql
+++ prisma/migrations/20230612103002_one_to_one_relation_profile_to_profile_visibilities_optional/migration.sql
@@ -3,17 +3,16 @@
 
   - You are about to drop the column `profile_id` on the `profile_visibilities` table. All the data in the column will be lost.
   - A unique constraint covering the columns `[profile_visibility_id]` on the table `profiles` will be added. If there are existing duplicate values, this will fail.
-  - Added the required column `profile_visibility_id` to the `profiles` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
 ALTER TABLE "profile_visibilities" DROP COLUMN "profile_id";
 
 -- AlterTable
-ALTER TABLE "profiles" ADD COLUMN     "profile_visibility_id" TEXT NOT NULL;
+ALTER TABLE "profiles" ADD COLUMN     "profile_visibility_id" TEXT;
 
 -- CreateIndex
 CREATE UNIQUE INDEX "profiles_profile_visibility_id_key" ON "profiles"("profile_visibility_id");
 
 -- AddForeignKey
-ALTER TABLE "profiles" ADD CONSTRAINT "profiles_profile_visibility_id_fkey" FOREIGN KEY ("profile_visibility_id") REFERENCES "profile_visibilities"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "profiles" ADD CONSTRAINT "profiles_profile_visibility_id_fkey" FOREIGN KEY ("profile_visibility_id") REFERENCES "profile_visibilities"("id") ON DELETE SET NULL ON UPDATE CASCADE;
