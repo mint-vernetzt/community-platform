28f10958d4bc4a739ab6788aefb3be11ccc3e9e0
Manuel Portela <manuel.portela@digitalnoise.de>
2023-06-20 18:45:24 +0200

    feat: implement pakt submission (#900)

---
+++ app/lib/submissions/forms/pakt/paktFormData.ts
@@ -0,0 +1,10 @@
+export interface PaktFormData {
+  institution: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  reachableBy: string;
+  message: string;
+  terms_accepted: string;
+}
