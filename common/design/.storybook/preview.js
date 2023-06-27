a269152958483b42c53d8f958e88d24fa8df74ce
Peter Holló <hello@songsforthe.dev>
2023-06-27 15:41:50 +0200

    add viewports with tailwind breakpoints

--- common/design/.storybook/preview.js
+++ common/design/.storybook/preview.js
@@ -6,6 +6,51 @@ import "@fontsource/source-sans-pro/600.css";
 import "@fontsource/source-sans-pro/700.css";
 import "@fontsource/source-sans-pro/900.css";
 
+const viewports = {
+  xs: {
+    name: "xs",
+    styles: {
+      width: "320px",
+      height: "480px",
+    },
+  },
+  sm: {
+    name: "sm",
+    styles: {
+      width: "640px",
+      height: "480px",
+    },
+  },
+  md: {
+    name: "md",
+    styles: {
+      width: "768px",
+      height: "480px",
+    },
+  },
+  lg: {
+    name: "lg",
+    styles: {
+      width: "1024px",
+      height: "768px",
+    },
+  },
+  xl: {
+    name: "xl",
+    styles: {
+      width: "1280px",
+      height: "800px",
+    },
+  },
+  "2xl": {
+    name: "2xl",
+    styles: {
+      width: "1536px",
+      height: "800px",
+    },
+  },
+};
+
 const preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
@@ -15,6 +60,7 @@ const preview = {
         date: /Date$/,
       },
     },
+    viewport: { viewports },
   },
 };
 
