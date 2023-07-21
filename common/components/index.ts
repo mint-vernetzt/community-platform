f4c82175a9f9167ceac1133a089a31ee6e632eeb
Sirko Stenz <sirko.stenz@digitalnoise.de>
2023-07-20 22:32:35 -0500

    integrated footer

--- common/components/index.ts
+++ common/components/index.ts
@@ -1,3 +1,4 @@
 export { ProfileCard, OrganizationCard } from "./src/organisms/cards";
 export { Avatar, Button, Chip, Link } from "./src/molecules";
 export { CardContainer } from "./src/organisms/containers";
+export { Footer } from "./src/organisms";
