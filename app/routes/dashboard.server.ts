f0ea17746e4a2eb217240454401b5b71b9f51650
nac62116 <56918308+nac62116@users.noreply.github.com>
2023-07-24 12:45:41 +0200

    get profile by sessionUser.id instead of metadata
    Also created a dashboard.server.ts for the prisma calls to prevent overfetching

---
+++ app/routes/dashboard.server.ts
@@ -0,0 +1,105 @@
+import { prismaClient } from "~/prisma";
+
+export async function getProfileById(id: string) {
+  const profile = await prismaClient.profile.findUnique({
+    where: { id },
+    select: {
+      firstName: true,
+      lastName: true,
+      username: true,
+    },
+  });
+
+  return profile;
+}
+
+export async function getProfileCount() {
+  return await prismaClient.profile.count();
+}
+
+export async function getProfilesForCards(skip: number, take: number) {
+  const profiles = await prismaClient.profile.findMany({
+    select: {
+      academicTitle: true,
+      username: true,
+      firstName: true,
+      lastName: true,
+      position: true,
+      avatar: true,
+      background: true,
+      offers: { select: { offer: { select: { title: true } } } },
+      areas: { select: { area: { select: { name: true } } } },
+      memberOf: {
+        select: {
+          organization: {
+            select: {
+              slug: true,
+              logo: true,
+              name: true,
+            },
+          },
+        },
+        orderBy: {
+          organization: {
+            updatedAt: "asc",
+          },
+        },
+      },
+      _count: {
+        select: {
+          memberOf: true,
+        },
+      },
+    },
+    skip,
+    take,
+    orderBy: [{ score: "desc" }, { updatedAt: "desc" }],
+  });
+
+  return profiles;
+}
+
+export async function getOrganizationCount() {
+  return await prismaClient.organization.count();
+}
+
+export async function getOrganizationsForCards(skip: number, take: number) {
+  const profiles = await prismaClient.organization.findMany({
+    select: {
+      slug: true,
+      name: true,
+      logo: true,
+      background: true,
+      focuses: { select: { focus: { select: { title: true } } } },
+      areas: { select: { area: { select: { name: true } } } },
+      types: { select: { organizationType: { select: { title: true } } } },
+      teamMembers: {
+        select: {
+          profile: {
+            select: {
+              username: true,
+              avatar: true,
+              firstName: true,
+              lastName: true,
+            },
+          },
+        },
+        orderBy: {
+          profile: {
+            updatedAt: "asc",
+          },
+        },
+      },
+      _count: {
+        select: {
+          memberOf: true,
+        },
+      },
+    },
+    skip,
+    take,
+    orderBy: [{ score: "desc" }, { updatedAt: "desc" }],
+  });
+
+  return profiles;
+}
