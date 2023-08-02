b0d7fcf8382e2ac361e4783e3dfff99d93b9a6b7
nac62116 <56918308+nac62116@users.noreply.github.com>
2023-08-02 11:20:17 +0200

    feat: trigger test mail on move from waiting list to participants (#945)
    * prepare task
    * mail on waitinglist to participant with dummy text
    * Remove unneccessary protocol
    * Installed new package "handlebars"
    * Send html and text via handlebars template
    - Added SYSTEM_MAIL_SENDER to .env
    * Add templates for system mail delivery
    * format date
    * fix broken test
    * fix typos in templates
    * fix date format issue

--- app/routes/event/$slug/settings/waiting-list/move-to-participants.tsx
+++ app/routes/event/$slug/settings/waiting-list/move-to-participants.tsx
@@ -3,10 +3,13 @@ import { json } from "@remix-run/node";
 import { makeDomainFunction } from "remix-domains";
 import type { PerformMutation } from "remix-forms";
 import { performMutation } from "remix-forms";
+import { badRequest, serverError } from "remix-utils";
 import type { Schema } from "zod";
 import { z } from "zod";
 import { createAuthClient, getSessionUserOrThrow } from "~/auth.server";
+import { mailerOptions } from "~/lib/submissions/mailer/mailerOptions";
 import { checkFeatureAbilitiesOrThrow } from "~/lib/utils/application";
+import { getCompiledMailTemplate, mailer } from "~/mailer.server";
 import { getProfileByUserId } from "~/profile.server";
 import { checkSameEventOrThrow, getEventByIdOrThrow } from "../../utils.server";
 import { checkIdentityOrThrow, checkOwnershipOrThrow } from "../utils.server";
@@ -50,6 +53,67 @@ export const action: ActionFunction = async (args) => {
     if (profile !== null) {
       await connectParticipantToEvent(event.id, profile.id);
       await disconnectFromWaitingListOfEvent(event.id, result.data.profileId);
+      // Send info mail
+      const sender = process.env.SYSTEM_MAIL_SENDER;
+      if (sender === undefined) {
+        console.error(
+          "No system mail sender address provided. Please add one inside the .env."
+        );
+        return badRequest({
+          message:
+            "No system mail sender address provided. Please add one inside the .env.",
+        });
+      }
+      // -> mail of person which was moved to waitinglist
+      const recipient = profile.email;
+      const subject = `Deine Teilnahme an der Veranstaltung ${event.name}`;
+      const content = {
+        recipient: {
+          firstName: profile.firstName,
+          lastName: profile.lastName,
+        },
+        event: {
+          name: event.name,
+          url: `${process.env.COMMUNITY_BASE_URL}/event/${event.slug}`,
+          startDate: `${event.startTime.getDate()}.${
+            event.startTime.getMonth() + 1
+          }.${event.startTime.getFullYear()}`,
+          startTime: `${event.startTime.getHours()}:${
+            event.startTime.getMinutes() < 10
+              ? `${event.startTime.getMinutes()}0`
+              : event.startTime.getMinutes()
+          }`,
+          supportContact: {
+            firstName:
+              event.teamMembers[0].profile.firstName ??
+              "Kein zuständiges Teammitglied gefunden",
+            lastName:
+              event.teamMembers[0].profile.lastName ??
+              "Kein zuständiges Teammitglied gefunden",
+            email:
+              event.teamMembers[0].profile.email ??
+              "Kein zuständiges Teammitglied gefunden",
+          },
+        },
+      };
+      const textTemplatePath = "mail-templates/move-to-participants/text.hbs";
+      const text = await getCompiledMailTemplate<typeof textTemplatePath>(
+        textTemplatePath,
+        content
+      );
+      const htmlTemplatePath = "mail-templates/move-to-participants/html.hbs";
+      const html = await getCompiledMailTemplate<typeof htmlTemplatePath>(
+        htmlTemplatePath,
+        content
+      );
+
+      try {
+        await mailer(mailerOptions, sender, recipient, subject, text, html);
+      } catch (error) {
+        // Throw a 500 -> Mailer issue
+        console.error(error);
+        return serverError({ message: "Mailer Issue" });
+      }
     }
   }
   return json<ActionData>(result, { headers: response.headers });
