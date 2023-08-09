8f732b6e4cc320cafaf48ad2be19f6b0c3d9662b
nac62116 <56918308+nac62116@users.noreply.github.com>
2023-08-09 09:35:29 +0200

    wip: suggest own organizations on event settings organizations

--- app/routes/event/$slug/settings/organizations.tsx
+++ app/routes/event/$slug/settings/organizations.tsx
@@ -31,6 +31,7 @@ import {
   getResponsibleOrganizationDataFromEvent,
   getResponsibleOrganizationSuggestions,
 } from "./utils.server";
+import { getOwnOrganizationsSuggestions } from "./organizations.server";
 
 export const loader = async (args: LoaderArgs) => {
   const { request, params } = args;
@@ -57,17 +58,39 @@ export const loader = async (args: LoaderArgs) => {
     return organization;
   });
 
+  const alreadyResponsibleOrganizationSlugs = organizations.map(
+    (organization) => {
+      return organization.slug;
+    }
+  );
+
+  const ownOrganizationsSuggestions = await getOwnOrganizationsSuggestions(
+    sessionUser.id,
+    alreadyResponsibleOrganizationSlugs
+  );
+
+  const enhancedOwnOrganizations = ownOrganizationsSuggestions.map(
+    (organization) => {
+      if (organization.logo !== null) {
+        const publicURL = getPublicURL(authClient, organization.logo);
+        if (publicURL !== null) {
+          organization.logo = getImageURL(publicURL, {
+            resize: { type: "fill", width: 64, height: 64 },
+            gravity: GravityType.center,
+          });
+        }
+      }
+      return organization;
+    }
+  );
+
   const url = new URL(request.url);
   const suggestionsQuery =
     url.searchParams.get("autocomplete_query") || undefined;
   let responsibleOrganizationSuggestions;
   if (suggestionsQuery !== undefined && suggestionsQuery !== "") {
     const query = suggestionsQuery.split(" ");
-    const alreadyResponsibleOrganizationSlugs = organizations.map(
-      (organization) => {
-        return organization.slug;
-      }
-    );
+
     responsibleOrganizationSuggestions =
       await getResponsibleOrganizationSuggestions(
         authClient,
@@ -82,6 +105,7 @@ export const loader = async (args: LoaderArgs) => {
       eventId: event.id,
       responsibleOrganizations: enhancedOrganizations,
       responsibleOrganizationSuggestions,
+      ownOrganizationsSuggestions: enhancedOwnOrganizations,
     },
     { headers: response.headers }
   );
@@ -172,6 +196,82 @@ function Organizations() {
           {addOrganizationFetcher.data.message}
         </div>
       ) : null}
+      <h4 className="mb-4 mt-16 font-semibold">
+        Eigene Organisationen hinzufügen
+      </h4>
+      <p className="mb-8">
+        Hier werden dir Deine eigenen Organisationen vorgeschlagen um sie auf
+        einen Klick als verantwortliche Organisationen hinzuzufügen.
+      </p>
+      <div className="mb-4 md:max-h-[630px] overflow-auto">
+        <ul>
+          {loaderData.ownOrganizationsSuggestions.map((organization) => {
+            const initials = getInitialsOfName(organization.name);
+            return (
+              <li
+                className="w-full flex items-center flex-row flex-nowrap border-b border-neutral-400 py-4 md:px-4"
+                key={organization.id}
+              >
+                <div className="h-16 w-16 bg-primary text-white text-3xl flex items-center justify-center rounded-full border overflow-hidden shrink-0">
+                  {organization.logo !== null && organization.logo !== "" ? (
+                    <img src={organization.logo} alt={organization.name} />
+                  ) : (
+                    <>{initials}</>
+                  )}
+                </div>
+                <div className="pl-4">
+                  <Link to={`/organization/${organization.slug}`}>
+                    <H3
+                      like="h4"
+                      className="text-xl mb-1 no-underline hover:underline"
+                    >
+                      {organization.name}
+                    </H3>
+                  </Link>
+                  {organization.types.length !== 0 ? (
+                    <p className="font-bold text-sm cursor-default">
+                      {organization.types
+                        .map((relation) => {
+                          return relation.organizationType.title;
+                        })
+                        .join(" / ")}
+                    </p>
+                  ) : null}
+                </div>
+                <Form
+                  schema={addOrganizationSchema}
+                  fetcher={addOrganizationFetcher}
+                  action={`/event/${slug}/settings/organizations/add-organization`}
+                  hiddenFields={["userId", "eventId"]}
+                  values={{
+                    userId: loaderData.userId,
+                    eventId: loaderData.eventId,
+                  }}
+                  className="ml-auto"
+                >
+                  {(props) => {
+                    const { Field, Button, Errors } = props;
+                    return (
+                      <>
+                        <Errors />
+                        <Field name="userId" />
+                        <Field name="eventId" />
+                        <Button
+                          className="btn btn-outline-primary ml-auto btn-small"
+                          title="Hinzufügen"
+                          type="submit"
+                        >
+                          Hinzufügen
+                        </Button>
+                      </>
+                    );
+                  }}
+                </Form>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
       <h4 className="mb-4 mt-16 font-semibold">Organisationen</h4>
       <p className="mb-8">
         Hier siehst du alle für die Veranstaltung verantwortlichen
