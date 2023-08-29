9f02a199c251ac6c28dabe1ff2575509ff8ea36c
Manuel Portela <manuel.portela@digitalnoise.de>
2023-08-29 18:17:26 +0200

    fix neglegted promises and improve script

--- prisma/scripts/update-targetgroups/index.ts
+++ prisma/scripts/update-targetgroups/index.ts
@@ -4,7 +4,6 @@ const ELEMENTARY = "646c5323-2b9f-40be-a284-9b13d673bc1c";
 const PRIMARY = "cbbc8772-8de9-41d5-ac13-b46bf9daafed";
 
 async function removeTargetGroupElementary() {
-  console.log(`Delete target group elementary school`);
   const result = await prismaClient.targetGroup.deleteMany({
     where: {
       id: ELEMENTARY,
@@ -15,92 +14,110 @@ async function removeTargetGroupElementary() {
 }
 
 async function updateEvents() {
-  console.log(`Select all events with elementary school target group`);
-  const eventsWithElementarySchool = await prismaClient.event.findMany({
-    include: { targetGroups: true },
-    where: {
-      targetGroups: {
-        some: {
-          targetGroupId: ELEMENTARY,
-        },
-      },
-    },
-  });
-
-  eventsWithElementarySchool.forEach(async (event) => {
-    const isEventWithPrimaryTargetGroup = event.targetGroups.some(
-      (tg) => tg.targetGroupId === PRIMARY
-    );
-
-    if (!isEventWithPrimaryTargetGroup) {
-      console.log(
-        `Adding missing primary school to event ${event.id} ${event.name}`
-      );
-      await prismaClient.event.update({
-        where: {
-          id: event.id,
-        },
-        data: {
-          targetGroups: {
-            create: {
-              targetGroupId: PRIMARY,
-            },
+  try {
+    console.log(`Select all events with elementary school target group`);
+    const eventsWithElementarySchool = await prismaClient.event.findMany({
+      include: { targetGroups: true },
+      where: {
+        targetGroups: {
+          some: {
+            targetGroupId: ELEMENTARY,
           },
         },
-      });
+      },
+    });
+
+    for (const event of eventsWithElementarySchool) {
+      const isEventWithPrimaryTargetGroup = event.targetGroups.some(
+        (tg) => tg.targetGroupId === PRIMARY
+      );
+
+      if (!isEventWithPrimaryTargetGroup) {
+        console.log(
+          `Adding missing primary school to event ${event.id} ${event.name}`
+        );
+
+        await prismaClient.event.update({
+          where: {
+            id: event.id,
+          },
+          data: {
+            targetGroups: {
+              create: {
+                targetGroupId: PRIMARY,
+              },
+            },
+          },
+        });
+      }
     }
-  });
+  } catch (error) {
+    console.error(`Error updating events: ${error}`);
+    throw error;
+  }
 }
 
 async function updateProjects() {
-  console.log(`Select all projects with elementary school target group`);
-  const projectsWithElementarySchool = await prismaClient.project.findMany({
-    include: { targetGroups: true },
-    where: {
-      targetGroups: {
-        some: {
-          targetGroupId: ELEMENTARY,
-        },
-      },
-    },
-  });
-
-  projectsWithElementarySchool.forEach(async (project) => {
-    const isProjectWithPrimaryTargetGroup = project.targetGroups.some(
-      (tg) => tg.targetGroupId === PRIMARY
-    );
-
-    if (!isProjectWithPrimaryTargetGroup) {
-      console.log(
-        `Adding missing primary school to project ${project.id} ${project.name}`
-      );
-      await prismaClient.project.update({
-        where: {
-          id: project.id,
-        },
-        data: {
-          targetGroups: {
-            create: {
-              targetGroupId: PRIMARY,
-            },
+  try {
+    console.log(`Select all projects with elementary school target group`);
+    const projectsWithElementarySchool = await prismaClient.project.findMany({
+      include: { targetGroups: true },
+      where: {
+        targetGroups: {
+          some: {
+            targetGroupId: ELEMENTARY,
           },
         },
-      });
+      },
+    });
+
+    for (const project of projectsWithElementarySchool) {
+      const isProjectWithPrimaryTargetGroup = project.targetGroups.some(
+        (tg) => tg.targetGroupId === PRIMARY
+      );
+
+      if (!isProjectWithPrimaryTargetGroup) {
+        console.log(
+          `Adding missing primary school to project ${project.id} ${project.name}`
+        );
+
+        await prismaClient.project.update({
+          where: {
+            id: project.id,
+          },
+          data: {
+            targetGroups: {
+              create: {
+                targetGroupId: PRIMARY,
+              },
+            },
+          },
+        });
+      }
     }
-  });
+  } catch (error) {
+    console.error(`Error updating projects: ${error}`);
+    throw error;
+  }
 }
 
 async function main() {
-  console.log(`Update Events`);
-  await updateEvents();
+  try {
+    console.log(`Starting update process`);
+    console.log(`Updating events`);
+    await updateEvents();
 
-  console.log(`Update Projects`);
-  await updateProjects();
+    console.log(`Updating projects`);
+    await updateProjects();
 
-  console.log(`Update TargetGroups`);
-  await removeTargetGroupElementary();
+    console.log(`Deleting target group elementary school`);
+    await removeTargetGroupElementary();
 
-  console.log("Done.");
+    console.log(`Update process completed successfully`);
+  } catch (error) {
+    console.error(`Error during update process: ${error}`);
+    throw error;
+  }
 }
 
 main().catch(console.error);
