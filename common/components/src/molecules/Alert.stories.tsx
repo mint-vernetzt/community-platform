2dc48b65de5bdff55496c99f7242c1e6e59d0ef4
Peter Holló <hello@songsforthe.dev>
2023-09-19 11:46:33 +0200

    implement levels

--- common/components/src/molecules/Alert.stories.tsx
+++ common/components/src/molecules/Alert.stories.tsx
@@ -4,16 +4,40 @@ type AlertProps = {
   message: string;
 };
 
+export function AlertWithHTML() {
+  const html = (
+    <>
+      Alert message with <span className="mv-font-bold mv-underline">HTML</span>{" "}
+      content
+    </>
+  );
+
+  return (
+    <div className="mv-flex mv-flex-col mv-gap-2">
+      <Alert>{html}</Alert>
+      <Alert level="attention">{html}</Alert>
+      <Alert level="negative">{html}</Alert>
+    </div>
+  );
+}
+
+AlertWithHTML.storyName = "with HTML";
+
 export function AlertPlayground(props: AlertProps) {
   const { message, ...otherProps } = props;
   return <Alert {...otherProps}>{message}</Alert>;
 }
 AlertPlayground.args = {
   message: "Alert message",
+  level: "positive",
 };
 AlertPlayground.argTypes = {
   message: {
     control: "text",
+  },
+  level: {
+    control: "select",
+    options: ["positive", "attention", "negative"],
   },
 };
 AlertPlayground.storyName = "Playground";
