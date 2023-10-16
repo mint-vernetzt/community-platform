4a3e9685f138fd20cf2003c093f0f49d74a742bc
Peter Holló <hello@songsforthe.dev>
2023-10-16 09:26:55 +0200

    implement back button to project detail

--- app/routes/next/project/$slug/detail.tsx
+++ app/routes/next/project/$slug/detail.tsx
@@ -1,7 +1,34 @@
-import { TabBar } from "@mint-vernetzt/components";
-import { Link, NavLink, Outlet, useMatches } from "@remix-run/react";
+import { TabBar, TextButton } from "@mint-vernetzt/components";
+import { json, type DataFunctionArgs } from "@remix-run/node";
+import { Link, Outlet, useLoaderData, useMatches } from "@remix-run/react";
+
+import { createAuthClient, getSessionUser } from "~/auth.server";
+import { prismaClient } from "~/prisma.server";
+
+export const loader = async (args: DataFunctionArgs) => {
+  const { request } = args;
+  const response = new Response();
+
+  const authClient = createAuthClient(request, response);
+  const sessionUser = await getSessionUser(authClient);
+
+  let username: string | null = null;
+
+  if (sessionUser !== null) {
+    const profile = await prismaClient.profile.findFirst({
+      where: { id: sessionUser.id },
+      select: { username: true },
+    });
+    if (profile !== null) {
+      username = profile.username;
+    }
+  }
+
+  return json({ username }, { headers: response.headers });
+};
 
 function ProjectDetail() {
+  const loaderData = useLoaderData<typeof loader>();
   const matches = useMatches();
   let pathname = "";
 
@@ -13,16 +40,28 @@ function ProjectDetail() {
 
   return (
     <>
+      {loaderData.username !== null && (
+        <>
+          <TextButton weight="thin" variant="neutral" arrowLeft>
+            <Link
+              to={`/profile/${loaderData.username}/#projects`}
+              prefetch="intent"
+            >
+              Meine Projekte
+            </Link>
+          </TextButton>
+        </>
+      )}
       <h1>Project Detail</h1>
       <TabBar>
         <TabBar.Item active={pathname.endsWith("/about")}>
-          <NavLink to="./about">about</NavLink>
+          <Link to="./about">about</Link>
         </TabBar.Item>
         <TabBar.Item active={pathname.endsWith("/requirements")}>
-          <NavLink to="./requirements">requirements</NavLink>
+          <Link to="./requirements">requirements</Link>
         </TabBar.Item>
         <TabBar.Item active={pathname.endsWith("/attachments")}>
-          <NavLink to="./attachments">attachments</NavLink>
+          <Link to="./attachments">attachments</Link>
         </TabBar.Item>
       </TabBar>
       <Link to="./../settings">⚙️</Link>
