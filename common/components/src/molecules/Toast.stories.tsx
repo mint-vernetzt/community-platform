7d077e2de412e759deb0456e62b5f3c893b199dd
Peter Holló <hello@songsforthe.dev>
2023-10-25 11:06:34 +0200

    implement toast component

---
+++ common/components/src/molecules/Toast.stories.tsx
@@ -0,0 +1,51 @@
+import Toast, { type ToastVariant } from "./Toast";
+
+export function ToastPlayground(props: {
+  text: string;
+  delay: number;
+  variant: ToastVariant;
+}) {
+  const { text, ...otherProps } = props;
+  return (
+    <div className="mv-m-2">
+      <Toast {...otherProps}>{text}</Toast>
+    </div>
+  );
+}
+ToastPlayground.storyName = "Playground";
+ToastPlayground.args = {
+  text: "Toast",
+  variant: "positive",
+  delay: 2000,
+};
+ToastPlayground.argTypes = {
+  text: {
+    control: "text",
+  },
+  variant: {
+    control: "select",
+    options: ["positive", "attention", "negative"],
+  },
+  delay: {
+    control: "number",
+  },
+};
+
+ToastPlayground.parameters = {
+  controls: { disable: false },
+  viewport: {
+    defaultViewport: "xl",
+  },
+  layout: "fullscreen",
+};
+
+export default {
+  title: "Molecules/Toast",
+  component: Toast,
+  parameters: {
+    controls: { disable: true },
+    actions: {
+      disable: true,
+    },
+  },
+};
