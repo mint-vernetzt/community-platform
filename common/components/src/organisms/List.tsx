06b64fab84aa2d371f95a3a538eeff5506bc69d8
Peter Holló <hello@songsforthe.dev>
2023-10-26 14:28:06 +0200

    handle border and interactions

--- common/components/src/organisms/List.tsx
+++ common/components/src/organisms/List.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Avatar from "../molecules/Avatar";
+import classNames from "classnames";
 
 function List() {
   return <>List</>;
@@ -57,30 +58,80 @@ function ListItemControls(props: React.PropsWithChildren<{}>) {
 
 export type ListItemProps = {
   noBorder?: boolean;
+  interactive?: boolean;
 };
 
-export function ListItem(props: React.PropsWithChildren<{}>) {
+export function ListItem(props: React.PropsWithChildren<ListItemProps>) {
+  const { noBorder = false, interactive = false } = props;
+
   const validChildren = React.Children.toArray(props.children).filter(
     (child) => {
       return React.isValidElement(child);
     }
   );
 
-  const avatar = validChildren.find((child) => {
-    return React.isValidElement(child) && child.type === Avatar;
-  });
-  const info = validChildren.find((child) => {
-    return React.isValidElement(child) && child.type === ListItemInfo;
-  });
   const controls = validChildren.find((child) => {
     return React.isValidElement(child) && child.type === ListItemControls;
   });
 
+  let avatar: React.ReactElement | undefined;
+  let info: React.ReactElement | undefined;
+
+  const listItemClasses = classNames(
+    "mv-border mv-rounded-md mv-list-none",
+    noBorder ? "mv-border-transparent" : "mv-border-neutral-200",
+    interactive && "hover:mv-bg-primary-50"
+  );
+
+  const containerClasses = classNames(
+    "mv-p-4 mv-flex mv-flex-row mv-items-center mv-no-wrap mv-gap-2"
+  );
+
+  if (interactive) {
+    const childrenWithoutControls = validChildren.filter((child) => {
+      return React.isValidElement(child) && child.type !== ListItemControls;
+    });
+    const wrapper = childrenWithoutControls[0];
+    const wrapperChildren = React.Children.toArray(
+      (wrapper as React.ReactElement).props.children
+    );
+    avatar = wrapperChildren.find((child) => {
+      return React.isValidElement(child) && child.type === Avatar;
+    }) as React.ReactElement;
+    info = wrapperChildren.find((child) => {
+      return React.isValidElement(child) && child.type === ListItemInfo;
+    }) as React.ReactElement;
+
+    const wrapperClasses = classNames(containerClasses, "mv-flex-1");
+
+    const wrapperClone = React.cloneElement(
+      wrapper as React.ReactElement,
+      {
+        className: wrapperClasses,
+      },
+      <>
+        {typeof avatar !== "undefined" && avatar}
+        {typeof info !== "undefined" && info}
+      </>
+    );
+
+    return <li className={listItemClasses}>{wrapperClone}</li>;
+  }
+
+  avatar = validChildren.find((child) => {
+    return React.isValidElement(child) && child.type === Avatar;
+  }) as React.ReactElement;
+  info = validChildren.find((child) => {
+    return React.isValidElement(child) && child.type === ListItemInfo;
+  }) as React.ReactElement;
+
   return (
-    <li className="mv-flex mv-flex-row mv-items-center mv-no-wrap mv-p-4 mv-gap-2 mv-border mv-rounded">
-      {typeof avatar !== "undefined" && avatar}
-      {typeof info !== "undefined" && info}
-      {typeof controls !== "undefined" && controls}
+    <li className={listItemClasses}>
+      <div className={containerClasses}>
+        {typeof avatar !== "undefined" && avatar}
+        {typeof info !== "undefined" && info}
+        {typeof controls !== "undefined" && controls}
+      </div>
     </li>
   );
 }
