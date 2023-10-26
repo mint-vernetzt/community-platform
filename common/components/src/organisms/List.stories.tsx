c130beb06d35f9ff26e750e4d25cd06ff95cb1f4
Peter Holló <hello@songsforthe.dev>
2023-10-26 14:28:06 +0200

    handle border and interactions

--- common/components/src/organisms/List.stories.tsx
+++ common/components/src/organisms/List.stories.tsx
@@ -2,9 +2,9 @@ import Button from "../molecules/Button";
 import Avatar from "../molecules/Avatar";
 import { ListItem } from "./List";
 
-export function LinkItemPlayground() {
+export function ListItemPlayground() {
   return (
-    <div className="mv-p-6">
+    <div className="mv-p-6 mv-flex mv-flex-col mv-gap-4">
       <ListItem>
         <Avatar
           firstName="Maria"
@@ -20,13 +20,27 @@ export function LinkItemPlayground() {
           <Button variant="outline">Edit</Button>
         </ListItem.Controls>
       </ListItem>
+      <ListItem interactive noBorder>
+        <a href="#">
+          <Avatar
+            firstName="Maria"
+            lastName="Lupan"
+            avatar="./maria-lupan-fE5IaNta2KM-unsplash.jpg"
+            size="lg"
+          />
+          <ListItem.Info>
+            <ListItem.Title>Item title</ListItem.Title>
+            <ListItem.Subtitle>Item subtitle</ListItem.Subtitle>
+          </ListItem.Info>
+        </a>
+      </ListItem>
     </div>
   );
 }
-LinkItemPlayground.storyName = "item";
-LinkItemPlayground.args = {};
-LinkItemPlayground.argTypes = {};
-LinkItemPlayground.parameters = {
+ListItemPlayground.storyName = "Playground (item)";
+ListItemPlayground.args = {};
+ListItemPlayground.argTypes = {};
+ListItemPlayground.parameters = {
   controls: { disable: false },
   viewport: {
     defaultViewport: "xl",
