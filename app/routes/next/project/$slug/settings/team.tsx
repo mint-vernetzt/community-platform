0a3d4c903b1a095083500187c9cae202510b87d5
Peter Holló <hello@songsforthe.dev>
2023-10-30 18:11:42 +0100

    implement adding and removing team members

--- app/routes/next/project/$slug/settings/team.tsx
+++ app/routes/next/project/$slug/settings/team.tsx
@@ -1,15 +1,28 @@
-import { redirect, type DataFunctionArgs, json } from "@remix-run/node";
-import { useLoaderData, useLocation } from "@remix-run/react";
+import { conform, useForm } from "@conform-to/react";
+import { parse } from "@conform-to/zod";
+import { Avatar, Button, List, Toast } from "@mint-vernetzt/components";
+import { json, redirect, type DataFunctionArgs } from "@remix-run/node";
+import {
+  Form,
+  useActionData,
+  useLoaderData,
+  useLocation,
+  useSearchParams,
+} from "@remix-run/react";
+import { GravityType } from "imgproxy/dist/types";
+import { z } from "zod";
 import { createAuthClient, getSessionUser } from "~/auth.server";
-import { invariantResponse } from "~/lib/utils/response";
-import { BackButton } from "./__components";
-import { getRedirectPathOnProtectedProjectRoute } from "./utils.server";
 import { H2 } from "~/components/Heading/Heading";
+import { getImageURL } from "~/images.server";
+import { invariantResponse } from "~/lib/utils/response";
 import { prismaClient } from "~/prisma.server";
 import { getPublicURL } from "~/storage.server";
-import { getImageURL } from "~/images.server";
-import { GravityType } from "imgproxy/dist/types";
-import { Avatar, List } from "@mint-vernetzt/components";
+import { BackButton } from "./__components";
+import { getRedirectPathOnProtectedProjectRoute } from "./utils.server";
+
+const searchSchema = z.object({
+  search: z.string().min(3).optional(),
+});
 
 export const loader = async (args: DataFunctionArgs) => {
   const { request, params } = args;
@@ -35,6 +48,7 @@ export const loader = async (args: DataFunctionArgs) => {
     return redirect(redirectPath, { headers: response.headers });
   }
 
+  // get project team members and admins
   const project = await prismaClient.project.findFirst({
     where: { slug: params.slug },
     include: {
@@ -62,11 +76,11 @@ export const loader = async (args: DataFunctionArgs) => {
     },
   });
 
-  invariantResponse(project !== null, "No found", {
+  invariantResponse(project !== null, "Not found", {
     status: 404,
   });
 
-  console.log(project);
+  // enhance team members with avatar
   project.teamMembers = project.teamMembers.map((relation) => {
     let avatar = relation.profile.avatar;
     if (avatar !== null) {
@@ -81,12 +95,154 @@ export const loader = async (args: DataFunctionArgs) => {
     return { profile: { ...relation.profile, avatar } };
   });
 
-  return json({ project }, { headers: response.headers });
+  // get search query
+  const url = new URL(request.url);
+  const queryString = url.searchParams.get("search") || undefined;
+  const query =
+    typeof queryString !== "undefined" ? queryString.split(" ") : [];
+
+  // get profiles via search query
+  let searchResult: {
+    firstName: string;
+    lastName: string;
+    username: string;
+    avatar: string | null;
+  }[] = [];
+  if (query.length > 0) {
+    const whereQueries = [];
+    for (const word of query) {
+      whereQueries.push({
+        OR: [
+          { firstName: { contains: word } },
+          { lastName: { contains: word } },
+          { username: { contains: word } },
+          { email: { contains: word } },
+        ],
+      });
+    }
+    searchResult = await prismaClient.profile.findMany({
+      where: {
+        AND: whereQueries,
+      },
+      select: {
+        firstName: true,
+        lastName: true,
+        username: true,
+        avatar: true,
+      },
+      take: 10,
+    });
+    searchResult = searchResult.filter((relation) => {
+      const isTeamMember = project.teamMembers.some((teamMember) => {
+        return teamMember.profile.username === relation.username;
+      });
+      return !isTeamMember;
+    });
+    searchResult = searchResult.map((relation) => {
+      let avatar = relation.avatar;
+      if (avatar !== null) {
+        const publicURL = getPublicURL(authClient, avatar);
+        if (publicURL !== null) {
+          avatar = getImageURL(publicURL, {
+            resize: { type: "fill", width: 64, height: 64 },
+            gravity: GravityType.center,
+          });
+        }
+      }
+      return { ...relation, avatar };
+    });
+  }
+
+  return json({ project, searchResult }, { headers: response.headers });
+};
+
+export const action = async (args: DataFunctionArgs) => {
+  // get action type
+  const { request } = args;
+  const formData = await request.formData();
+  const action = formData.get(conform.INTENT) as string;
+  if (action.startsWith("add_")) {
+    const username = action.replace("add_", "");
+
+    const project = await prismaClient.project.findFirst({
+      where: { slug: args.params.slug },
+      select: {
+        id: true,
+      },
+    });
+
+    const profile = await prismaClient.profile.findFirst({
+      where: { username },
+      select: {
+        id: true,
+        firstName: true,
+        lastName: true,
+      },
+    });
+
+    invariantResponse(project !== null && profile !== null, "Not found", {
+      status: 404,
+    });
+
+    await prismaClient.teamMemberOfProject.create({
+      data: {
+        projectId: project.id,
+        profileId: profile.id,
+      },
+    });
+
+    return json({ success: true, action, profile });
+  } else if (action.startsWith("remove_")) {
+    const username = action.replace("remove_", "");
+
+    const project = await prismaClient.project.findFirst({
+      where: { slug: args.params.slug },
+      select: {
+        id: true,
+      },
+    });
+
+    const profile = await prismaClient.profile.findFirst({
+      where: { username },
+      select: {
+        id: true,
+        firstName: true,
+        lastName: true,
+      },
+    });
+
+    invariantResponse(project !== null && profile !== null, "Not found", {
+      status: 404,
+    });
+
+    await prismaClient.teamMemberOfProject.delete({
+      where: {
+        profileId_projectId: {
+          projectId: project.id,
+          profileId: profile.id,
+        },
+      },
+    });
+
+    return json({ success: true, action, profile });
+  }
+
+  return null;
 };
 
 function Team() {
-  const { project } = useLoaderData<typeof loader>();
+  const { project, searchResult } = useLoaderData<typeof loader>();
+  const actionData = useActionData<typeof action>();
+  const [searchParams] = useSearchParams();
   const location = useLocation();
+
+  const [searchForm, fields] = useForm({
+    shouldValidate: "onSubmit",
+    onValidate: (values) => {
+      return parse(values.formData, { schema: searchSchema });
+    },
+    shouldRevalidate: "onInput",
+  });
 
   return (
     <>
@@ -94,26 +250,97 @@ function Team() {
       <p>Füge Teammitglieder zu Deinem Projekt hinzu oder entferne sie.</p>
       <H2 as="h4">Aktuelle Teammitglieder</H2>
       <p>Teammitglieder und Rollen sind hier aufgelistet.</p>
-      <List>
-        {project.teamMembers.map((teamMember) => {
-          return (
-            <List.Item key={teamMember.profile.username}>
-              <Avatar {...teamMember.profile} />
-              <List.Item.Title>
-                {teamMember.profile.firstName} {teamMember.profile.lastName}
-              </List.Item.Title>
-              <List.Item.Subtitle>
-                {project.admins.some((admin) => {
-                  return admin.profile.username === teamMember.profile.username;
-                })
-                  ? "Administrator:in"
-                  : "Teammitglied"}
-              </List.Item.Subtitle>
-            </List.Item>
-          );
-        })}
-      </List>
+      <Form method="post">
+        <List>
+          {project.teamMembers.map((teamMember) => {
+            return (
+              <List.Item key={teamMember.profile.username}>
+                <Avatar {...teamMember.profile} />
+                <List.Item.Title>
+                  {teamMember.profile.firstName} {teamMember.profile.lastName}
+                </List.Item.Title>
+                <List.Item.Subtitle>
+                  {project.admins.some((admin) => {
+                    return (
+                      admin.profile.username === teamMember.profile.username
+                    );
+                  })
+                    ? "Administrator:in"
+                    : "Teammitglied"}
+                </List.Item.Subtitle>
+                <List.Item.Controls>
+                  <Button
+                    name={conform.INTENT}
+                    variant="outline"
+                    value={`remove_${teamMember.profile.username}`}
+                    type="submit"
+                  >
+                    Entfernen
+                  </Button>
+                </List.Item.Controls>
+              </List.Item>
+            );
+          })}
+        </List>
+        {/* TODO: resolve type issues */}
+        {typeof actionData !== "undefined" &&
+          actionData !== null &&
+          actionData.success === true &&
+          actionData.action.startsWith("remove_") && (
+            <Toast key="delete-toast">
+              {actionData.profile.firstName} {actionData.profile.lastName}{" "}
+              entfernt.
+            </Toast>
+          )}
+      </Form>
       <H2 as="h4">Teammitglied hinzufügen</H2>
+      <Form method="get" {...searchForm.props}>
+        <input
+          className="mv-border"
+          name="search"
+          defaultValue={searchParams.get("search") || ""}
+        />
+
+        <p id={fields.search.errorId}>{fields.search.error}</p>
+
+        {/* <noscript> */}
+        <button type="submit">Suchen</button>
+        {/* </noscript> */}
+      </Form>
+      <Form method="post">
+        <List>
+          {searchResult.map((profile) => {
+            return (
+              <List.Item key={profile.username}>
+                <Avatar {...profile} />
+                <List.Item.Title>
+                  {profile.firstName} {profile.lastName}
+                </List.Item.Title>
+                <List.Item.Controls>
+                  <Button
+                    name={conform.INTENT}
+                    variant="outline"
+                    value={`add_${profile.username}`}
+                    type="submit"
+                  >
+                    Hinzufügen
+                  </Button>
+                </List.Item.Controls>
+              </List.Item>
+            );
+          })}
+        </List>
+        {/* TODO: resolve type issues */}
+        {typeof actionData !== "undefined" &&
+          actionData !== null &&
+          actionData.success === true &&
+          actionData.action.startsWith("add_") && (
+            <Toast key="add-toast">
+              {actionData.profile.firstName} {actionData.profile.lastName}{" "}
+              hinzugefügt.
+            </Toast>
+          )}
+      </Form>
     </>
   );
 }
