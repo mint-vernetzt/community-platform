411ed0740187f8aee84d0ef755db9555a72ed872
nac62116 <56918308+nac62116@users.noreply.github.com>
2023-11-06 12:49:44 +0100

    feat: project settings web and social (#1095)
    * Add conform and action to project settings web-social
    * Add mastodon and bluesky to Project model
    * Add loader and frontend
    * Remigrate because of typo...
    * Set published on seed projects
    * Show own unpublished projects on profile detail
    * Add todo for disabled attribute
    * Add autoFocus to first input
    * Remigrate because of typo (once again...)
    * Use complete if statement with brackets
    * Add response headers

--- app/lib/utils/schemas.ts
+++ app/lib/utils/schemas.ts
@@ -7,3 +7,139 @@ export const fileUploadSchema = z.object({
   uploadKey: uploadKey,
   redirect: z.string(),
 });
+
+function addProtocolToUrl(url: string) {
+  let validUrl = url.trim();
+  if (validUrl !== "" && validUrl.search(/^https?:\/\//) === -1) {
+    validUrl = `https://${validUrl}`;
+  }
+  return validUrl;
+}
+
+export const websiteSchema = z
+  .string()
+  .regex(
+    /(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/gi,
+    {
+      message: "Deine Eingabe entspricht nicht dem Format einer Website URL.",
+    }
+  )
+  .optional()
+  .transform((value) => {
+    if (value === undefined) {
+      return null;
+    }
+    return addProtocolToUrl(value);
+  });
+
+export const facebookSchema = z
+  .string()
+  .regex(/(https?:\/\/)?(.*\.)?facebook.com\/.+$|^$/, {
+    message:
+      "Deine Eingabe entspricht nicht dem Format einer Facebook Seite (facebook.com/...).",
+  })
+  .optional()
+  .transform((value) => {
+    if (value === undefined) {
+      return null;
+    }
+    return addProtocolToUrl(value);
+  });
+
+export const linkedinSchema = z
+  .string()
+  .regex(/(https?:\/\/)?(.*\.)?linkedin.com\/.+$|^$/, {
+    message:
+      "Deine Eingabe entspricht nicht dem Format einer LinkedIn Seite (linkedin.com/...).",
+  })
+  .optional()
+  .transform((value) => {
+    if (value === undefined) {
+      return null;
+    }
+    return addProtocolToUrl(value);
+  });
+
+export const xingSchema = z
+  .string()
+  .regex(/(https?:\/\/)?(.*\.)?xing.com\/.+$|^$/, {
+    message:
+      "Deine Eingabe entspricht nicht dem Format einer Xing Seite (xing.com/...).",
+  })
+  .optional()
+  .transform((value) => {
+    if (value === undefined) {
+      return null;
+    }
+    return addProtocolToUrl(value);
+  });
+
+export const twitterSchema = z
+  .string()
+  .regex(/(https?:\/\/)?(.*\.)?twitter.com\/.+$|^$/, {
+    message:
+      "Deine Eingabe entspricht nicht dem Format einer Twitter Seite (twitter.com/...).",
+  })
+  .optional()
+  .transform((value) => {
+    if (value === undefined) {
+      return null;
+    }
+    return addProtocolToUrl(value);
+  });
+
+export const mastodonSchema = z
+  .string()
+  .regex(/(https?:\/\/)?(.*\.)?mastodon.social\/.+$|^$/, {
+    message:
+      "Deine Eingabe entspricht nicht dem Format einer Mastodon Seite (mastodon.social/...).",
+  })
+  .optional()
+  .transform((value) => {
+    if (value === undefined) {
+      return null;
+    }
+    return addProtocolToUrl(value);
+  });
+
+export const blueskySchema = z
+  .string()
+  .regex(/(https?:\/\/)?(.*\.)?bsky.app\/.+$|^$/, {
+    message:
+      "Deine Eingabe entspricht nicht dem Format einer Blue Sky Seite (bsky.app/...).",
+  })
+  .optional()
+  .transform((value) => {
+    if (value === undefined) {
+      return null;
+    }
+    return addProtocolToUrl(value);
+  });
+
+export const instagramSchema = z
+  .string()
+  .regex(/(https?:\/\/)?(.*\.)?instagram.com\/.+$|^$/, {
+    message:
+      "Deine Eingabe entspricht nicht dem Format einer Instagram Seite (instagram.com/...).",
+  })
+  .optional()
+  .transform((value) => {
+    if (value === undefined) {
+      return null;
+    }
+    return addProtocolToUrl(value);
+  });
+
+export const youtubeSchema = z
+  .string()
+  .regex(/(https?:\/\/)?(.*\.)?youtube.com\/.+$|^$/, {
+    message:
+      "Deine Eingabe entspricht nicht dem Format einer Youtube Seite (youtube.com/...).",
+  })
+  .optional()
+  .transform((value) => {
+    if (value === undefined) {
+      return null;
+    }
+    return addProtocolToUrl(value);
+  });
--- app/public-fields-filtering.server.ts
+++ app/public-fields-filtering.server.ts
@@ -490,6 +490,8 @@ export async function filterProjectByVisibility<
         key === "youtube" ||
         key === "instagram" ||
         key === "xing" ||
+        key === "mastodon" ||
+        key === "bluesky" ||
         key === "backgroundImage" ||
         key === "backgroundImageId" ||
         key === "city"
