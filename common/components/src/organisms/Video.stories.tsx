cace0581bb84555da85243300f788541210d6ce7
Peter Holló <hello@songsforthe.dev>
2023-11-08 21:26:37 +0100

    add video subline

--- common/components/src/organisms/Video.stories.tsx
+++ common/components/src/organisms/Video.stories.tsx
@@ -1,15 +1,32 @@
-import Video, { type VideoProps } from "./Video";
+import Video from "./Video";
 
-export function VideoPlayground(props: VideoProps) {
-  return <Video src={props.src} />;
+type VideoPlaygroundProps = {
+  src: string;
+  subline: string;
+};
+
+export function VideoPlayground(props: VideoPlaygroundProps) {
+  return (
+    <Video src={props.src}>
+      {typeof props.subline !== "undefined" && props.subline === "" && (
+        <Video.Subline>{props.subline}</Video.Subline>
+      )}
+    </Video>
+  );
 }
 VideoPlayground.storyName = "Playground";
 VideoPlayground.args = {
   src: "https://www.youtube-nocookie.com/embed/aDaOgu2CQtI",
+  subline: "Pearl Jam - Do the Evolution (Official Video)",
 };
 
 VideoPlayground.argTypes = {
   src: {
+    control: {
+      type: "text",
+    },
+  },
+  subline: {
     control: {
       type: "text",
     },
