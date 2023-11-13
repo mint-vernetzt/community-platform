e0b144616da40815001cc9ec58901eed69fa5ebf
nac62116 <56918308+nac62116@users.noreply.github.com>
2023-11-13 14:43:06 +0100

    Add loader and action to settings requirements

--- app/routes/next/project/$slug/settings/requirements.tsx
+++ app/routes/next/project/$slug/settings/requirements.tsx
@@ -3,9 +3,83 @@ import { useLoaderData, useLocation } from "@remix-run/react";
 import { createAuthClient, getSessionUser } from "~/auth.server";
 import { invariantResponse } from "~/lib/utils/response";
 import { BackButton } from "./__components";
-import { getRedirectPathOnProtectedProjectRoute } from "./utils.server";
+import {
+  getRedirectPathOnProtectedProjectRoute,
+  getSubmissionHash,
+} from "./utils.server";
 import { Section } from "@mint-vernetzt/components";
 import { prismaClient } from "~/prisma.server";
+import { conform, list, useFieldList, useForm } from "@conform-to/react";
+import { getFieldsetConstraint, parse } from "@conform-to/zod";
+import { z } from "zod";
+
+const requirementsSchema = z.object({
+  timeframe: z
+    .string()
+    .max(
+      200,
+      "Deine Eingabe übersteigt die maximal zulässige Zeichenzahl von 200."
+    )
+    .optional()
+    .transform((value) => (value === undefined ? null : value)),
+  jobFillings: z
+    .string()
+    .max(
+      500,
+      "Deine Eingabe übersteigt die maximal zulässige Zeichenzahl von 500."
+    )
+    .optional()
+    .transform((value) => (value === undefined ? null : value)),
+  yearlyBudget: z
+    .string()
+    .max(
+      80,
+      "Deine Eingabe übersteigt die maximal zulässige Zeichenzahl von 80."
+    )
+    .optional()
+    .transform((value) => (value === undefined ? null : value)),
+  financings: z.array(z.string().uuid()),
+  furtherFinancings: z
+    .string()
+    .max(
+      500,
+      "Deine Eingabe übersteigt die maximal zulässige Zeichenzahl von 500."
+    )
+    .optional()
+    .transform((value) => (value === undefined ? null : value)),
+  technicalRequirements: z
+    .string()
+    .max(
+      500,
+      "Deine Eingabe übersteigt die maximal zulässige Zeichenzahl von 500."
+    )
+    .optional()
+    .transform((value) => (value === undefined ? null : value)),
+  furtherTechnicalRequirements: z
+    .string()
+    .max(
+      500,
+      "Deine Eingabe übersteigt die maximal zulässige Zeichenzahl von 500."
+    )
+    .optional()
+    .transform((value) => (value === undefined ? null : value)),
+  roomSituation: z
+    .string()
+    .max(
+      200,
+      "Deine Eingabe übersteigt die maximal zulässige Zeichenzahl von 200."
+    )
+    .optional()
+    .transform((value) => (value === undefined ? null : value)),
+  furtherRoomSituation: z
+    .string()
+    .max(
+      200,
+      "Deine Eingabe übersteigt die maximal zulässige Zeichenzahl von 200."
+    )
+    .optional()
+    .transform((value) => (value === undefined ? null : value)),
+});
 
 export const loader = async (args: DataFunctionArgs) => {
   const { request, params } = args;
@@ -35,21 +109,140 @@ export const loader = async (args: DataFunctionArgs) => {
     status: 403,
   });
 
-  const profile = await prismaClient.profile.findFirst({
+  const project = await prismaClient.project.findUnique({
     select: {
-      firstName: true,
+      timeframe: true,
+      jobFillings: true,
+      yearlyBudget: true,
+      furtherFinancings: true,
+      technicalRequirements: true,
+      furtherTechnicalRequirements: true,
+      roomSituation: true,
+      furtherRoomSituation: true,
+      financings: {
+        select: {
+          financing: {
+            select: {
+              id: true,
+              title: true,
+            },
+          },
+        },
+      },
     },
     where: {
-      id: sessionUser.id,
+      slug: params.slug,
     },
   });
-
-  invariantResponse(profile !== null, "Profile not found", {
+  invariantResponse(project !== null, "Project not found", {
     status: 404,
   });
 
-  return json({ profile }, { headers: response.headers });
+  const allFinancings = await prismaClient.financing.findMany({
+    select: {
+      id: true,
+      title: true,
+    },
+  });
+
+  return json({ project, allFinancings }, { headers: response.headers });
 };
+
+export async function action({ request, params }: DataFunctionArgs) {
+  const response = new Response();
+  const authClient = createAuthClient(request, response);
+  const sessionUser = await getSessionUser(authClient);
+  // check slug exists (throw bad request if not)
+  invariantResponse(params.slug !== undefined, "No valid route", {
+    status: 400,
+  });
+  const redirectPath = await getRedirectPathOnProtectedProjectRoute({
+    request,
+    slug: params.slug,
+    sessionUser,
+    authClient,
+  });
+  if (redirectPath !== null) {
+    return redirect(redirectPath, { headers: response.headers });
+  }
+  const project = await prismaClient.project.findUnique({
+    select: {
+      id: true,
+    },
+    where: {
+      slug: params.slug,
+    },
+  });
+  invariantResponse(project !== null, "Project not found", {
+    status: 404,
+  });
+  // Validation
+  const formData = await request.formData();
+  const submission = await parse(formData, {
+    schema: (intent) =>
+      requirementsSchema.transform(async (data, ctx) => {
+        if (intent !== "submit") return { ...data };
+
+        const { financings, ...rest } = data;
+
+        try {
+          await prismaClient.project.update({
+            where: {
+              slug: params.slug,
+            },
+            data: {
+              ...rest,
+              financings: {
+                deleteMany: {},
+                connectOrCreate: financings.map((financingId: string) => {
+                  return {
+                    where: {
+                      financingId_projectId: {
+                        financingId: financingId,
+                        projectId: project.id,
+                      },
+                    },
+                    create: {
+                      financingId,
+                    },
+                  };
+                }),
+              },
+            },
+          });
+        } catch (e) {
+          console.warn(e);
+          ctx.addIssue({
+            code: "custom",
+            message:
+              "Die Daten konnten nicht gespeichert werden. Bitte versuche es erneut oder wende dich an den Support.",
+          });
+          return z.NEVER;
+        }
+
+        return { ...data };
+      }),
+    async: true,
+  });
+
+  const hash = getSubmissionHash(submission);
+
+  if (submission.intent !== "submit") {
+    return json({ status: "idle", submission, hash } as const, {
+      headers: response.headers,
+    });
+  }
+  if (!submission.value) {
+    return json({ status: "error", submission, hash } as const, {
+      status: 400,
+      headers: response.headers,
+    });
+  }
+
+  return json({ status: "success", submission, hash } as const, {
+    headers: response.headers,
+  });
+}
 
 function Requirements() {
   const location = useLocation();
@@ -64,7 +257,7 @@ function Requirements() {
             <h2 className="mv-text-primary mv-text-lg mv-font-semibold mv-mb-4">
               Noch in Bearbeitung...
             </h2>
-            <p className="mv-mb-2">Hi {loaderData.profile.firstName},</p>
+            <p className="mv-mb-2">Hi Colin,</p>
             <p className="mv-mb-2">
               dieser Bereich ist noch in Bearbeitung. Bald kannst Du hier der
               Community zeigen unter welchen Rahmenbedingungen das Projekt
