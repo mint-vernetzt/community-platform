deda3f23f04a0dc5b3bc7a97701101d2904327dd
nac62116 <56918308+nac62116@users.noreply.github.com>
2024-01-08 13:13:21 +0100

    Use v1 route convention via package @remix-run/v1-route-convention

--- remix.config.js
+++ remix.config.js
@@ -1,3 +1,5 @@
+const { createRoutesFromFolders } = require("@remix-run/v1-route-convention");
+
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
@@ -10,4 +12,9 @@ module.exports = {
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
   // devServerPort: 8002
+
+  routes(defineRoutes) {
+    // uses the v1 convention, works in v1.15+ and v2
+    return createRoutesFromFolders(defineRoutes);
+  },
 };
