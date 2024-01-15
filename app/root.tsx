1ce443635852cc851af2663b08163f3c3abc19fa
nac62116 <56918308+nac62116@users.noreply.github.com>
2024-01-15 11:37:52 +0100

    fix: scroll-to-top button cuts off query parameter (#1239)

--- app/root.tsx
+++ app/root.tsx
@@ -471,7 +471,7 @@ export default function App() {
       <div className="w-0 h-16"></div>
       <div className="w-0 h-screen sticky top-0">
         <div className="absolute bottom-4 md:bottom-8 -left-20">
-          <Link to="#top">
+          <Link to={`${location.pathname}${location.search}#top`}>
             <CircleButton size="large" floating>
               <svg
                 width="30"
