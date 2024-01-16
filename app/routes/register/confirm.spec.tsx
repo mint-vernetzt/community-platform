820b59dcdffbeeb06fe0a40cea40f2c045ea40ac
nac62116 <56918308+nac62116@users.noreply.github.com>
2024-01-16 17:57:14 +0100

    write test specifications for register/confirm.tsx

---
+++ app/routes/register/confirm.spec.tsx
@@ -0,0 +1,18 @@
+/* 
+
+Unit tests:
+
+Loader:
+- Redirect to /dashboard on authenticated
+- All possible errors are thrown when the confirmation link doesn't have the right structure
+? --> Only login_redirect param to own domain should work (TODO: Not current behaviour)
+- Returns correct data structure
+
+
+Functional tests:
+
+- Render test
+- Link to /register/verify works
+? - No browser console warnings/errors
+
+*/
