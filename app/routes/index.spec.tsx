60abb30542ca7af627b52f3fdd0cb56eb414a9f6
nac62116 <56918308+nac62116@users.noreply.github.com>
2024-01-16 17:42:08 +0100

    write test specifications for index.tsx

---
+++ app/routes/index.spec.tsx
@@ -0,0 +1,34 @@
+/* 
+
+Unit tests:
+
+Loader:
+- Redirect to /dashboard on authenticated
+- Returns correct data structure
+
+Action:
+- Returns correct error messages on validation failure
+- Returns correct error message and no set-cookie header on login failure
+- Throws profile not found when profile === null (TODO: Not current behaviour)
+- Redirects to dashboard with correct set-cookie header on login success
+- Redirects to loginRedirect on login success with loginRedirect as form value
+? --> Only redirect param to own domain should work (TODO: Not current behaviour)
+? --> Trims the form input values and normalizes the email to lower case (TODO: Not current behaviour)
+
+
+Functional tests:
+
+- Render test
+- Login form works (success, failure, login_redirect searchParam)
+--> submit with enter works
+--> validation errors are rendered on the input fields (or on the form if global)
+? --> After first validation the form is revalidated onChange and hides the errors on successful revalidation
+? --> Only redirect param to own domain should work (TODO: Not current behaviour)
+- All links work
+--> Link to register and reset password also ship along the login_redirect searchParam
+- Scroll to intro section works
+? - MINT-ID works (Only check correct redirect on "Anmelden mit MINT-ID" and "MINT-ID erstellen"?)
+? - Entity counter are functional
+? - No browser console warnings/errors
+
+*/
