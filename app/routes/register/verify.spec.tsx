238f66df2bfdd7dfb5fad91af82318cc03254961
nac62116 <56918308+nac62116@users.noreply.github.com>
2024-01-17 14:39:14 +0100

    Update test specification for register/verify.tsx

--- app/routes/register/verify.spec.tsx
+++ app/routes/register/verify.spec.tsx
@@ -9,12 +9,12 @@ Loader:
 ? --> Same behaviour if type param is not "signup"
 - Throws correct error if user meta data is not complete
 - Throws correct error and does not create a profile if session could not be set
-? --> Is the alert even rendered without a default component?
+? --> Is the server side alert even rendered without a default component?
 - Creates profile on correct token and type combination, when session is set and user meta data is complete
 --> The prisma call also creates the two relations ProfileVisibility and notificationSettings
 - The user is logged in after successful verification
 --> set-cookie header is present and valid
---> Redirect to dashboard on success (TODO: Not current behaviour)
+--> Redirect to /profile/$username on success
 --> Redirect to loginRedirect if login_redirect param is specified
 ? --> Only login_redirect param to own domain should work (TODO: Not current behaviour)
 
