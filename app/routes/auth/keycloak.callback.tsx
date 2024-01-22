907d5a06c1e6b7ea86d26344bafaad3d3b9fd82f
nac62116 <56918308+nac62116@users.noreply.github.com>
2024-01-22 21:25:16 +0100

    Clean up verify.tsx and keycloak.callback.tsx

--- app/routes/auth/keycloak.callback.tsx
+++ app/routes/auth/keycloak.callback.tsx
@@ -1,4 +1,4 @@
-import { json, redirect, type LoaderFunctionArgs } from "@remix-run/node";
+import { redirect, type LoaderFunctionArgs } from "@remix-run/node";
 import { createAuthClient, getSession } from "~/auth.server";
 import { invariantResponse } from "~/lib/utils/response";
 import { prismaClient } from "~/prisma.server";
@@ -7,15 +7,12 @@ import { createProfile } from "../register/utils.server";
 export const loader = async (args: LoaderFunctionArgs) => {
   const { request } = args;
   const { authClient, headers } = createAuthClient(request);
-
-  const url = new URL(request.url);
-  const urlSearchParams = new URLSearchParams(url.searchParams);
-
-  // check if user is already logged in and redirect to dashboard or login_redirect
   const session = await getSession(authClient);
   if (session !== null) {
     return redirect("/dashboard");
   }
+  const url = new URL(request.url);
+  const urlSearchParams = new URLSearchParams(url.searchParams);
   const code = urlSearchParams.get("code");
   const loginRedirect = urlSearchParams.get("login_redirect");
   invariantResponse(code !== null, "Bad Request", { status: 400 });
@@ -34,12 +31,11 @@ export const loader = async (args: LoaderFunctionArgs) => {
   const firstLogin = profile === null;
   if (firstLogin) {
     const profile = await createProfile(user);
-    if (profile === null) {
-      throw json(
-        "Did not provide necessary user meta data to create a corresponding profile after sign up.",
-        { status: 400 }
-      );
-    }
+    invariantResponse(
+      profile !== null,
+      "Did not provide necessary user meta data to create a corresponding profile after sign up.",
+      { status: 400 }
+    );
     return redirect(loginRedirect || `/profile/${profile.username}`, {
       headers,
     });
--- app/routes/auth/verify.tsx
+++ app/routes/auth/verify.tsx
@@ -1,69 +1,62 @@
-import { redirect, type LoaderFunctionArgs, json } from "@remix-run/node";
+import { redirect, type LoaderFunctionArgs } from "@remix-run/node";
 import { type EmailOtpType } from "@supabase/supabase-js";
 import { createAuthClient } from "~/auth.server";
 import { createProfile } from "../register/utils.server";
 import { updateProfileEmailByUserId } from "./verify.server";
+import { invariantResponse } from "~/lib/utils/response";
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const requestUrl = new URL(request.url);
   const token_hash = requestUrl.searchParams.get("token_hash");
   const type = requestUrl.searchParams.get("type") as EmailOtpType | null;
-
-  if (token_hash !== null && type !== null) {
-    const { authClient, headers } = createAuthClient(request);
-    const { error, data } = await authClient.auth.verifyOtp({
-      type,
-      token_hash,
-    });
-
-    if (error === null && data.user !== null && data.session !== null) {
-      const user = data.user;
-      let loginRedirect = requestUrl.searchParams.get("login_redirect");
-      if (type === "signup") {
-        const profile = await createProfile(user);
-        if (profile === null) {
-          throw json(
-            "Did not provide necessary user meta data to create a corresponding profile after sign up.",
-            { status: 400 }
-          );
-        }
-        // Supabase defaults the login redirect to "/" if not specified so this will overwrite this behaviour
-        if (loginRedirect === process.env.COMMUNITY_BASE_URL) {
-          loginRedirect = null;
-        }
-        return redirect(loginRedirect || `/profile/${profile.username}`, {
-          headers,
-        });
-      } else if (type === "email_change") {
-        if (user.email === undefined) {
-          throw json({ message: "Server error" }, { status: 500 });
-        }
-        const profile = await updateProfileEmailByUserId(user.id, user.email);
-        // Supabase defaults the login redirect to "/" if not specified so this will overwrite this behaviour
-        if (loginRedirect === process.env.COMMUNITY_BASE_URL) {
-          loginRedirect = null;
-        }
-        return redirect(loginRedirect || `/profile/${profile.username}`, {
-          headers,
-        });
-      } else if (type === "recovery") {
-        // TODO:
-        // Could we reuse the profile/security set password route?
-        // Dont forget to add login redirect
-        // Supabase defaults the login redirect to "/" if not specified so this will overwrite this behaviour
-        if (loginRedirect === process.env.COMMUNITY_BASE_URL) {
-          loginRedirect = null;
-        }
-        return redirect("TODO", { headers });
-      } else {
-        throw json({ message: "Bad request" }, { status: 400 });
-      }
-    } else {
-      throw json(
-        { message: "Server Error during verification" },
-        { status: 500 }
-      );
-    }
+  invariantResponse(token_hash !== null && type !== null, "Bad request", {
+    status: 400,
+  });
+  const { authClient, headers } = createAuthClient(request);
+  const { error, data } = await authClient.auth.verifyOtp({
+    type,
+    token_hash,
+  });
+  invariantResponse(
+    error === null && data.user !== null && data.session !== null,
+    "Server Error during verification",
+    { status: 500 }
+  );
+  invariantResponse(
+    type === "signup" || type === "email_change" || type === "recovery",
+    "Bad request",
+    { status: 400 }
+  );
+  const user = data.user;
+  let loginRedirect = requestUrl.searchParams.get("login_redirect");
+  // Supabase defaults the login redirect to "/" if not specified so this will overwrite this behaviour
+  if (loginRedirect === process.env.COMMUNITY_BASE_URL) {
+    loginRedirect = null;
   }
-  throw json({ message: "Bad request" }, { status: 400 });
+  if (type === "signup") {
+    const profile = await createProfile(user);
+    invariantResponse(
+      profile !== null,
+      "Did not provide necessary user meta data to create a corresponding profile after sign up.",
+      { status: 400 }
+    );
+    return redirect(loginRedirect || `/profile/${profile.username}`, {
+      headers,
+    });
+  }
+  if (type === "email_change") {
+    invariantResponse(user.email !== undefined, "Server error", {
+      status: 500,
+    });
+    const profile = await updateProfileEmailByUserId(user.id, user.email);
+    return redirect(loginRedirect || `/profile/${profile.username}`, {
+      headers,
+    });
+  }
+  if (type === "recovery") {
+    // TODO:
+    // Could we reuse the profile/security set password route?
+    // Dont forget to add login redirect
+    return redirect("TODO", { headers });
+  }
 }
