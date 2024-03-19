18f62ffb707ea3c03408a2eb95cc676543b6d2e1
nac62116 <56918308+nac62116@users.noreply.github.com>
2024-03-19 10:07:10 +0100

    Add feature access to next and explore routes

--- app/routes/explore/index.tsx
+++ app/routes/explore/index.tsx
@@ -1,6 +1,16 @@
-import { redirect } from "@remix-run/node";
+import { type LoaderFunctionArgs, redirect } from "@remix-run/node";
+import { createAuthClient } from "~/auth.server";
+import { getFeatureAbilities } from "~/lib/utils/application";
 
 // handle "/profiles" as default route
-export const loader = async () => {
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const { authClient } = createAuthClient(request);
+
+  const abilities = await getFeatureAbilities(authClient, ["filter"]);
+
+  if (abilities.filter.hasAccess) {
+    return redirect("/next/explore/profiles");
+  }
+
   return redirect("/explore/profiles");
 };
--- app/routes/next/explore/index.tsx
+++ app/routes/next/explore/index.tsx
@@ -1,6 +1,16 @@
-import { redirect } from "@remix-run/node";
+import { type LoaderFunctionArgs, redirect } from "@remix-run/node";
+import { createAuthClient } from "~/auth.server";
+import { getFeatureAbilities } from "~/lib/utils/application";
 
 // handle "/profiles" as default route
-export const loader = async () => {
-  return redirect("/explore/profiles");
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const { authClient } = createAuthClient(request);
+
+  const abilities = await getFeatureAbilities(authClient, ["filter"]);
+
+  if (abilities.filter.hasAccess === false) {
+    return redirect("/explore/profiles");
+  }
+
+  return redirect("/next/explore/profiles");
 };
