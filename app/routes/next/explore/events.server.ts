906bd570531d3f5bd94d3f4f49325a34e120aa46
nac62116 <56918308+nac62116@users.noreply.github.com>
2024-03-20 13:58:52 +0100

    wip: Add new backend to next explore events without timeframe filter

---
+++ app/routes/next/explore/events.server.ts
@@ -0,0 +1,443 @@
+import { json } from "@remix-run/server-runtime";
+import { type User } from "@supabase/supabase-js";
+import { invariantResponse } from "~/lib/utils/response";
+import { type ArrayElement } from "~/lib/utils/types";
+import { prismaClient } from "~/prisma.server";
+import { type GetEventsSchema } from "./events";
+
+export function getTakeParam(page: GetEventsSchema["page"] = 1) {
+  const itemsPerPage = 12;
+  const take = itemsPerPage * page;
+  return take;
+}
+
+export async function getVisibilityFilteredEventsCount(options: {
+  filter: NonNullable<GetEventsSchema["filter"]>;
+}) {
+  const whereClauses = [];
+  const visibilityWhereClauses = [];
+  for (const filterKey in options.filter) {
+    const typedFilterKey = filterKey as keyof typeof options.filter;
+    const visibilityWhereStatement = {
+      eventVisibility: {
+        [`${typedFilterKey}${typedFilterKey === "focus" ? "es" : "s"}`]: false,
+      },
+    };
+    visibilityWhereClauses.push(visibilityWhereStatement);
+
+    for (const slug of options.filter[typedFilterKey]) {
+      const filterWhereStatement = {
+        [`${typedFilterKey}${typedFilterKey === "focus" ? "es" : "s"}`]: {
+          some: {
+            [`${typedFilterKey === "type" ? "eventType" : typedFilterKey}`]: {
+              slug,
+            },
+          },
+        },
+      };
+      whereClauses.push(filterWhereStatement);
+    }
+  }
+  whereClauses.push({ OR: [...visibilityWhereClauses] });
+
+  const count = await prismaClient.event.count({
+    where: {
+      AND: whereClauses,
+    },
+  });
+
+  return count;
+}
+
+export async function getEventsCount(options: {
+  filter: GetEventsSchema["filter"];
+}) {
+  const whereClauses = [];
+  if (options.filter !== undefined) {
+    for (const filterKey in options.filter) {
+      const typedFilterKey = filterKey as keyof typeof options.filter;
+      for (const slug of options.filter[typedFilterKey]) {
+        const filterWhereStatement = {
+          [`${typedFilterKey}${typedFilterKey === "focus" ? "es" : "s"}`]: {
+            some: {
+              [`${typedFilterKey === "type" ? "eventType" : typedFilterKey}`]: {
+                slug,
+              },
+            },
+          },
+        };
+        whereClauses.push(filterWhereStatement);
+      }
+    }
+  }
+
+  const count = await prismaClient.event.count({
+    where: {
+      AND: whereClauses,
+    },
+  });
+
+  return count;
+}
+
+export async function getAllEvents(options: {
+  filter: GetEventsSchema["filter"];
+  sortBy: GetEventsSchema["sortBy"];
+  take: ReturnType<typeof getTakeParam>;
+  isLoggedIn: boolean;
+}) {
+  const whereClauses = [];
+  if (options.filter !== undefined) {
+    for (const filterKey in options.filter) {
+      const typedFilterKey = filterKey as keyof typeof options.filter;
+      if (options.isLoggedIn === false) {
+        const visibilityWhereStatement = {
+          eventVisibility: {
+            [`${typedFilterKey}${typedFilterKey === "focus" ? "es" : "s"}`]:
+              true,
+          },
+        };
+        whereClauses.push(visibilityWhereStatement);
+      }
+      for (const slug of options.filter[typedFilterKey]) {
+        const filterWhereStatement = {
+          [`${typedFilterKey}${typedFilterKey === "focus" ? "es" : "s"}`]: {
+            some: {
+              [`${typedFilterKey === "type" ? "eventType" : typedFilterKey}`]: {
+                slug,
+              },
+            },
+          },
+        };
+        whereClauses.push(filterWhereStatement);
+      }
+    }
+  }
+
+  const events = await prismaClient.event.findMany({
+    select: {
+      id: true,
+      name: true,
+      slug: true,
+      parentEventId: true,
+      startTime: true,
+      endTime: true,
+      participationUntil: true,
+      participationFrom: true,
+      participantLimit: true,
+      background: true,
+      published: true,
+      stage: {
+        select: {
+          title: true,
+          slug: true,
+        },
+      },
+      canceled: true,
+      subline: true,
+      description: true,
+      _count: {
+        select: {
+          childEvents: true,
+          participants: true,
+          responsibleOrganizations: true,
+          waitingList: true,
+        },
+      },
+      responsibleOrganizations: {
+        select: {
+          organization: {
+            select: {
+              id: true,
+              name: true,
+              slug: true,
+              logo: true,
+              organizationVisibility: {
+                select: {
+                  id: true,
+                  name: true,
+                  slug: true,
+                  logo: true,
+                },
+              },
+            },
+          },
+        },
+      },
+      eventVisibility: {
+        select: {
+          id: true,
+          name: true,
+          slug: true,
+          parentEventId: true,
+          startTime: true,
+          endTime: true,
+          participationUntil: true,
+          participationFrom: true,
+          participantLimit: true,
+          background: true,
+          published: true,
+          stage: true,
+          canceled: true,
+          subline: true,
+          description: true,
+          responsibleOrganizations: true,
+        },
+      },
+    },
+    where: {
+      AND: whereClauses,
+    },
+    orderBy:
+      options.sortBy !== undefined
+        ? {
+            [options.sortBy.value]: options.sortBy.direction,
+          }
+        : {
+            name: "asc",
+          },
+    take: options.take,
+  });
+
+  return events;
+}
+
+export async function enhanceEventsWithParticipationStatus(
+  sessionUser: User | null,
+  events: Array<
+    ArrayElement<Awaited<ReturnType<typeof getAllEvents>>> & {
+      blurredBackground: string | undefined;
+    }
+  >
+) {
+  if (sessionUser === null) {
+    const enhancedEvents = events.map((item) => {
+      const isParticipant = false;
+      const isOnWaitingList = false;
+      const isSpeaker = false;
+      const isTeamMember = false;
+
+      return {
+        ...item,
+        isParticipant,
+        isOnWaitingList,
+        isSpeaker,
+        isTeamMember,
+      };
+    });
+    return enhancedEvents;
+  } else {
+    const eventIdsWhereParticipant = (
+      await prismaClient.participantOfEvent.findMany({
+        where: {
+          profileId: sessionUser.id,
+        },
+        select: {
+          eventId: true,
+        },
+      })
+    ).map((event) => event.eventId);
+    const eventIdsWhereOnWaitingList = (
+      await prismaClient.waitingParticipantOfEvent.findMany({
+        where: {
+          profileId: sessionUser.id,
+        },
+        select: {
+          eventId: true,
+        },
+      })
+    ).map((event) => event.eventId);
+    const eventIdsWhereSpeaker = (
+      await prismaClient.speakerOfEvent.findMany({
+        where: {
+          profileId: sessionUser.id,
+        },
+        select: {
+          eventId: true,
+        },
+      })
+    ).map((event) => event.eventId);
+    const eventIdsWhereTeamMember = (
+      await prismaClient.teamMemberOfEvent.findMany({
+        where: {
+          profileId: sessionUser.id,
+        },
+        select: {
+          eventId: true,
+        },
+      })
+    ).map((event) => event.eventId);
+
+    const enhancedEvents = events.map((item) => {
+      const isParticipant = eventIdsWhereParticipant.includes(item.id);
+      const isOnWaitingList = eventIdsWhereOnWaitingList.includes(item.id);
+      const isSpeaker = eventIdsWhereSpeaker.includes(item.id);
+      const isTeamMember = eventIdsWhereTeamMember.includes(item.id);
+
+      return {
+        ...item,
+        isParticipant,
+        isOnWaitingList,
+        isSpeaker,
+        isTeamMember,
+      };
+    });
+    return enhancedEvents;
+  }
+}
+
+export async function getEventFilterVector(options: {
+  filter: GetEventsSchema["filter"];
+}) {
+  let whereClause = "";
+  const whereStatements = [];
+  if (options.filter !== undefined) {
+    for (const filterKey in options.filter) {
+      const typedFilterKey = filterKey as keyof typeof options.filter;
+      // TODO: Union type issue when we add another filter key. Reason is shown below. The select statement can have different signatures because of the relations.
+      /* Example:
+          const test = await prismaClient.eventType.findMany({
+            select: {
+              slug: true,
+              events: {
+                select: {
+                  eventId: true,
+                },
+              },
+            },
+          });
+          const test2 = await prismaClient.area.findMany({
+            select: {
+              slug: true,
+              events: {
+                select: {
+                  areaId: true,
+                },
+              },
+            },
+          });
+          */
+      // Further reading:
+      // https://www.prisma.io/docs/orm/prisma-schema/data-model/table-inheritance#union-types
+      // https://github.com/prisma/prisma/issues/2505
+
+      // I worked arround with an assertion. But if any table except eventTypes remove their slug, this will break and typescript will not warn us.
+      const fakeTypedFilterKey = filterKey as "eventType";
+      let allFilterValues;
+      try {
+        allFilterValues = await prismaClient[
+          `${typedFilterKey === "type" ? "eventType" : fakeTypedFilterKey}`
+        ].findMany({
+          select: {
+            slug: true,
+          },
+        });
+      } catch (error: any) {
+        throw json({ message: "Server error" }, { status: 500 });
+      }
+
+      for (const slug of options.filter[typedFilterKey]) {
+        // Validate slug because of queryRawUnsafe
+        invariantResponse(
+          allFilterValues.some((value) => {
+            return value.slug === slug;
+          }),
+          "Cannot filter by the specified slug.",
+          { status: 400 }
+        );
+        const tuple = `${typedFilterKey}\\:${slug}`;
+        const whereStatement = `filter_vector @@ '${tuple}'::tsquery`;
+        whereStatements.push(whereStatement);
+      }
+    }
+  }
+
+  if (whereStatements.length > 0) {
+    whereClause = `WHERE ${whereStatements.join(" AND ")}`;
+  }
+
+  const filterVector: {
+    attr: keyof NonNullable<typeof options.filter>;
+    value: string[];
+    count: number[];
+  }[] = await prismaClient.$queryRawUnsafe(`
+      SELECT
+        split_part(word, ':', 1) AS attr,
+        array_agg(split_part(word, ':', 2)) AS value,
+        array_agg(ndoc) AS count
+      FROM ts_stat($$
+        SELECT filter_vector
+        FROM events
+        ${whereClause}
+      $$)
+      GROUP BY attr;
+      `);
+
+  return filterVector;
+}
+
+export function getFilterCountForSlug(
+  // TODO: Remove '| null' when slug isn't optional anymore (after migration)
+  slug: string | null,
+  filterVector: Awaited<ReturnType<typeof getEventFilterVector>>,
+  attribute: ArrayElement<
+    Awaited<ReturnType<typeof getEventFilterVector>>
+  >["attr"]
+) {
+  const filterKeyVector = filterVector.find((vector) => {
+    return vector.attr === attribute;
+  });
+
+  if (typeof filterKeyVector === "undefined") {
+    return 0;
+  }
+
+  // TODO: Remove '|| ""' when slug isn't optional anymore (after migration)
+  const valueIndex = filterKeyVector.value.indexOf(slug || "");
+  if (valueIndex === -1) {
+    return 0;
+  }
+
+  const filterCount = filterKeyVector.count[valueIndex];
+
+  return filterCount;
+}
+
+export async function getAllEventTypes() {
+  return await prismaClient.eventType.findMany({
+    orderBy: {
+      title: "asc",
+    },
+    select: {
+      id: true,
+      title: true,
+      slug: true,
+    },
+  });
+}
+
+export async function getAllFocuses() {
+  return await prismaClient.focus.findMany({
+    orderBy: {
+      title: "asc",
+    },
+    select: {
+      id: true,
+      title: true,
+      slug: true,
+    },
+  });
+}
+
+export async function getAllEventTargetGroups() {
+  return await prismaClient.eventTargetGroup.findMany({
+    orderBy: {
+      title: "asc",
+    },
+    select: {
+      id: true,
+      title: true,
+      slug: true,
+    },
+  });
+}
--- app/routes/next/explore/events.tsx
+++ app/routes/next/explore/events.tsx
@@ -1,90 +1,356 @@
+import { parseWithZod } from "@conform-to/zod-v1";
 import { Button, CardContainer, EventCard } from "@mint-vernetzt/components";
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
 import { useFetcher, useLoaderData, useSearchParams } from "@remix-run/react";
 import { utcToZonedTime } from "date-fns-tz";
 import React from "react";
-import { createAuthClient, getSessionUser } from "~/auth.server";
-import { H1 } from "~/components/Heading/Heading";
-import {
-  type PrepareEventsQuery,
-  getPaginationValues,
-  prepareEvents,
-} from "./utils.server";
-import { prismaClient } from "~/prisma.server";
 import { useTranslation } from "react-i18next";
 import { useHydrated } from "remix-utils/use-hydrated";
+import { z } from "zod";
+import { createAuthClient, getSessionUser } from "~/auth.server";
+import { H1 } from "~/components/Heading/Heading";
+import { GravityType, getImageURL } from "~/images.server";
 import { getFeatureAbilities } from "~/lib/utils/application";
+import { invariantResponse } from "~/lib/utils/response";
+import { type ArrayElement } from "~/lib/utils/types";
+import {
+  filterEventByVisibility,
+  filterOrganizationByVisibility,
+} from "~/next-public-fields-filtering.server";
+import { getPublicURL } from "~/storage.server";
+import {
+  enhanceEventsWithParticipationStatus,
+  getAllEventTargetGroups,
+  getAllEventTypes,
+  getAllEvents,
+  getAllFocuses,
+  getEventFilterVector,
+  getEventsCount,
+  getFilterCountForSlug,
+  getTakeParam,
+  getVisibilityFilteredEventsCount,
+} from "./events.server";
+import { getAreaNameBySlug, getAreasBySearchQuery } from "./utils.server";
 
 const i18nNS = ["routes/explore/events"];
 export const handle = {
   i18n: i18nNS,
 };
 
+const sortValues = ["name-asc", "name-desc", "createdAt-desc"] as const;
+
+export type GetEventsSchema = z.infer<typeof getEventsSchema>;
+
+const getEventsSchema = z.object({
+  filter: z
+    .object({
+      type: z.array(z.string()),
+      focus: z.array(z.string()),
+      eventTargetGroup: z.array(z.string()),
+      area: z.array(z.string()),
+    })
+    .optional(),
+  sortBy: z
+    .enum(sortValues)
+    .optional()
+    .transform((sortValue) => {
+      if (sortValue !== undefined) {
+        const splittedValue = sortValue.split("-");
+        return {
+          value: splittedValue[0],
+          direction: splittedValue[1],
+        };
+      }
+      return sortValue;
+    }),
+  page: z.number().optional(),
+  search: z.string().optional(),
+});
+
 export const loader = async (args: LoaderFunctionArgs) => {
   const { request } = args;
-  const { skip, take, page, itemsPerPage } = getPaginationValues(request);
-
+  const url = new URL(request.url);
+  const searchParams = url.searchParams;
+  const submission = parseWithZod(searchParams, {
+    schema: getEventsSchema,
+  });
+  invariantResponse(
+    submission.status === "success",
+    "Validation failed for get request",
+    { status: 400 }
+  );
+  const take = getTakeParam(submission.value.page);
   const { authClient } = createAuthClient(request);
 
   const abilities = await getFeatureAbilities(authClient, ["filter"]);
-
   if (abilities.filter.hasAccess === false) {
-    return redirect("/explore/events");
+    return redirect("/explore/organizations");
   }
 
   const sessionUser = await getSessionUser(authClient);
+  const isLoggedIn = sessionUser !== null;
 
-  const inFuture = true;
-  const futureEventsCount = await prismaClient.event.count({
-    where: {
-      endTime: { gte: new Date() },
-      published: true,
-      OR: [
-        {
-          parentEventId: null,
-          childEvents: {
-            none: {},
-          },
-        },
-        {
-          childEvents: {
-            some: {},
-          },
-        },
-      ],
-    },
+  let filteredByVisibilityCount;
+  if (!isLoggedIn && submission.value.filter !== undefined) {
+    filteredByVisibilityCount = await getVisibilityFilteredEventsCount({
+      filter: submission.value.filter,
+    });
+  }
+  const eventsCount = await getEventsCount({
+    filter: submission.value.filter,
+  });
+  const events = await getAllEvents({
+    filter: submission.value.filter,
+    sortBy: submission.value.sortBy,
+    take,
+    isLoggedIn,
   });
 
-  let events: PrepareEventsQuery = [];
-  if (futureEventsCount - skip > 0) {
-    const actuallyTake = Math.min(take, futureEventsCount - skip);
-    events = await prepareEvents(authClient, sessionUser, inFuture, {
-      skip,
-      take: actuallyTake,
+  const enhancedEvents = [];
+  for (const event of events) {
+    let enhancedEvent = {
+      ...event,
+    };
+
+    // Filtering by visbility settings
+    if (sessionUser === null) {
+      // Filter event
+      enhancedEvent =
+        filterEventByVisibility<typeof enhancedEvent>(enhancedEvent);
+      // Filter responsible Organizations
+      enhancedEvent.responsibleOrganizations =
+        enhancedEvent.responsibleOrganizations.map((relation) => {
+          const filteredOrganization = filterOrganizationByVisibility<
+            typeof relation.organization
+          >(relation.organization);
+          return { ...relation, organization: filteredOrganization };
+        });
+    }
+
+    // Add images from image proxy
+    let blurredBackground;
+    if (enhancedEvent.background !== null) {
+      const publicURL = getPublicURL(authClient, enhancedEvent.background);
+      if (publicURL) {
+        enhancedEvent.background = getImageURL(publicURL, {
+          resize: { type: "fill", width: 594, height: 396 },
+        });
+      }
+      blurredBackground = getImageURL(publicURL, {
+        resize: { type: "fill", width: 18, height: 12 },
+        blur: 5,
+      });
+    } else {
+      enhancedEvent.background = "/images/default-event-background.jpg";
+      blurredBackground = "/images/default-event-background-blurred.jpg";
+    }
+
+    enhancedEvent.responsibleOrganizations =
+      enhancedEvent.responsibleOrganizations.map((relation) => {
+        let logo = relation.organization.logo;
+        if (logo !== null) {
+          const publicURL = getPublicURL(authClient, logo);
+          if (publicURL) {
+            logo = getImageURL(publicURL, {
+              resize: { type: "fill", width: 64, height: 64 },
+              gravity: GravityType.center,
+            });
+          }
+        }
+        return {
+          ...relation,
+          organization: { ...relation.organization, logo },
+        };
+      });
+
+    const imageEnhancedEvent = {
+      ...enhancedEvent,
+      blurredBackground,
+    };
+
+    enhancedEvents.push(imageEnhancedEvent);
+  }
+
+  const enhancedEventsWithParticipationStatus =
+    await enhanceEventsWithParticipationStatus(sessionUser, enhancedEvents);
+
+  const filterVector = await getEventFilterVector({
+    filter: submission.value.filter,
+  });
+
+  const areas = await getAreasBySearchQuery(submission.value.search);
+  type EnhancedAreas = Array<
+    ArrayElement<Awaited<ReturnType<typeof getAreasBySearchQuery>>> & {
+      vectorCount: ReturnType<typeof getFilterCountForSlug>;
+      isChecked: boolean;
+    }
+  >;
+  const enhancedAreas = {
+    global: [] as EnhancedAreas,
+    country: [] as EnhancedAreas,
+    state: [] as EnhancedAreas,
+    district: [] as EnhancedAreas,
+  };
+  for (const area of areas) {
+    const vectorCount = getFilterCountForSlug(area.slug, filterVector, "area");
+    let isChecked;
+    // TODO: Remove '|| area.slug === null' when slug isn't optional anymore (after migration)
+    if (submission.value.filter === undefined || area.slug === null) {
+      isChecked = false;
+    } else {
+      isChecked = submission.value.filter.area.includes(area.slug);
+    }
+    const enhancedArea = {
+      ...area,
+      vectorCount,
+      isChecked,
+    };
+    enhancedAreas[area.type].push(enhancedArea);
+  }
+  let selectedAreas: Array<{
+    slug: string;
+    name: string | null;
+    vectorCount: number;
+    isInSearchResultsList: boolean;
+  }> = [];
+  if (submission.value.filter !== undefined) {
+    selectedAreas = await Promise.all(
+      submission.value.filter.area.map(async (slug) => {
+        const vectorCount = getFilterCountForSlug(slug, filterVector, "area");
+        const isInSearchResultsList = areas.some((area) => {
+          return area.slug === slug;
+        });
+        return {
+          slug,
+          name: (await getAreaNameBySlug(slug)) || null,
+          vectorCount,
+          isInSearchResultsList,
+        };
+      })
+    );
+  }
+
+  const types = await getAllEventTypes();
+  const enhancedTypes = types.map((type) => {
+    const vectorCount = getFilterCountForSlug(type.slug, filterVector, "type");
+    let isChecked;
+    // TODO: Remove '|| offer.slug === null' when slug isn't optional anymore (after migration)
+    if (submission.value.filter === undefined || type.slug === null) {
+      isChecked = false;
+    } else {
+      isChecked = submission.value.filter.type.includes(type.slug);
+    }
+    return { ...type, vectorCount, isChecked };
+  });
+  let selectedTypes: Array<{ slug: string; title: string | null }> = [];
+  if (submission.value.filter !== undefined) {
+    selectedTypes = submission.value.filter.type.map((slug) => {
+      const typeMatch = types.find((type) => {
+        return type.slug === slug;
+      });
+      return {
+        slug,
+        title: typeMatch?.title || null,
+      };
     });
   }
-  if (events.length < take && events.length > 0) {
-    const pastEvents = await prepareEvents(authClient, sessionUser, !inFuture, {
-      skip: 0,
-      take: take - events.length,
+
+  const focuses = await getAllFocuses();
+  const enhancedFocuses = focuses.map((focus) => {
+    const vectorCount = getFilterCountForSlug(
+      focus.slug,
+      filterVector,
+      "focus"
+    );
+    let isChecked;
+    // TODO: Remove '|| offer.slug === null' when slug isn't optional anymore (after migration)
+    if (submission.value.filter === undefined || focus.slug === null) {
+      isChecked = false;
+    } else {
+      isChecked = submission.value.filter.focus.includes(focus.slug);
+    }
+    return { ...focus, vectorCount, isChecked };
+  });
+  let selectedFocuses: Array<{ slug: string; title: string | null }> = [];
+  if (submission.value.filter !== undefined) {
+    selectedFocuses = submission.value.filter.focus.map((slug) => {
+      const focusMatch = focuses.find((focus) => {
+        return focus.slug === slug;
+      });
+      return {
+        slug,
+        title: focusMatch?.title || null,
+      };
     });
-    events = [...events, ...pastEvents];
   }
-  if (events.length === 0) {
-    events = await prepareEvents(authClient, sessionUser, !inFuture, {
-      skip: skip - futureEventsCount,
-      take,
-    });
+
+  const targetGroups = await getAllEventTargetGroups();
+  const enhancedTargetGroups = targetGroups.map((targetGroup) => {
+    const vectorCount = getFilterCountForSlug(
+      targetGroup.slug,
+      filterVector,
+      "eventTargetGroup"
+    );
+    let isChecked;
+    // TODO: Remove '|| offer.slug === null' when slug isn't optional anymore (after migration)
+    if (submission.value.filter === undefined || targetGroup.slug === null) {
+      isChecked = false;
+    } else {
+      isChecked = submission.value.filter.eventTargetGroup.includes(
+        targetGroup.slug
+      );
+    }
+    return { ...targetGroup, vectorCount, isChecked };
+  });
+  let selectedTargetGroups: Array<{ slug: string; title: string | null }> = [];
+  if (submission.value.filter !== undefined) {
+    selectedTargetGroups = submission.value.filter.eventTargetGroup.map(
+      (slug) => {
+        const targetGroupMatch = targetGroups.find((targetGroup) => {
+          return targetGroup.slug === slug;
+        });
+        return {
+          slug,
+          title: targetGroupMatch?.title || null,
+        };
+      }
+    );
+  }
+
+  let transformedSubmission;
+  if (submission.value.sortBy !== undefined) {
+    transformedSubmission = {
+      ...submission,
+      value: {
+        ...submission.value,
+        sortBy: `${submission.value.sortBy.value}-${submission.value.sortBy.direction}`,
+      },
+    };
+  } else {
+    transformedSubmission = {
+      ...submission,
+      value: {
+        ...submission.value,
+        sortBy: sortValues[0],
+      },
+    };
   }
 
   return json({
-    events,
-    pagination: {
-      page,
-      itemsPerPage,
-    },
-    userId: sessionUser?.id || undefined,
+    isLoggedIn,
+    events: enhancedEventsWithParticipationStatus,
+    areas: enhancedAreas,
+    selectedAreas,
+    focuses: enhancedFocuses,
+    selectedFocuses,
+    targetGroups: enhancedTargetGroups,
+    selectedTargetGroups,
+    types: enhancedTypes,
+    selectedTypes,
+    submission: transformedSubmission,
+    filteredByVisibilityCount,
+    eventsCount,
   });
 };
 
