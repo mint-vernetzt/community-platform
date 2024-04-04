2db7fe2fa78871a3bd76de8ac9d1c614206ebbd8
Peter Holló <hello@songsforthe.dev>
2024-04-04 11:27:28 +0200

    fix: Missing meta data causes error on registration via keycloak

--- app/routes/auth/keycloak.callback.tsx
+++ app/routes/auth/keycloak.callback.tsx
@@ -4,6 +4,7 @@ import { invariantResponse } from "~/lib/utils/response";
 import { prismaClient } from "~/prisma.server";
 import { createProfile, sendWelcomeMail } from "../register/utils.server";
 import * as Sentry from "@sentry/remix";
+import { generateValidSlug } from "~/utils.server";
 
 export const loader = async (args: LoaderFunctionArgs) => {
   const { request } = args;
@@ -31,6 +32,19 @@ export const loader = async (args: LoaderFunctionArgs) => {
   const firstLogin = profile === null;
   const loginRedirect = urlSearchParams.get("login_redirect");
   if (firstLogin) {
+    const username = generateValidSlug(user.user_metadata.full_name);
+    const fullNameParts = user.user_metadata.full_name.split(" ");
+    // combine all parts except the last one to get the first name
+    const firstName = fullNameParts.slice(0, -1).join(" ");
+    // use the last part as last name
+    const lastName = fullNameParts[fullNameParts.length - 1];
+
+    // enhance user metadata to pass to createProfile condition
+    user.user_metadata.username = username;
+    user.user_metadata.firstName = firstName;
+    user.user_metadata.lastName = lastName;
+    user.user_metadata.termsAccepted = false;
+
     const profile = await createProfile(user);
     invariantResponse(
       profile !== null,
