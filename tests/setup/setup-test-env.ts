c412b9425402c86f3a736136fbcc775f24bd0ef6
nac62116 <56918308+nac62116@users.noreply.github.com>
2024-04-29 16:18:05 +0200

    Remove msw

--- tests/setup/setup-test-env.ts
+++ tests/setup/setup-test-env.ts
@@ -1,11 +1,9 @@
 import { installGlobals } from "@remix-run/node";
 import { cleanup } from "@testing-library/react";
-import { server } from "./../mocks/index";
 import { afterEach, beforeEach, vi, type MockInstance } from "vitest";
 
 installGlobals();
 
-afterEach(() => server.resetHandlers());
 afterEach(() => cleanup());
 
 export let consoleError: MockInstance<Parameters<(typeof console)["error"]>>;
