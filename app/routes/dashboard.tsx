138fa19abb5eacbf281ae9d48e57c22fc8b65df8
nac62116 <56918308+nac62116@users.noreply.github.com>
2024-07-04 12:37:43 +0200

    feat: change link and wording for mesh mint card on dashboard

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -355,7 +355,7 @@ function getExternalLinks() {
       ),
     },
     meshMint: {
-      link: "https://www.meshmint.org/",
+      link: "https://www.mint-vernetzt.de/mesh-studie/?limit=6¤tPostType=mesh_study",
       icon: (
         <svg
           width="85"
