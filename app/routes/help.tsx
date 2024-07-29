fb4b213f59ffd0a005db97a67097ec4eefe2204a
nac62116 <56918308+nac62116@users.noreply.github.com>
2024-07-29 21:38:16 +0200

    Implement desktop version of help route

--- app/routes/help.tsx
+++ app/routes/help.tsx
@@ -12,8 +12,8 @@ export default function Help() {
 
   return (
     <>
-      <section className="mv-px-4 mv-pt-16 mv-pb-8">
-        <h1 className="mv-w-full mv-text-center mv-text-5xl mv-font-[900] mv-leading-9">
+      <section className="mv-w-full mv-mx-auto @md:mv-max-w-screen-container-md @lg:mv-max-w-screen-container-lg @xl:mv-max-w-screen-container-xl @2xl:mv-max-w-screen-container-2xl mv-px-4 @md:mv-px-6 @xl:mv-px-8 mv-pt-16 mv-pb-8 @md:mv-pb-12 @xl:mv-pb-16">
+        <h1 className="mv-w-full mv-text-center mv-text-5xl @sm:mv-text-6xl @md:mv-text-7xl @xl:mv-text-8xl mv-font-[900] mv-leading-9 @sm:mv-leading-10 @md:mv-leading-[64px] @xl:mv-leading-[80px]">
           {t("headline")}
         </h1>
         <p className="mv-w-full mv-text-center mv-text-neutral-700 mv-leading-5">
@@ -21,15 +21,15 @@ export default function Help() {
         </p>
       </section>
       {/* Margin bottom in this section is only applied because the next secion is not yet implemented (Support CTA) */}
-      <section className="mv-px-4 mv-py-6 mv-mb-[72px]">
-        <ul className="mv-flex mv-flex-col mv-gap-10">
+      <section className="mv-w-full mv-mx-auto @md:mv-max-w-screen-container-md @lg:mv-max-w-screen-container-lg @xl:mv-max-w-screen-container-xl @2xl:mv-max-w-screen-container-2xl mv-px-4 @md:mv-px-6 @xl:mv-px-8 mv-py-6 mv-mb-[72px]">
+        <ul className="mv-flex mv-flex-col mv-gap-10 @xl:mv-gap-[52px] mv-px-0 mv-pt-0 mv-pb-0 @md:mv-px-8 @md:mv-pt-8 @md:mv-pb-12 @md:mv-border @md:mv-border-neutral-200 @md:mv-rounded-2xl @md:mv-bg-white">
           {Object.entries(faq).map(([topicKey, topicValue]) => {
             return (
               <li key={topicKey}>
-                <h2 className="mv-mb-8 mv-text-secondary mv-text-3xl mv-font-semibold mv-leading-7">
+                <h2 className="mv-mb-2 @md:mv-mb-3 @xl:mv-mb-5 mv-text-secondary mv-text-3xl mv-font-semibold mv-leading-7">
                   {t(`faq.${topicKey}.headline`)}
                 </h2>
-                <ul className="mv-flex mv-flex-col mv-gap-6">
+                <ul>
                   {Object.entries(topicValue).map(([qAndAkey]) => {
                     if (qAndAkey === "headline") {
                       return null;
@@ -37,16 +37,16 @@ export default function Help() {
                     return (
                       <li
                         key={`${topicKey}-${qAndAkey}`}
-                        className="mv-pb-6 mv-group mv-border-neutral-200 mv-border-b"
+                        className="mv-group mv-border-neutral-200 mv-border-b"
                       >
                         <label
                           htmlFor={`expand-question-${topicKey}-${qAndAkey}`}
-                          className="mv-flex-grow mv-text-primary-600 mv-text-xl mv-font-bold mv-leading-6 mv-cursor-pointer mv-flex mv-gap-2 mv-items-center group-has-[:checked]:mv-mb-[10px] mv-mb-0"
+                          className="mv-flex-grow mv-pb-6 mv-pt-6 @xl:mv-pb-8 @xl:mv-pt-8 group-has-[:checked]:mv-pb-0 mv-text-primary-600 mv-text-xl mv-font-bold mv-leading-6 mv-cursor-pointer mv-flex mv-gap-2 mv-items-center mv-mb-0"
                         >
                           <p className="mv-flex-grow">
                             {t(`faq.${topicKey}.${qAndAkey}.question`)}
                           </p>
-                          <span className="mv-flex-shrink mv-w-fit mv-h-fit mv-rotate-90 group-has-[:checked]:-mv-rotate-90">
+                          <span className="mv-flex-shrink mv-w-fit mv-h-fit mv-rotate-90 group-has-[:checked]:-mv-rotate-90 mv-mr-0 @md:mv-mr-3 @lg:mv-mr-6">
                             <Icon type="chevron-right" />
                           </span>
                         </label>
@@ -55,7 +55,7 @@ export default function Help() {
                           type="checkbox"
                           className="mv-absolute mv-opacity-0 mv-w-0 mv-h-0 mv-overflow-hidden"
                         />
-                        <div className="mv-text-primary-600 mv-leading-[20.8px] mv-font-normal mv-pr-5 mv-hidden group-has-[:checked]:mv-block mv-hyphens-auto">
+                        <div className="mv-text-primary-600 mv-leading-[20.8px] mv-font-normal mv-pr-5 mv-pt-[10px] mv-pb-6 @xl:mv-pt-4 @xl:mv-pb-8 @sm:mv-pr-8 @md:mv-pr-20 @xl:mv-pr-[166px] mv-hidden group-has-[:checked]:mv-block mv-hyphens-auto">
                           {qAndAkey === "whatIsStem" ? (
                             <Trans
                               i18nKey={`faq.${topicKey}.${qAndAkey}.answer`}
