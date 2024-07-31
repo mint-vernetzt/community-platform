c7275f437b281e1d02c2ae971aca5afcbdaa75e0
nac62116 <56918308+nac62116@users.noreply.github.com>
2024-07-31 11:25:30 +0200

    Add missing wobbel on mobile

--- app/routes/help.tsx
+++ app/routes/help.tsx
@@ -50,7 +50,21 @@ export default function Help() {
           })}
         </Accordion>
       </section>
-      <section className="mv-flex mv-flex-col mv-items-center mv-w-full mv-py-16 mv-px-4 @md:mv-px-6 @xl:mv-px-8 mv-bg-accent-200 @xl:mv-bg-primary-50">
+      <section className="mv-relative mv-flex mv-flex-col mv-items-center mv-w-full mv-py-16 mv-px-4 @md:mv-px-6 @xl:mv-px-8 mv-bg-accent-200 @md:mv-bg-primary-50">
+        <div className="mv-absolute mv-top-0 mv-right-0 @md:mv-hidden">
+          <svg
+            width="160"
+            height="95"
+            viewBox="0 0 160 95"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M6.09958 -64.2173C7.54864 -102.081 17.6128 -113.709 30.5663 -131.074C43.5198 -148.438 67.766 -154.578 80.5481 -156.374C98.0195 -158.828 140.796 -152.403 156.899 -150.791C185.751 -147.902 258.393 -127.105 264.522 -50.2964C273.557 62.9148 173.385 95.6622 144.191 90.0838C109.053 83.3698 111.189 65.6072 90.3268 51.8716C68.2058 37.3072 42.247 39.1163 27.195 24.9196C6.98256 5.85556 4.65052 -26.3535 6.09958 -64.2173Z"
+              stroke="#FCC433"
+            />
+          </svg>
+        </div>
         <div className="mv-w-full mv-text-center mv-mb-8 @md:mv-mb-10 @xl:mv-mb-12 mv-text-primary-600 @xl:mv-text-primary-500 mv-font-semibold">
           <h2 className="mv-mb-6 @xl:mv-mb-8 mv-text-4xl @xl:mv-text-5xl mv-leading-7 @md:mv-leading-8 @xl:mv-leading-9">
             {t("support.headline")}
