af671b2d43954945b764c48e3f1104f1b5cba816
Peter Holló <hello@songsforthe.dev>
2024-08-05 14:07:22 +0200

    fix: TLD .education not valid (#1486)

--- app/lib/utils/schemas.ts
+++ app/lib/utils/schemas.ts
@@ -30,7 +30,7 @@ export const createWebsiteSchema = (t: TFunction) =>
   z
     .string()
     .regex(
-      /^(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/gi,
+      /^(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,9}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/gi,
       {
         message: t("validation.website.regex", { ns: i18nNS }),
       }
