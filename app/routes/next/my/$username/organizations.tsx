f21071f7f82c4c25e20358dd8905c0ebc4fefc56
nac62116 <56918308+nac62116@users.noreply.github.com>
2024-08-22 14:03:00 +0200

    Add syling to my organizations page

--- app/routes/next/my/$username/organizations.tsx
+++ app/routes/next/my/$username/organizations.tsx
@@ -61,26 +61,32 @@ export default function MyOrganizations() {
   // TODO: Styling
   // TODO: i18n
   return (
-    <>
-      <h1>Meine Organisationen</h1>
-      <Button as="a" href={"/organization/create"}>
-        <svg
-          width="20"
-          height="20"
-          viewBox="0 0 20 20"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M10 5C10.1658 5 10.3247 5.06585 10.4419 5.18306C10.5592 5.30027 10.625 5.45924 10.625 5.625V9.375H14.375C14.5408 9.375 14.6997 9.44085 14.8169 9.55806C14.9342 9.67527 15 9.83424 15 10C15 10.1658 14.9342 10.3247 14.8169 10.4419C14.6997 10.5592 14.5408 10.625 14.375 10.625H10.625V14.375C10.625 14.5408 10.5592 14.6997 10.4419 14.8169C10.3247 14.9342 10.1658 15 10 15C9.83424 15 9.67527 14.9342 9.55806 14.8169C9.44085 14.6997 9.375 14.5408 9.375 14.375V10.625H5.625C5.45924 10.625 5.30027 10.5592 5.18306 10.4419C5.06585 10.3247 5 10.1658 5 10C5 9.83424 5.06585 9.67527 5.18306 9.55806C5.30027 9.44085 5.45924 9.375 5.625 9.375H9.375V5.625C9.375 5.45924 9.44085 5.30027 9.55806 5.18306C9.67527 5.06585 9.83424 5 10 5Z"
-            fill="currentColor"
-          />
-        </svg>
-        Organisation anlegen
-      </Button>
+    <div className="mv-py-6 mv-px-4 @lg:mv-py-8 @md:mv-px-6 @lg:mv-px-8 mv-flex mv-flex-col mv-gap-6 mv-mb-10 @sm:mv-mb-[72px] @lg:mv-mb-16">
+      <div className="mv-flex mv-flex-col @sm:mv-flex-row mv-gap-4 @md:mv-gap-6 @lg:mv-gap-8 mv-items-center mv-justify-between">
+        <h1 className="mv-mb-0 mv-text-5xl mv-text-primary mv-font-bold mv-leading-9">
+          Meine Organisationen
+        </h1>
+        <Button as="a" href={"/organization/create"}>
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M10 5C10.1658 5 10.3247 5.06585 10.4419 5.18306C10.5592 5.30027 10.625 5.45924 10.625 5.625V9.375H14.375C14.5408 9.375 14.6997 9.44085 14.8169 9.55806C14.9342 9.67527 15 9.83424 15 10C15 10.1658 14.9342 10.3247 14.8169 10.4419C14.6997 10.5592 14.5408 10.625 14.375 10.625H10.625V14.375C10.625 14.5408 10.5592 14.6997 10.4419 14.8169C10.3247 14.9342 10.1658 15 10 15C9.83424 15 9.67527 14.9342 9.55806 14.8169C9.44085 14.6997 9.375 14.5408 9.375 14.375V10.625H5.625C5.45924 10.625 5.30027 10.5592 5.18306 10.4419C5.06585 10.3247 5 10.1658 5 10C5 9.83424 5.06585 9.67527 5.18306 9.55806C5.30027 9.44085 5.45924 9.375 5.625 9.375H9.375V5.625C9.375 5.45924 9.44085 5.30027 9.55806 5.18306C9.67527 5.06585 9.83424 5 10 5Z"
+              fill="currentColor"
+            />
+          </svg>
+          Organisation anlegen
+        </Button>
+      </div>
       {loaderData.teamMemberOrganizations.length > 0 ||
       loaderData.adminOrganizations.length > 0 ? (
-        <section>
+        // <section className="mv-w-full mv-mx-auto @md:mv-max-w-screen-container-md @lg:mv-max-w-screen-container-lg @xl:mv-max-w-screen-container-xl @2xl:mv-max-w-screen-container-2xl">
+        // <div className="mv-w-full mv-flex mv-flex-col mv-gap-8 @sm:mv-px-2 @md:mv-px-4 @lg:mv-px-8 @sm:mv-py-2 @md:mv-py-4 @lg:mv-py-6 @sm:mv-gap-4 @sm:mv-bg-white @sm:mv-rounded-2xl @sm:mv-border @sm:mv-border-neutral-200">
+        <section className="mv-w-full mv-flex mv-flex-col mv-gap-8 @sm:mv-px-2 @md:mv-px-4 @lg:mv-px-8 @sm:mv-py-2 @md:mv-py-4 @lg:mv-py-6 @sm:mv-gap-4 @sm:mv-bg-white @sm:mv-rounded-2xl @sm:mv-border @sm:mv-border-neutral-200">
           <TabBar>
             <TabBar.Item
               active={activeItem === "teamMember"}
@@ -94,7 +100,7 @@ export default function MyOrganizations() {
                   return;
                 }}
               >
-                <div className="mv-flex mv-gap-1.5">
+                <div className="mv-flex mv-gap-1.5 mv-items-center">
                   <span>Teammitglied</span>
                   <TabBar.Counter active={activeItem === "teamMember"}>
                     {loaderData.teamMemberOrganizations.length}
@@ -114,7 +120,7 @@ export default function MyOrganizations() {
                   return;
                 }}
               >
-                <div className="mv-flex mv-gap-1.5">
+                <div className="mv-flex mv-gap-1.5 mv-items-center">
                   <span>Admin</span>
                   <TabBar.Counter active={activeItem === "admin"}>
                     {loaderData.adminOrganizations.length}
@@ -123,39 +129,42 @@ export default function MyOrganizations() {
               </Link>
             </TabBar.Item>
           </TabBar>
-          {activeItem === "teamMember" ? (
-            <p>Diesen Organisationen bist du als Teammitglied zugeordnet.</p>
-          ) : (
-            <p>Diesen Organisationen bist du als Admin zugeordnet.</p>
-          )}
-          {activeItem === "teamMember" &&
-          loaderData.teamMemberOrganizations.length > 0 ? (
-            <CardContainer type="multi row">
-              {loaderData.teamMemberOrganizations.map((organization) => {
-                return (
-                  <OrganizationCard
-                    key={`team-member-organization-${organization.id}`}
-                    organization={organization}
-                  />
-                );
-              })}
-            </CardContainer>
-          ) : null}
-          {activeItem === "admin" &&
-          loaderData.adminOrganizations.length > 0 ? (
-            <CardContainer type="multi row">
-              {loaderData.adminOrganizations.map((organization) => {
-                return (
-                  <OrganizationCard
-                    key={`admin-organization-${organization.id}`}
-                    organization={organization}
-                  />
-                );
-              })}
-            </CardContainer>
-          ) : null}
+          <p className="mv-hidden @sm:mv-block">
+            {activeItem === "teamMember"
+              ? "Diesen Organisationen bist du als Teammitglied zugeordnet."
+              : "Diesen Organisationen bist du als Admin zugeordnet."}
+          </p>
+          <div className="-mv-mx-4">
+            {activeItem === "teamMember" &&
+            loaderData.teamMemberOrganizations.length > 0 ? (
+              <CardContainer type="multi row">
+                {loaderData.teamMemberOrganizations.map((organization) => {
+                  return (
+                    <OrganizationCard
+                      key={`team-member-organization-${organization.id}`}
+                      organization={organization}
+                    />
+                  );
+                })}
+              </CardContainer>
+            ) : null}
+            {activeItem === "admin" &&
+            loaderData.adminOrganizations.length > 0 ? (
+              <CardContainer type="multi row">
+                {loaderData.adminOrganizations.map((organization) => {
+                  return (
+                    <OrganizationCard
+                      key={`admin-organization-${organization.id}`}
+                      organization={organization}
+                    />
+                  );
+                })}
+              </CardContainer>
+            ) : null}
+          </div>
+          {/* </div> */}
         </section>
       ) : null}
-    </>
+    </div>
   );
 }
