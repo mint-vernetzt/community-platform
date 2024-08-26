11e5b013278ec0d821d4cc3103c354a31f61c5f9
nac62116 <56918308+nac62116@users.noreply.github.com>
2024-08-26 11:05:38 +0200

    fix: my organizations section should not shrink to min content

--- app/routes/my/organizations.tsx
+++ app/routes/my/organizations.tsx
@@ -61,7 +61,7 @@ export default function MyOrganizations() {
 
   return (
     <div className="mv-w-full mv-flex mv-justify-center">
-      <div className="mv-py-6 mv-px-4 @lg:mv-py-8 @md:mv-px-6 @lg:mv-px-8 mv-flex mv-flex-col mv-gap-6 mv-mb-10 @sm:mv-mb-[72px] @lg:mv-mb-16 mv-max-w-screen-2xl">
+      <div className="mv-w-full mv-py-6 mv-px-4 @lg:mv-py-8 @md:mv-px-6 @lg:mv-px-8 mv-flex mv-flex-col mv-gap-6 mv-mb-10 @sm:mv-mb-[72px] @lg:mv-mb-16 mv-max-w-screen-2xl">
         <div className="mv-flex mv-flex-col @sm:mv-flex-row mv-gap-4 @md:mv-gap-6 @lg:mv-gap-8 mv-items-center mv-justify-between">
           <h1 className="mv-mb-0 mv-text-5xl mv-text-primary mv-font-bold mv-leading-9">
             {t("headline")}
