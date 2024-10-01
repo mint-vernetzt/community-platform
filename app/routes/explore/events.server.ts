2fbceb21197cba26265e85f7d43dd5ffd82e8840
nac62116 <56918308+nac62116@users.noreply.github.com>
2024-10-01 20:29:12 +0200

    Implement translated datasets on explore events filter

--- app/routes/explore/events.server.ts
+++ app/routes/explore/events.server.ts
@@ -629,20 +629,6 @@ export function getFilterCountForSlug(
   return filterCount;
 }
 
-export async function getAllEventTypes() {
-  return await prismaClient.eventType.findMany({
-    orderBy: {
-      title: "asc",
-    },
-    select: {
-      id: true,
-      title: true,
-      description: true,
-      slug: true,
-    },
-  });
-}
-
 export async function getAllStages() {
   return await prismaClient.stage.findMany({
     orderBy: {
@@ -650,8 +636,6 @@ export async function getAllStages() {
     },
     select: {
       id: true,
-      title: true,
-      description: true,
       slug: true,
     },
   });
@@ -664,8 +648,6 @@ export async function getAllFocuses() {
     },
     select: {
       id: true,
-      title: true,
-      description: true,
       slug: true,
     },
   });
@@ -678,8 +660,6 @@ export async function getAllEventTargetGroups() {
     },
     select: {
       id: true,
-      title: true,
-      description: true,
       slug: true,
     },
   });
