152d3413b214ab2d7afc6a7af99517b9fc146718
nac62116 <56918308+nac62116@users.noreply.github.com>
2024-10-24 09:44:56 +0200

    Use process.env instead of ENV on server

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -25,7 +25,7 @@ global.ENV = getEnv();
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
   tracesSampleRate: 1,
-  environment: ENV.COMMUNITY_BASE_URL.replace(/https?:\/\//, ""),
+  environment: process.env.COMMUNITY_BASE_URL.replace(/https?:\/\//, ""),
 });
 
 export function handleError(
