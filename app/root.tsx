90958f13dcc2675f9e815bf035e854e577a3c8ff
nac62116 <56918308+nac62116@users.noreply.github.com>
2024-11-05 15:28:50 +0100

    fix: event detail layout breaking when description contains extra large word (#1711)

--- app/root.tsx
+++ app/root.tsx
@@ -212,7 +212,7 @@ export const ErrorBoundary = () => {
   const navBarMenuIsOpen = searchParams.get(openNavBarMenuKey);
 
   const bodyClasses = classNames(
-    "mv-min-h-screen",
+    "mv-min-h-screen mv-break-words",
     navBarMenuIsOpen !== null &&
       navBarMenuIsOpen !== "false" &&
       "mv-overflow-hidden xl:mv-overflow-auto"
@@ -366,7 +366,7 @@ export default function App() {
   const navBarMenuIsOpen = searchParams.get(openNavBarMenuKey);
 
   const bodyClasses = classNames(
-    "mv-min-h-screen",
+    "mv-min-h-screen mv-break-words",
     modal && "mv-overflow-hidden",
     showFilters !== null &&
       showFilters !== "false" &&
