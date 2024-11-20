09a37291221b3ab0aa2b4e030c0a72ed6d66c6f6
nac62116 <56918308+nac62116@users.noreply.github.com>
2024-11-20 17:31:12 +0100

    wip: clean loader, clean action and clean component with only conform forms

--- app/routes/next/organization/$slug/settings/admins.server.ts
+++ app/routes/next/organization/$slug/settings/admins.server.ts
@@ -3,9 +3,69 @@ import { BlurFactor, getImageURL, ImageSizes } from "~/images.server";
 import { prismaClient } from "~/prisma.server";
 import { getPublicURL } from "~/storage.server";
 
-export async function getInvitedProfilesOfOrganization(
-  authClient: SupabaseClient,
-  organizationId: string
+export async function getOrganizationWithAdmins(
+  slug: string,
+  authClient: SupabaseClient
+) {
+  const organization = await prismaClient.organization.findFirst({
+    where: {
+      slug,
+    },
+    select: {
+      id: true,
+      admins: {
+        select: {
+          profile: {
+            select: {
+              username: true,
+              firstName: true,
+              lastName: true,
+              avatar: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  if (organization === null) {
+    return null;
+  }
+
+  // enhance admins with avatar
+  const admins = organization.admins.map((relation) => {
+    let avatar = relation.profile.avatar;
+    let blurredAvatar;
+    if (avatar !== null) {
+      const publicURL = getPublicURL(authClient, avatar);
+      if (publicURL !== null) {
+        avatar = getImageURL(publicURL, {
+          resize: {
+            type: "fill",
+            ...ImageSizes.Profile.ListItemProjectDetailAndSettings.Avatar,
+          },
+        });
+        blurredAvatar = getImageURL(publicURL, {
+          resize: {
+            type: "fill",
+            ...ImageSizes.Profile.ListItemProjectDetailAndSettings
+              .BlurredAvatar,
+          },
+          blur: BlurFactor,
+        });
+      }
+    }
+    return { profile: { ...relation.profile, avatar, blurredAvatar } };
+  });
+
+  const enhancedOrganization = { ...organization, admins };
+
+  return enhancedOrganization;
+}
+
+export async function getPendingAdminInvitesOfOrganization(
+  organizationId: string,
+  authClient: SupabaseClient
 ) {
   const profiles =
     await prismaClient.inviteForProfileToJoinOrganization.findMany({
--- app/routes/next/organization/$slug/settings/admins.tsx
+++ app/routes/next/organization/$slug/settings/admins.tsx
@@ -1,15 +1,13 @@
 import { getFormProps, getInputProps, useForm } from "@conform-to/react-v1";
-import { getZodConstraint } from "@conform-to/zod-v1";
-import { Avatar, Input, List, Section } from "@mint-vernetzt/components";
+import { Input, Section } from "@mint-vernetzt/components";
 import {
-  type ActionFunctionArgs,
   json,
   redirect,
+  type ActionFunctionArgs,
   type LoaderFunctionArgs,
 } from "@remix-run/node";
 import {
   Form,
-  useLoaderData,
   useLocation,
   useSearchParams,
   useSubmit,
@@ -17,16 +15,16 @@ import {
 import { useTranslation } from "react-i18next";
 import { createAuthClient, getSessionUser } from "~/auth.server";
 import i18next from "~/i18next.server";
-import { BlurFactor, ImageSizes, getImageURL } from "~/images.server";
 import { invariantResponse } from "~/lib/utils/response";
 import { getParamValueOrThrow } from "~/lib/utils/routes";
-import { prismaClient } from "~/prisma.server";
 import { detectLanguage } from "~/root.server";
 import { getRedirectPathOnProtectedOrganizationRoute } from "~/routes/organization/$slug/utils.server";
 import { BackButton } from "~/routes/project/$slug/settings/__components";
-import { getPublicURL } from "~/storage.server";
 import { getToast } from "~/toast.server";
-import { getInvitedProfilesOfOrganization } from "./admins.server";
+import {
+  getOrganizationWithAdmins,
+  getPendingAdminInvitesOfOrganization,
+} from "./admins.server";
 
 // TODO: Import namespaces from validation functions if neccessary (searchProfiles, inviteProfileToBeOrganizationAdmin, cancelAdminInvitation, removeAdminFromOrganization)
 const i18nNS = ["routes/next/organization/settings/admins"];
@@ -36,117 +34,40 @@ export const handle = {
 
 export const loader = async (args: LoaderFunctionArgs) => {
   const { request, params } = args;
+  const slug = getParamValueOrThrow(params, "slug");
 
   const locale = detectLanguage(request);
   const t = await i18next.getFixedT(locale, i18nNS);
 
   const { authClient } = createAuthClient(request);
-
   const sessionUser = await getSessionUser(authClient);
-
-  // check slug exists (throw bad request if not)
-  invariantResponse(
-    params.slug !== undefined,
-    t("error.invariant.invalidRoute"),
-    { status: 400 }
-  );
-
   const redirectPath = await getRedirectPathOnProtectedOrganizationRoute({
     request,
-    slug: params.slug,
+    slug,
     sessionUser,
     authClient,
   });
-
   if (redirectPath !== null) {
     return redirect(redirectPath);
   }
-
-  // get organization admins
-  const organization = await prismaClient.organization.findFirst({
-    where: {
-      slug: params.slug,
-    },
-    select: {
-      id: true,
-      admins: {
-        select: {
-          profile: {
-            select: {
-              username: true,
-              firstName: true,
-              lastName: true,
-              avatar: true,
-            },
-          },
-        },
-      },
-    },
-  });
-
+  // TODO: Clean loader
+  const organization = await getOrganizationWithAdmins(slug, authClient);
   invariantResponse(organization !== null, t("error.invariant.notFound"), {
     status: 404,
   });
-
-  // enhance admins with avatar
-  const admins = organization.admins.map((relation) => {
-    let avatar = relation.profile.avatar;
-    let blurredAvatar;
-    if (avatar !== null) {
-      const publicURL = getPublicURL(authClient, avatar);
-      if (publicURL !== null) {
-        avatar = getImageURL(publicURL, {
-          resize: {
-            type: "fill",
-            ...ImageSizes.Profile.ListItemProjectDetailAndSettings.Avatar,
-          },
-        });
-        blurredAvatar = getImageURL(publicURL, {
-          resize: {
-            type: "fill",
-            ...ImageSizes.Profile.ListItemProjectDetailAndSettings
-              .BlurredAvatar,
-          },
-          blur: BlurFactor,
-        });
-      }
-    }
-    return { profile: { ...relation.profile, avatar, blurredAvatar } };
-  });
-
-  const enhancedOrganization = { ...organization, admins };
-
-  const invitedProfiles = await getInvitedProfilesOfOrganization(
-    authClient,
-    organization.id
+  const pendingAdminInvites = await getPendingAdminInvitesOfOrganization(
+    organization.id,
+    authClient
   );
-
-  const { toast, headers: toastHeaders } = await getToast(request);
-
-  // TODO: Clean loader
-
-  // const admins = await getAdminsOfOrganization(slug);
-  // const pendingAdminInvites = await getPendingAdminInvitesOfOrganization(slug);
   // const {searchedProfiles, submission} = await searchProfiles(searchParams); -> How to implement which profiles are excluded (f.e. already admins or invited)?
-  // const toast = await getToast(request);
-
-  // return json(
-  //   {
-  //     admins
-  //     pendingAdminInvites,
-  //     searchedProfiles,
-  //     submission,
-  //     toast,
-  //   },
-  //   {
-  //     headers: toastHeaders || undefined,
-  //   }
-  // );
+  const { toast, headers: toastHeaders } = await getToast(request);
 
   return json(
     {
-      organization: enhancedOrganization,
-      invitedProfiles,
+      organization,
+      pendingAdminInvites,
+      // searchedProfiles,
+      // submission,
       toast,
     },
     {
@@ -173,6 +94,7 @@ export const action = async (args: ActionFunctionArgs) => {
   if (redirectPath !== null) {
     return redirect(redirectPath);
   }
+
   let response;
   const formData = await request.formData();
   const intent = formData.get("intent");
@@ -193,8 +115,13 @@ export const action = async (args: ActionFunctionArgs) => {
 };
 
 function Admins() {
-  const { admins, pendingAdminInvites, searchedProfiles, submission, toast } =
-    useLoaderData<typeof loader>();
+  // const {
+  //   organization,
+  //   pendingAdminInvites,
+  //   searchedProfiles,
+  //   submission,
+  //   toast,
+  // } = useLoaderData<typeof loader>();
 
   const location = useLocation();
   const { t } = useTranslation(i18nNS);
@@ -203,7 +130,7 @@ function Admins() {
 
   const [searchForm, searchFields] = useForm({
     id: "search-profiles",
-    lastResult: submission,
+    // lastResult: submission,
     shouldValidate: "onSubmit",
     shouldRevalidate: "onBlur",
     // TODO: Import search schema from module where searchProfiles is implemented
@@ -223,7 +150,7 @@ function Admins() {
       <BackButton to={location.pathname}>{t("content.headline")}</BackButton>
       <p className="mv-my-6 @md:mv-mt-0">{t("content.intro")}</p>
 
-      {/* TODO: Remove Admin Form */}
+      {/* TODO: Remove Admin Form with conform */}
       <div className="mv-flex mv-flex-col mv-gap-6 @md:mv-gap-4">
         {/* {toast !== null && toast.id === "remove-admin-toast" && (
           <div id={toast.id}>
@@ -306,7 +233,8 @@ function Admins() {
             {/* TODO: no script submit button */}
           </Form>
 
-          {/* <Form method="post" preventScrollReset> */}
+          {/* TODO: Add Admin Form with conform */}
+          {/* <Form method="post" preventScrollReset>
           {searchedProfiles.length > 0 ? (
             <List>
               {searchedProfiles.map((profile) => {
@@ -316,7 +244,7 @@ function Admins() {
                     <List.Item.Title>
                       {profile.firstName} {profile.lastName}
                     </List.Item.Title>
-                    {/* <List.Item.Controls>
+                    <List.Item.Controls>
                       <Button
                         name={conform.INTENT}
                         variant="outline"
@@ -325,13 +253,14 @@ function Admins() {
                       >
                         {t("content.add.submit")}
                       </Button>
-                    </List.Item.Controls> */}
+                    </List.Item.Controls>
                   </List.Item>
                 );
               })}
             </List>
           ) : null}
-          {/* </Form> */}
+          </Form> */}
+          {/* TODO: pending invites section */}
         </div>
       </div>
     </Section>
