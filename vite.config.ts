3bae6b4f896e46dd560d7ff3cc2bef9c27623883
nac62116 <56918308+nac62116@users.noreply.github.com>
2024-12-11 18:48:38 +0100

    fix: missing ignored routes pattern in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,14 @@ export default defineConfig({
       routes(defineRoutes) {
         // uses the v1 convention, works in v1.15+ and v2
         return createRoutesFromFolders(defineRoutes, {
-          ignoredFilePatterns: ["**/*.func.*", "**/*.spec.*"],
+          ignoredFilePatterns: [
+            "**/*.css",
+            "**/*.func.*",
+            "**/*.spec.*",
+            "**/*.shared.*",
+            "**/*.components.*",
+            "**/*.server.*",
+          ],
         });
       },
     }),
