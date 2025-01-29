5aa33f5963b95c7b0d3981dfa715df676c8ed0fc
Peter Holló <hello@songsforthe.dev>
2025-01-29 15:00:45 +0100

    feat: update deadline to confirm registration (#1825)

---
+++ app/locales/.server/en/routes/auth/confirm.ts
@@ -0,0 +1,20 @@
+export const locale = {
+  signup: {
+    title: "Registration confirmation",
+    description:
+      "Welcome to the MINTcommunity! Please confirm your email address within one hour to activate your profile on the MINTvernetzt platform via the following link:",
+    action: "Confirm registration",
+  },
+  changeEmail: {
+    title: "Change email address",
+    description:
+      "To change your email address on the MINTvernetzt platform, please follow this link:",
+    action: "Confirm new email address",
+  },
+  recovery: {
+    title: "Reset password",
+    description:
+      "Forgot your password? Click on the link below to reset your password:",
+    action: "Reset password",
+  },
+} as const;
