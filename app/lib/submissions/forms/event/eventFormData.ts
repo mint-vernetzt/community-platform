10768f0332b3bdba0f780e65ad69cc4852635085
nac62116 <56918308+nac62116@users.noreply.github.com>
2025-03-11 16:10:35 +0100

    Remove obsolete submission routes and utilities that were only used by them

--- app/lib/submissions/forms/event/eventFormData.ts
+++
@@ -1,9 +0,0 @@
-export interface EventFormData {
-  name: string;
-  place: string;
-  date: string;
-  description: string;
-  contact_name: string;
-  contact_email: string;
-  terms_accepted: string;
-}
