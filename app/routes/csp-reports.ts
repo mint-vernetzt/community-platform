38a61f62e8eaf7b7c0524346cf8576b8e68fe410
nac62116 <56918308+nac62116@users.noreply.github.com>
2025-04-16 10:17:14 +0200

    Add console log to test csp report sentry capture

--- app/routes/csp-reports.ts
+++ app/routes/csp-reports.ts
@@ -4,6 +4,7 @@ import * as Sentry from "@sentry/node";
 export const action = async (args: ActionFunctionArgs) => {
   const { request } = args;
   const body = await request.json();
+  console.log(body);
   Sentry.captureException(body);
   return null;
 };
