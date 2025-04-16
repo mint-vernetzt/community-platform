13669a0cc37a05db0ef37a8fea39139b05133ba9
nac62116 <56918308+nac62116@users.noreply.github.com>
2025-04-16 10:08:35 +0200

    fix: csp reports do have diffent content types depending on browser environment

--- app/routes/csp-reports.ts
+++ app/routes/csp-reports.ts
@@ -1,15 +1,8 @@
 import { type ActionFunctionArgs } from "react-router";
-import { invariantResponse } from "~/lib/utils/response";
 import * as Sentry from "@sentry/node";
 
 export const action = async (args: ActionFunctionArgs) => {
   const { request } = args;
-  const contentType = request.headers.get("content-type");
-  invariantResponse(
-    contentType === "application/csp-report",
-    "Invalid content type",
-    { status: 400 }
-  );
   const body = await request.json();
   Sentry.captureException(body);
   return null;
