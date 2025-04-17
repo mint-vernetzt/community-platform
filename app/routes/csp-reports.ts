cc2b781be059759bd8e76a0cd94da6cb644bda2e
nac62116 <56918308+nac62116@users.noreply.github.com>
2025-04-17 12:03:25 +0200

    feat: add console error to csp endpoint additionally to sentry captureException (#1973)

--- app/routes/csp-reports.ts
+++ app/routes/csp-reports.ts
@@ -4,6 +4,7 @@ import * as Sentry from "@sentry/node";
 export const action = async (args: ActionFunctionArgs) => {
   const { request } = args;
   const body = await request.json();
+  console.error("CSP Report: ", body);
   Sentry.captureException(String(body));
   return null;
 };
