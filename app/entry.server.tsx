1efce50f62c5ee76851105f371855e349c6f53e6
nac62116 <56918308+nac62116@users.noreply.github.com>
2025-04-24 13:58:11 +0200

    feat: add X-Frame_Options heater to entry.server

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -56,6 +56,7 @@ export default async function handleRequest(
         : " connect-src 'self'"
     }`
   );
+  responseHeaders.set("X-Frame-Options", "SAMEORIGIN");
 
   // Appending profiling policy header to the response when sentry is enabled
   if (
