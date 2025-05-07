41a4bf430672e5630487dc89211ff82f502cbf11
Peter Holló <hello@songsforthe.dev>
2025-05-07 14:51:13 +0200

    fix: more than one period at sentence end

--- app/locales/.server/en/routes/login/index.ts
+++ app/locales/.server/en/routes/login/index.ts
@@ -12,7 +12,7 @@ export const locale = {
     invalidCredentials:
       "Your login credentials (email or password) are incorrect. Please check your entries.",
     notConfirmed:
-      'Your email address has not yet been confirmed. That\'s why we have sent you a new confirmation link.. Please check your inbox and click on the confirmation link. If you haven\'t received an email, please check your spam folder or contact <a href="mailto:{{supportMail}}" className="mv-text-primary mv-font-bold hover:mv-underline">Support</a>.',
+      'Your email address has not yet been confirmed. That\'s why we have sent you a new confirmation link. Please check your inbox and click on the confirmation link. If you haven\'t received an email, please check your spam folder or contact <a href="mailto:{{supportMail}}" className="mv-text-primary mv-font-bold hover:mv-underline">Support</a>.',
     confirmationLinkExpired:
       "Your confirmation link has expired. Please login with your credentials to request a new one.",
   },
