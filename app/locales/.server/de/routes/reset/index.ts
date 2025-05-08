b45cd27b381c8dc9341c58d5cbaa320e3abbead1
nac62116 <56918308+nac62116@users.noreply.github.com>
2025-05-08 14:19:38 +0200

    feat: refactor reset password route with conform

--- app/locales/.server/de/routes/reset/index.ts
+++ app/locales/.server/de/routes/reset/index.ts
@@ -1,19 +1,14 @@
 export const locale = {
   login: "Anmelden",
   validation: {
-    email: {
-      email: "Bitte gib eine gültige E-Mail-Adresse ein.",
-      min: "Bitte gib eine gültige E-Mail-Adresse ein.",
-    },
+    email: "Bitte gib eine gültige E-Mail-Adresse ein.",
   },
   response: {
     headline: "Passwort zurücksetzen",
-    done: {
-      prefix: "Eine E-Mail zum Zurücksetzen des Passworts wurde an",
-      suffix: "geschickt.",
-    },
+    success:
+      'Eine E-Mail zum Zurücksetzen des Passworts wurde an <0>{{email}}</0> gesendet. Um das Passwort zurückzusetzen, klicke bitte innerhalb von einer Stunde den "Passwort zuzücksetzen"-Link, den wir Dir über <0>{{systemMail}}</0> zusenden. Bitte sieh auch in Deinem Spam-Ordner nach. Falls Du die E-Mail nicht erhalten hast, kannst Du dich gerne an unseren <1>Support</1> wenden.',
     notice:
-      "Solltest Du Dich noch nicht unter dieser E-Mail-Adresse registriert haben, erhältst Du keine E-Mail zum Zurücksetzen des Passworts.",
+      "Solltest Du Dich noch nicht unter dieser E-Mail-Adresse registriert haben, erhältst Du keine E-Mail zum Zurücksetzen des Passworts. Das gleiche gilt, wenn Du über die <0>MINT-ID</0> registriert bist. In diesem Fall kannst Du das Passwort auf <1>mint-id.org</1> zurücksetzen.",
   },
   form: {
     intro:
--- app/locales/.server/en/routes/reset/index.ts
+++ app/locales/.server/en/routes/reset/index.ts
@@ -1,19 +1,14 @@
 export const locale = {
   login: "Login",
   validation: {
-    email: {
-      email: "Please enter a valid email address.",
-      min: "Please enter a valid email address.",
-    },
+    email: "Please enter a valid email address.",
   },
   response: {
     headline: "Reset password",
-    done: {
-      prefix: "An email to reset your password has been sent to",
-      suffix: ".",
-    },
+    success:
+      'An email to reset the password has been sent to <0>{{email}}</0>. To reset the password, please click within one hour on the "Reset password" link that we send you via <0>{{systemMail}}</0>. Please also check your spam folder. If you do not receive the email, feel free to contact our <1>support</1>.',
     notice:
-      "If you have not yet registered under this email address, you will not receive an email to reset your password.",
+      "If you have not registered with this email address, you will not receive an email to reset the password. The same applies if you are registered via <0>MINT-ID</0>. In this case, you can reset the password at <1>mint-id.org</1>.",
   },
   form: {
     intro:
