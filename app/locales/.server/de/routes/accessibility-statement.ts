ddcbd1162fb348a50d883fd5abf81be4b066f5dc
nac62116 <56918308+nac62116@users.noreply.github.com>
2025-06-26 18:41:30 +0200

    feat: add new route for accessibility statement (#2161)

---
+++ app/locales/.server/de/routes/accessibility-statement.ts
@@ -0,0 +1,85 @@
+export const locale = {
+  title: "Barrierefreiheitserklärung für die MINTvernetzt Community-Plattform",
+  scope: {
+    title: "1. Geltungsbereich dieser Erklärung",
+    content:
+      "Diese Erklärung zur Barrierefreiheit gilt für die unter <0>{{baseUrl}}</0> erreichbare MINTvernetzt Community-Plattform einschließlich ihrer von dort erreichbaren Anwendungen MINT-Mediendatenbank und MINT-Sharepic-Generator.",
+  },
+  legalBasis: {
+    title: "2. Rechtsgrundlagen",
+    content:
+      "Diese Erklärung basiert auf den Vorgaben des Barrierefreiheitsstärkungsgesetzes (BFSG) gemäß EU-Richtlinie 2016/2102 und entsprechenden nationalen Rechtsvorschriften.",
+  },
+  complianceStatus: {
+    title: "3. Stand der Vereinbarkeit mit den Anforderungen",
+    disclaimer:
+      "Die MINTvernetzt Community-Plattform ist bestrebt, ihre digitale Infrastruktur barrierefrei zugänglich zu machen. Aktuell ist die Plattform noch nicht vollständig barrierefrei. Wir arbeiten kontinuierlich daran, die Zugänglichkeit weiter zu verbessern. Es bestehen insbesondere Einschränkungen in folgenden Bereichen:",
+    restrictions: {
+      userGeneratedContent: {
+        title: "Nutzer:innengenerierter Content",
+        subline:
+          "Nutzer:innengenerierter Content kann <0>in manchen Fällen</0> nicht barrierefrei sein, darunter:",
+        list: {
+          altTexts:
+            "Das Einfügen von ALT-Texte bei Bildern, die von Nutzer:innen hochgeladen werden, ist z.Z. noch nicht möglich. Wir arbeiten dran, dass dies möglich sein wird. ",
+          material:
+            "Nutzer:innengenerierter Content, wie PDFs, die als Material-Upload bereitgestellt werden, sind ggf. nicht barrierefrei zugänglich.",
+          videos:
+            "Nicht alle von den Nutzenden eingebetteten Videos verfügen über Untertitel oder eine Übersetzung in Gebärdensprache.",
+        },
+      },
+      ownContent: {
+        title: "Eigene Inhalte",
+        subline:
+          "Eigene Inhalte sind <0>in seltenen Fällen</0> noch nicht barrierefrei, darunter:",
+        list: {
+          altTexts:
+            "Fehlende Textalternativen: Einige Bilder und Nicht-Text-Inhalte haben unzureichende oder keine beschreibenden Alternativtexte.",
+          simpleLanguage:
+            "Erläuterungen in Leichter Sprache sind noch nicht vorhanden und werden noch erstellt.",
+          signLanguage:
+            "Erläuterungen in Deutscher Gebärdensprache sind nicht vorhanden.",
+          structural:
+            "Unzureichende Strukturierung außerhalb der Registrierung und Anmeldung zu Events. Prüfung und Verbesserungen der anderen Inhalte sind im Gange.",
+          contrast:
+            "Schwacher Kontrast: Erhöhter Kontrast und Kontraste von Grafiken und Bedienelementen sind nicht immer ausreichend.",
+          keyboardInteraction:
+            "Tastaturbedienung ggf. eingeschränkt: Einige Funktionen sind ggf. nicht oder nur eingeschränkt per Tastatur bedienbar, Fokus-Markierung kann u.U. fehlen.",
+          metaTitles:
+            "Unklare oder fehlende Seitentitel in den Überblicksseiten: Der Titel der Webseiten ist nicht immer sinnvoll gewählt oder fehlt.",
+          links:
+            "Linkzwecke teils nicht verständlich: Links sind ohne zusätzlichen Kontext oft nicht eindeutig erkennbar.",
+          syntax:
+            "Fehlende oder fehlerhafte Syntax: HTML- und ARIA-Auszeichnungen enthalten ggf. noch Fehler, was Screenreader-Nutzenden Probleme bereitet.",
+        },
+      },
+    },
+  },
+  measuresToImprove: {
+    title: "4. Maßnahmen zur Verbesserung der Barrierefreiheit",
+    subline:
+      "Wir setzen kontinuierlich Maßnahmen um, um die Barrierefreiheit der Plattform zu verbessern:",
+    regularChecks:
+      "<0>Regelmäßige Überprüfungen:</0> Durchführung von Barrierefreiheitstests gemäß den Richtlinien der Barrierefreien Informationstechnik-Verordnung (BITV 2.0).",
+    training:
+      "<0>Schulungen:</0> Sensibilisierung und Schulung des Entwicklungsteams in Bezug auf barrierefreie Entwicklung im Austausch mit dem Design-Team.",
+    userFeedback:
+      "<0>Nutzer:innen-Feedback:</0> Integration von Feedback von Nutzer:innen zur Identifikation und Behebung von Barrieren.",
+  },
+  reportBarriers: {
+    title: "5. Feedback und Kontakt – Barrieren melden",
+    subline:
+      "Sollten Mängel in Bezug auf die barrierefreie Gestaltung unserer Plattform auffallen oder benötigst Du Informationen in barrierefreiem Format, kontaktiere uns bitte:",
+    email: "<0>E-Mail:</0> <1>{{supportMail}}</1>",
+    phone:
+      "<0>Telefon:</0> <1>+49 (0) 162 16 96 01 9</1> – <2>Inga Leffers</2>, Product Ownerin & Community-Managerin",
+    disclaimer: "Wir bemühen uns, Deine Anfragen zeitnah zu bearbeiten.",
+  },
+  moreInformation: {
+    title: "6. Weitere Informationen",
+    content:
+      "Die MINTvernetzt Community-Plattform ist ein Projekt von MINTvernetzt, das darauf abzielt, die MINT-Community in Deutschland zu vernetzen und den Austausch von Wissen und Erfahrungen zu fördern. Weitere Informationen findest Du auf unserer Website: <0>https://www.mint-vernetzt.de</0>",
+  },
+  notice:
+    "Hinweis: Diese Erklärung wird regelmäßig überprüft und aktualisiert, um den aktuellen Stand der Barrierefreiheit unserer Plattform widerzuspiegeln.",
+} as const;
---
+++ app/routes/accessibility-statement.tsx
@@ -0,0 +1,221 @@
+import { TextButton } from "@mint-vernetzt/components/src/molecules/TextButton";
+import { useLoaderData, type LoaderFunctionArgs } from "react-router";
+import { detectLanguage } from "~/i18n.server";
+import {
+  insertComponentsIntoLocale,
+  insertParametersIntoLocale,
+} from "~/lib/utils/i18n";
+import { languageModuleMap } from "~/locales/.server";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const language = await detectLanguage(request);
+  const locales = languageModuleMap[language]["accessibility-statement"];
+  return {
+    locales,
+    baseUrl: process.env.COMMUNITY_BASE_URL,
+    supportMail: process.env.SUPPORT_MAIL,
+  };
+};
+
+export default function Imprint() {
+  const { locales, baseUrl, supportMail } = useLoaderData<typeof loader>();
+
+  return (
+    <section className="mv-w-full mv-mx-auto mv-px-4 @sm:mv-max-w-screen-container-sm @md:mv-max-w-screen-container-md @lg:mv-max-w-screen-container-lg @xl:mv-max-w-screen-container-xl @xl:mv-px-6 @2xl:mv-max-w-screen-container-2xl mv-my-8 @md:mv-my-10 @lg:mv-my-20">
+      <h1>{locales.title}</h1>
+      <section className="mv-mt-6">
+        <h2>{locales.scope.title}</h2>
+        <p>
+          {insertComponentsIntoLocale(
+            insertParametersIntoLocale(locales.scope.content, {
+              baseUrl: baseUrl,
+            }),
+            [
+              <TextButton
+                key="baseUrl"
+                as="link"
+                to="/"
+                className="mv-inline-flex"
+              />,
+            ]
+          )}
+        </p>
+      </section>
+      <section className="mv-mt-6">
+        <h2>{locales.legalBasis.title}</h2>
+        <p>{locales.legalBasis.content}</p>
+      </section>
+      <section className="mv-mt-6">
+        <h2>{locales.complianceStatus.title}</h2>
+        <p>{locales.complianceStatus.disclaimer}</p>
+        <h3 className="mv-mt-4">
+          {locales.complianceStatus.restrictions.userGeneratedContent.title}
+        </h3>
+        <p className="mv-mb-4">
+          {insertComponentsIntoLocale(
+            locales.complianceStatus.restrictions.userGeneratedContent.subline,
+            [
+              <span
+                key="highlight-user-generated-content"
+                className="mv-font-semibold"
+              />,
+            ]
+          )}
+        </p>
+        <ul className="mv-list-disc mv-list-inside">
+          <li>
+            {
+              locales.complianceStatus.restrictions.userGeneratedContent.list
+                .altTexts
+            }
+          </li>
+          <li>
+            {
+              locales.complianceStatus.restrictions.userGeneratedContent.list
+                .material
+            }
+          </li>
+          <li>
+            {
+              locales.complianceStatus.restrictions.userGeneratedContent.list
+                .videos
+            }
+          </li>
+        </ul>
+        <h3 className="mv-mt-4">
+          {locales.complianceStatus.restrictions.ownContent.title}
+        </h3>
+        <p className="mv-mb-4">
+          {insertComponentsIntoLocale(
+            locales.complianceStatus.restrictions.ownContent.subline,
+            [<span key="highlight own content" className="mv-font-semibold" />]
+          )}
+        </p>
+        <ul className="mv-list-disc mv-list-inside">
+          <li>
+            {locales.complianceStatus.restrictions.ownContent.list.altTexts}
+          </li>
+          <li>
+            {
+              locales.complianceStatus.restrictions.ownContent.list
+                .simpleLanguage
+            }
+          </li>
+          <li>
+            {locales.complianceStatus.restrictions.ownContent.list.signLanguage}
+          </li>
+          <li>
+            {locales.complianceStatus.restrictions.ownContent.list.structural}
+          </li>
+          <li>
+            {locales.complianceStatus.restrictions.ownContent.list.contrast}
+          </li>
+          <li>
+            {
+              locales.complianceStatus.restrictions.ownContent.list
+                .keyboardInteraction
+            }
+          </li>
+          <li>
+            {locales.complianceStatus.restrictions.ownContent.list.metaTitles}
+          </li>
+          <li>{locales.complianceStatus.restrictions.ownContent.list.links}</li>
+          <li>
+            {locales.complianceStatus.restrictions.ownContent.list.syntax}
+          </li>
+        </ul>
+      </section>
+      <section className="mv-mt-6">
+        <h2>{locales.measuresToImprove.title}</h2>
+        <p className="mv-mb-4">{locales.measuresToImprove.subline}</p>
+        <ul className="mv-list-disc mv-list-inside">
+          <li>
+            {insertComponentsIntoLocale(
+              locales.measuresToImprove.regularChecks,
+              [
+                <span
+                  key="highlight-regular-checks"
+                  className="mv-font-semibold"
+                />,
+              ]
+            )}
+          </li>
+          <li>
+            {insertComponentsIntoLocale(locales.measuresToImprove.training, [
+              <span key="highlight-training" className="mv-font-semibold" />,
+            ])}
+          </li>
+          <li>
+            {insertComponentsIntoLocale(
+              locales.measuresToImprove.userFeedback,
+              [
+                <span
+                  key="highlight-user-feedback"
+                  className="mv-font-semibold"
+                />,
+              ]
+            )}
+          </li>
+        </ul>
+      </section>
+      <section className="mv-mt-6">
+        <h2>{locales.reportBarriers.title}</h2>
+        <p className="mv-mb-4">{locales.reportBarriers.subline}</p>
+        <ul className="mv-list-disc mv-list-inside mv-mb-4">
+          <li>
+            {insertComponentsIntoLocale(
+              insertParametersIntoLocale(locales.reportBarriers.email, {
+                supportMail: supportMail,
+              }),
+              [
+                <span key="highlight-email" className="mv-font-semibold" />,
+                <TextButton
+                  key="email-link"
+                  as="link"
+                  to={`mailto:${supportMail}`}
+                  className="mv-inline-flex"
+                />,
+              ]
+            )}
+          </li>
+          <li>
+            {insertComponentsIntoLocale(locales.reportBarriers.phone, [
+              <span key="highlight-phone" className="mv-font-semibold" />,
+              <TextButton
+                key="phone-link"
+                as="link"
+                to="tel:+491621696019"
+                className="mv-inline-flex"
+              />,
+              <TextButton
+                key="contact-person"
+                as="link"
+                to="/profile/ingaleffers"
+                className="mv-inline-flex"
+              />,
+            ])}
+          </li>
+        </ul>
+        <p>{locales.reportBarriers.disclaimer}</p>
+      </section>
+      <section className="mv-mt-6">
+        <h2>{locales.moreInformation.title}</h2>
+        <p>
+          {insertComponentsIntoLocale(locales.moreInformation.content, [
+            <TextButton
+              key="more-info-link"
+              as="link"
+              to="https://www.mint-vernetzt.de"
+              className="mv-inline-flex"
+              rel="noreferrer noopener"
+              target="_blank"
+            />,
+          ])}
+        </p>
+      </section>
+      <section className="mv-mt-6">
+        <p>{locales.notice}</p>
+      </section>
+    </section>
+  );
+}
