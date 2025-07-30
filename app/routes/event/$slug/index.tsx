cd2cb9bc21908beb17045394b95a86a7c2e2c3b9
nac62116 <56918308+nac62116@users.noreply.github.com>
2025-07-30 09:21:04 +0200

    feat: mint width on overlay menu

--- app/routes/event/$slug/index.tsx
+++ app/routes/event/$slug/index.tsx
@@ -578,8 +578,8 @@ function Index() {
                   >
                     <span className="mv-p-1">
                       <svg
-                        width="16"
-                        height="16"
+                        width="20"
+                        height="20"
                         viewBox="0 0 16 16"
                         fill="none"
                         xmlns="http://www.w3.org/2000/svg"
