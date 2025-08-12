b5715798870a8433ac628ffaa122450cfaed4a5e
Peter Holló <hello@songsforthe.dev>
2025-08-12 16:40:21 +0200

    WIP

--- app/routes/map/index.tsx
+++ app/routes/map/index.tsx
@@ -1,16 +1,3 @@
-import { type HeadersArgs } from "react-router";
-
-export function headers(args: HeadersArgs) {
-  const { parentHeaders } = args;
-
-  parentHeaders.delete("X-Frame-Options");
-
-  const cspHeaders = parentHeaders.get("Content-Security-Policy");
-  console.log("CSP Headers:", cspHeaders);
-
-  return parentHeaders;
-}
-
 function MapIndex() {
   return (
     <div>
