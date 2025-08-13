a3ead74cbbb0772f96bb26e1f4226be1d8993108
Peter Holló <hello@songsforthe.dev>
2025-08-13 11:41:09 +0200

    feat: Map on iFrame route (#2271)

--- app/routes/map/index.tsx
+++ app/routes/map/index.tsx
@@ -1,6 +1,8 @@
 import { type LoaderFunctionArgs } from "react-router";
 import { checkFeatureAbilitiesOrThrow } from "../feature-access.server";
 import { createAuthClient } from "~/auth.server";
+import { useEffect, useRef } from "react";
+import maplibreGL from "maplibre-gl";
 
 export async function loader(args: LoaderFunctionArgs) {
   const { request } = args;
@@ -13,9 +15,42 @@ export async function loader(args: LoaderFunctionArgs) {
 }
 
 function MapIndex() {
+  const mapContainer = useRef<HTMLDivElement | null>(null);
+  const mapRef = useRef<maplibreGL.Map | null>(null);
+
+  useEffect(() => {
+    if (mapRef.current === null && mapContainer.current !== null) {
+      const center = [10.451526, 51.165691] as [number, number];
+      const maxBounds = [
+        [-2, 46],
+        [22, 56],
+      ] as [[number, number], [number, number]];
+      const zoom = 0;
+      const minZoom = 5.2;
+      const maxZoom = 12;
+      mapRef.current = new maplibreGL.Map({
+        container: mapContainer.current,
+        style: "https://tiles.openfreemap.org/styles/liberty",
+        center,
+        zoom,
+        minZoom,
+        maxZoom,
+        maxBounds,
+      });
+      mapRef.current.addControl(
+        new maplibreGL.NavigationControl({
+          visualizePitch: true,
+          visualizeRoll: true,
+          showZoom: true,
+          showCompass: false,
+        })
+      );
+    }
+  }, [mapContainer]);
+
   return (
-    <div>
-      <h1>Map Index</h1>
+    <div className="mv-relative mv-w-screen mv-h-dvh">
+      <div ref={mapContainer} className="mv-absolute mv-w-full mv-h-full" />
     </div>
   );
 }
