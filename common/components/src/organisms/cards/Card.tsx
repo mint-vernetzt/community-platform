3b02dfd4708880fd2366bc06b08ec08e6477ebd0
nac62116 <56918308+nac62116@users.noreply.github.com>
2025-10-22 10:02:03 +0200

    Remove legacy Card.RowContainer

--- common/components/src/organisms/cards/Card.tsx
+++ common/components/src/organisms/cards/Card.tsx
@@ -306,44 +306,6 @@ export function CardFooter(props: CardFooterProps) {
 
 Card.Footer = CardFooter;
 
-type CardRowContainerProps = {
-  children?: React.ReactNode;
-};
-
-function wrapCardRowContainerChildren(children: React.ReactNode) {
-  const validChildren = Children.toArray(children).filter((child) => {
-    return isValidElement(child);
-  });
-
-  return Children.map(validChildren, (child) => {
-    return (
-      <div className="w-3/4 @md:w-1/3 px-2 @md:px-4 shrink-0">{child}</div>
-    );
-  });
-}
-
-function CardRowContainer(props: CardRowContainerProps) {
-  const itemsPerRow = 3;
-
-  const validChildren = Children.toArray(props.children).filter((child) => {
-    return isValidElement(child);
-  });
-
-  const classes = classNames("flex -mx-2 @md:-mx-4 mb-8", {
-    "flex-wrap": itemsPerRow < validChildren.length,
-  });
-
-  return (
-    <div className="relative">
-      <div className={classes}>
-        {wrapCardRowContainerChildren(validChildren)}
-      </div>
-    </div>
-  );
-}
-
-Card.RowContainer = CardRowContainer;
-
 export function CardControls(props: React.PropsWithChildren) {
   const { children } = props;
 
