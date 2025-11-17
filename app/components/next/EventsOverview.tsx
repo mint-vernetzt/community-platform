72468460def48ae08e6d77b906929daa48280a3f
Peter Holló <hello@songsforthe.dev>
2025-11-17 16:23:01 +0100

    fix: ics download icon to big

--- app/components/next/EventsOverview.tsx
+++ app/components/next/EventsOverview.tsx
@@ -229,8 +229,8 @@ function PeriodOfTime(props: {
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
+          width="16"
+          height="16"
           viewBox="0 0 20 20"
           fill="none"
         >
