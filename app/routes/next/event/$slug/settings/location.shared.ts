55ae56e4ebeb3a6746c782aadaabdaf9f7f59267
nac62116 <56918308+nac62116@users.noreply.github.com>
2025-12-08 12:40:51 +0100

    fix: Clear onsite or online values depending on selected stage

--- app/routes/next/event/$slug/settings/location.shared.ts
+++ app/routes/next/event/$slug/settings/location.shared.ts
@@ -22,31 +22,51 @@ export function createEventLocationSchema(locales: {
   stageRequired: string;
   stageInvalid: string;
 }) {
-  const schema = z.object({
-    stage: z.enum(Object.values(Stages) as [string, ...string[]], {
-      required_error: locales.stageRequired,
-      invalid_type_error: locales.stageInvalid,
-    }),
-    venueName: z.string().optional().transform(transformEmptyToNull),
-    venueStreet: z.string().optional().transform(transformEmptyToNull),
-    venueZipCode: z.string().optional().transform(transformEmptyToNull),
-    venueCity: z.string().optional().transform(transformEmptyToNull),
-    conferenceLink: z.string().optional().transform(transformEmptyToNull),
-    conferenceCode: z.string().optional().transform(transformEmptyToNull),
-    accessibilityInformation: z
-      .string()
-      .optional()
-      .transform(transformEmptyToNull),
-    accessibilityInformationRTEState: z
-      .string()
-      .optional()
-      .transform(transformEmptyToNull),
-    privacyInformation: z.string().optional().transform(transformEmptyToNull),
-    privacyInformationRTEState: z
-      .string()
-      .optional()
-      .transform(transformEmptyToNull),
-  });
+  const schema = z
+    .object({
+      stage: z.enum(Object.values(Stages) as [string, ...string[]], {
+        required_error: locales.stageRequired,
+        invalid_type_error: locales.stageInvalid,
+      }),
+      venueName: z.string().optional().transform(transformEmptyToNull),
+      venueStreet: z.string().optional().transform(transformEmptyToNull),
+      venueZipCode: z.string().optional().transform(transformEmptyToNull),
+      venueCity: z.string().optional().transform(transformEmptyToNull),
+      conferenceLink: z.string().optional().transform(transformEmptyToNull),
+      conferenceCode: z.string().optional().transform(transformEmptyToNull),
+      accessibilityInformation: z
+        .string()
+        .optional()
+        .transform(transformEmptyToNull),
+      accessibilityInformationRTEState: z
+        .string()
+        .optional()
+        .transform(transformEmptyToNull),
+      privacyInformation: z.string().optional().transform(transformEmptyToNull),
+      privacyInformationRTEState: z
+        .string()
+        .optional()
+        .transform(transformEmptyToNull),
+    })
+    .transform((data) => {
+      if (data.stage === Stages.Online) {
+        return {
+          ...data,
+          venueName: null,
+          venueStreet: null,
+          venueZipCode: null,
+          venueCity: null,
+        };
+      }
+      if (data.stage === Stages.OnSite) {
+        return {
+          ...data,
+          conferenceLink: null,
+          conferenceCode: null,
+        };
+      }
+      return data;
+    });
 
   return schema;
 }
