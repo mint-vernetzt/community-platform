4bc4c00a1d54609b670ebf1e311df5481a8f4f09
Peter Holló <hello@songsforthe.dev>
2026-01-05 13:41:14 +0100

    Add design reference

--- app/components/next/Hint.tsx
+++ app/components/next/Hint.tsx
@@ -1,3 +1,8 @@
+// Design
+// Name: Hinweis
+// Source: https://www.figma.com/design/9aKvb1kUWVYaLDi4xjRaSB/Event-Settings?node-id=288-7794&m=dev
+// TODO: No component only frame
+
 function Hint(props: { children: React.ReactNode }) {
   return (
     <p className="p-4 rounded-lg bg-primary-50 text-neutral-600">
