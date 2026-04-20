234162ebef21f9e2d0c26ecd36a7d87977d987a2
nac62116 <56918308+nac62116@users.noreply.github.com>
2026-04-20 19:58:59 +0200

    fix: add missing delay to refactored CountUp

--- app/components-next/CountUp.tsx
+++ app/components-next/CountUp.tsx
@@ -1,39 +1,49 @@
 import { useEffect, useRef } from "react";
 import { useCountUp, type CountUpProps } from "react-countup";
 
-export function CountUp(
-  props: CountUpProps & {
-    triggerOn?: "visible" | "mount";
-  }
-) {
-  const { triggerOn = "visible", ...countUpProps } = props;
+export function CountUp(props: CountUpProps) {
+  const {
+    autoAnimate = true,
+    autoAnimateDelay = 100,
+    autoAnimateOnce = true,
+    start = 0,
+    ...countUpProps
+  } = props;
 
   const ref = useRef<HTMLElement | null>(null);
-  const { start } = useCountUp({
+  const { start: startCountUp } = useCountUp({
     ref: ref as React.RefObject<HTMLElement>,
     separator: ".",
-    startOnMount: triggerOn === "mount",
+    startOnMount: autoAnimate === false,
+    start: 0,
+    autoAnimate,
+    autoAnimateDelay,
+    autoAnimateOnce,
     ...countUpProps,
   });
 
   useEffect(() => {
     const element = ref.current;
-    if (element === null || triggerOn === "mount") {
+    if (element === null || autoAnimate === false) {
       return;
     }
 
     const observer = new IntersectionObserver(
       ([entry]) => {
         if (entry.isIntersecting) {
-          start();
-          observer.disconnect();
+          setTimeout(() => {
+            startCountUp();
+          }, autoAnimateDelay);
+          if (autoAnimateOnce === true) {
+            observer.disconnect();
+          }
         }
       },
       { threshold: 0.1 }
     );
     observer.observe(element);
     return () => observer.disconnect();
-  }, [start, triggerOn]);
+  }, [startCountUp, autoAnimate, autoAnimateDelay, autoAnimateOnce]);
 
-  return <span ref={ref}></span>;
+  return <span ref={ref}>{start}</span>;
 }
