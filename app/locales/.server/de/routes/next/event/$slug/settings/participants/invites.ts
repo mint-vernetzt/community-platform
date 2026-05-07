f831d50a5d5d5f7204f581e7b492fb2fa6a631b9
Peter Holló <hello@songsforthe.dev>
2026-05-07 21:43:50 +0200

    feat: next event settings participants invites

--- app/locales/.server/de/routes/next/event/$slug/settings/participants/invites.ts
+++ app/locales/.server/de/routes/next/event/$slug/settings/participants/invites.ts
@@ -1,1 +1,33 @@
-export const locale = {} as const;
+export const locale = {
+  title: "Ausstehende Einladungen",
+  subline: "Folgende Personen müssen noch auf Deine Einladung reagieren.",
+  search: {
+    label: "Durchsuche die Einladungen",
+    placeholder: "Name",
+    hint: "Gib mindestens 3 Buchstaben ein.",
+    validation: {
+      min: "Bitte gib mindestens 3 Zeichen ein, um zu suchen.",
+    },
+  },
+  list: {
+    item: {
+      invitedAt: "Eingeladen am {{date}}",
+      revoke: "Einladung zurückziehen",
+    },
+    more: "{{count}} weitere anzeigen",
+    less: "{{count}} weniger anzeigen",
+  },
+  mail: {
+    revokeInviteToParticipateOnEvent: {
+      subject: "Die Einladung zur Teilnahme am Event wurde zurückgezogen",
+    },
+  },
+  errors: {
+    revokeInviteToParticipateOnEvent:
+      "Das Zurückziehen der Einladung zur Teilnahme am Event ist fehlgeschlagen.",
+  },
+  success: {
+    revokeInviteToParticipateOnEvent:
+      "Die Einladung zur Teilnahme am Event wurde erfolgreich zurückgezogen.",
+  },
+} as const;
