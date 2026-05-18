5d4f89f3a22bc1508fb233ed92f4026637125fe6
nac62116 <56918308+nac62116@users.noreply.github.com>
2026-05-18 17:17:07 +0200

    fix: next event settings participants routes should also use new image relations

--- app/routes/next/event/$slug/settings/participants/waiting-list.server.ts
+++ app/routes/next/event/$slug/settings/participants/waiting-list.server.ts
@@ -54,7 +54,11 @@ export async function getWaitingListOfEvent(options: {
             academicTitle: true,
             firstName: true,
             lastName: true,
-            avatar: true,
+            avatarImage: {
+              select: {
+                path: true,
+              },
+            },
             createdAt: true,
           },
         },
@@ -93,7 +97,11 @@ export async function getWaitingListOfEvent(options: {
             academicTitle: true,
             firstName: true,
             lastName: true,
-            avatar: true,
+            avatarImage: {
+              select: {
+                path: true,
+              },
+            },
             createdAt: true,
           },
         },
@@ -107,7 +115,10 @@ export async function getWaitingListOfEvent(options: {
   }
 
   const enhancedWaitingList = waitingList.map((participantOnWaitingList) => {
-    let avatar = participantOnWaitingList.profile.avatar;
+    let avatar =
+      participantOnWaitingList.profile.avatarImage !== null
+        ? participantOnWaitingList.profile.avatarImage.path
+        : null;
     let blurredAvatar;
     if (avatar !== null) {
       const publicURL = getPublicURL(authClient, avatar);
