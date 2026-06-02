cea668c94552c14a352b81fe2e8bb852771b7254
nac62116 <56918308+nac62116@users.noreply.github.com>
2026-06-02 23:37:53 +0200

    fix: eslint warnings

--- app/routes/next/event/$slug/settings/participants/list-download.tsx
+++ app/routes/next/event/$slug/settings/participants/list-download.tsx
@@ -1,9 +1,5 @@
 import { redirect, type LoaderFunctionArgs } from "react-router";
-import {
-  createAuthClient,
-  getSessionUser,
-  getSessionUserOrThrow,
-} from "~/auth.server";
+import { createAuthClient, getSessionUser } from "~/auth.server";
 import { invariantResponse } from "~/lib/utils/response";
 import { checkFeatureAbilitiesOrThrow } from "~/routes/feature-access.server";
 import { getRedirectPathOnProtectedEventRoute } from "../../settings.server";
