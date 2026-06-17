b6147f566a3cb07cda0de3b3c3be15b5d7fe51ea
nicholassignore <nicholas.santos@live.de>
2026-06-17 15:30:54 +0200

    wip

--- prisma/scripts/migrate-to-parent-participation-required/index.ts
+++ prisma/scripts/migrate-to-parent-participation-required/index.ts
@@ -47,9 +47,26 @@ async function main() {
     );
   }
 
+  // Option 1: If the user is not allowed to be on the waiting list (he is not yet participant of the parent event) -> he has to move up in the parent event waiting list
+  // & be removed on the current waiting list(recursively)
+  // Problem (1): We need to communicate this to the user
+
+  // Option 2: CheckNecessityOfWaitingListMovement script
+  // Checks if there are users that are on the waiting list of a child event
+  // & are not participants of the parent
+  // & can not be added as participant to the parent
+  // & are not in the past
+
   // What's about waiting list
+  // Same iteration of adding participant to waiting list as Participants, but with exceptions
+  // We ignore events that are in the past
+  // If the parent event has spots left, add participant automatically instead of putting him on the waiting list
+  // In order add a participant from the waiting list, the user must first be added to all n-parent events as participant
+  // Problem: What if there are no spots left?
+
   // Enforce that published child events should have published parent event
   // 1. check database if this is a case
+  //
 
   // Attention: Check if n level deep events handled properly
 }
