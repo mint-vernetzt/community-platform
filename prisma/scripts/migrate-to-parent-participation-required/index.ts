5e5b2f784cab35e79fb0141f3d2d8778a8eae9a3
Peter Holló <hello@songsforthe.dev>
2026-06-18 10:20:49 +0200

    Update events that were not updated before + delete comments

--- prisma/scripts/migrate-to-parent-participation-required/index.ts
+++ prisma/scripts/migrate-to-parent-participation-required/index.ts
@@ -1,13 +1,12 @@
 import { prismaClient } from "~/prisma.server";
 
 async function main() {
-  // Default: all events have parentParticipationRequired set to null (Database migration)
-  // Update all existing events with child events to have parentParticipationRequired set to true
   const updatedEvents = await prismaClient.event.updateMany({
     where: {
       childEvents: {
         some: {},
       },
+      parentParticipationRequired: null,
     },
     data: {
       parentParticipationRequired: true,
@@ -16,7 +15,7 @@ async function main() {
   console.log(
     `Updated ${updatedEvents.count} events to have parentParticipationRequired set to true`
   );
-  // Participants on child events should be also participate on parent event
+
   const allParticipantsOnChildEvents =
     await prismaClient.participantOfEvent.findMany({
       where: {
@@ -48,7 +47,6 @@ async function main() {
 
   for (const entry of allParticipantsOnChildEvents) {
     if (entry.event.parentEventId === null) {
-      // Make Typescript happy, but should always be not null due to the query condition
       continue;
     }
     await recursivelyAddParticipantsToParentEvent(
@@ -57,22 +55,13 @@ async function main() {
     );
   }
 
-  // What we know: There are no people on the waiting list of a child event that are not participant of the parent event
-  // So we can skip handle waiting list entries
-
-  // Enforce that published child events should have published parent event
-  // What we know: There are published child events that have unpublished parent events
-  // Affected parent: 3a177d10-e379-473b-b46f-cceff07a2e1f "Tech4Kids  Durchgang 2026/2027"
-  // Question: Does this affect any functionality?
-  // Nope: You can actually participate on child event if parent event is not published (mode == "canParticipate") because parentEvent will be null
-  // We handle this case by setting participationRequired to false on published child events with unpublished parent events
-
   const updatedChildEvents = await prismaClient.event.updateMany({
     where: {
       published: true,
       parentEvent: {
         published: false,
       },
+      parentParticipationRequired: null,
     },
     data: {
       parentParticipationRequired: false,
@@ -82,13 +71,6 @@ async function main() {
   console.log(
     `Updated ${updatedChildEvents.count} published child events with unpublished parent events to have parentParticipationRequired set to false`
   );
-
-  // Attention: Check if n level deep events handled properly
-
-  // Test Scenario
-  // child event has participants that are not participant of the parent event
-  // child event has participants that are not participants of parent and grand parent event
-  // published child event has unpublished parent event
 }
 
 async function recursivelyAddParticipantsToParentEvent(
