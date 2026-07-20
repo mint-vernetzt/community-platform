008e5368e4f1fd742ae6cff5ffd196554386581b
Peter Holló <hello@songsforthe.dev>
2026-07-20 09:54:42 +0200

    feat: Handle confirmation token not found

--- app/locales/.server/de/routes/auth/guest/verify.ts
+++ app/locales/.server/de/routes/auth/guest/verify.ts
@@ -1,4 +1,9 @@
 export const locale = {
   subject: "Du hast Dich erfolgreich für eine Veranstaltung angemeldet",
   message: "Du hast Dich erfolgreich für eine Veranstaltung angemeldet.",
+  notFound: {
+    title: "Kein Gast gefunden",
+    description:
+      "Es konnte kein Gast für das angegebene Token gefunden werden. Es könnte sein, dass Du Deine Anmeldung bereits bestätigt hast. Bitte überprüfe Dein E-Mail-Postfach und ggf. auch den Spam-Ordner. Wenn Du keine E-Mail erhalten hast, wende Dich bitte an unseren <0>Support</0>.",
+  },
 } as const;
