c296b5292be554dba5e2a1ff1ea12234a8f7d22c
Peter Holló <hello@songsforthe.dev>
2026-08-17 17:21:25 +0200

    fix: Custom registration period  not shown properly

--- app/routes/event/$slug/settings/registration/period.tsx
+++ app/routes/event/$slug/settings/registration/period.tsx
@@ -228,16 +228,21 @@ function RegistrationPeriod() {
   const navigation = useNavigation();
 
   const [isDefault, setIsDefault] = useState(
-    event.participationFrom.getDate() === event.createdAt.getDate() &&
-      event.participationUntil.getDate() === event.startTime.getDate()
+    event.participationFrom.getTime() === event.createdAt.getTime() &&
+      event.participationUntil.getTime() === event.startTime.getTime()
   );
 
   useEffect(() => {
     if (navigation.state === "idle") {
-      setIsDefault(
-        searchParams.get(REGISTRATION_PERIOD_SEARCH_PARAM) !==
-          REGISTRATION_PERIOD_CUSTOM
+      const registrationSearchParamValue = searchParams.get(
+        REGISTRATION_PERIOD_SEARCH_PARAM
       );
+      if (registrationSearchParamValue !== null) {
+        setIsDefault(
+          searchParams.get(REGISTRATION_PERIOD_SEARCH_PARAM) !==
+            REGISTRATION_PERIOD_CUSTOM
+        );
+      }
     }
   }, [navigation.state, searchParams]);
 
