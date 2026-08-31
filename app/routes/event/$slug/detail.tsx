b8deb4ba2393028405cd34088971c07eadc837dd
nac62116 <56918308+nac62116@users.noreply.github.com>
2026-08-31 20:56:09 +0200

    feat: optimize wording in guest participation mail

--- app/routes/event/$slug/detail.tsx
+++ app/routes/event/$slug/detail.tsx
@@ -835,8 +835,6 @@ function Detail() {
     loaderData.event.parentEvent !== null &&
     tokenHash === loaderData.event.parentEvent.participationToken;
 
-  console.log(loaderData.mode);
-
   return (
     <>
       <BasicStructure>
