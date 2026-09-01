a42292a81b8efa708a0ba8646fe68bc95544bbef
nac62116 <56918308+nac62116@users.noreply.github.com>
2026-09-01 17:13:53 +0200

    fix: new CSRF check of react router 7.18.0 breaks all actions behind a reverse proxy (#3154)

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,5 +1,9 @@
 import type { Config } from "@react-router/dev/config";
+import "dotenv/config";
 
 export default {
   ssr: true,
+  allowedActionOrigins: [
+    process.env.COMMUNITY_BASE_URL.replace(/^https?:\/\//, ""),
+  ],
 } satisfies Config;
